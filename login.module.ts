8d1ddcc6de2b356b96a382334ed30bb6dee77ede
Dan Sarracayo <dan.sarracayo@setl.io>
2017-08-08 09:39:23 +0100

    1.0.3 - Added some dependacies and updated the version.

--- login.module.ts
+++ login.module.ts
@@ -1,5 +1,8 @@
 /* Core imports. */
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 /* Login view. */
 import { SetlLoginComponent } from './login.component';
@@ -13,6 +16,9 @@ import { ToasterModule, ToasterService } from 'angular2-toaster';
     ],
     imports: [
         ToasterModule,
+        BrowserModule,
+        FormsModule,
+        RouterModule
     ],
     exports: [
         SetlLoginComponent,
