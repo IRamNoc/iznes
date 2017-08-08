19c9cc71d436e05d702352ae589138ac67b051fb
ming <mingrui.huang@setl.io>
2017-08-08 16:08:24 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import {name} from './__init__';
+import {SetlWebSocket} from './SetlWebSocket';
+import {SetlCallbackRegister} from './SetlCallbackRegister';
+
+export {
+    name,
+    SetlWebSocket,
+    SetlCallbackRegister
+}
