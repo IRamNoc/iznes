f87ecd2d47c23f2eac6a12762fc613bcee110f3f
ming <mingrui.huang@setl.io>
2017-08-11 16:32:31 +0100

    TG-0 Add redux app state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule, FormControl, NgModel} from '@angular/forms';
+import {FormsModule, FormControl, NgModel, ReactiveFormsModule} from '@angular/forms';
 import {ClarityModule} from 'clarity-angular';
 import {RouterModule} from '@angular/router';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
@@ -8,9 +8,12 @@ import {HttpModule} from '@angular/http';
 
 import {SidebarModule} from 'ng-sidebar';
 
-/* Services */
+/* Connection Services */
 import {MemberSocketService} from '@setl/websocket-service';
 import {WalletNodeSocketService} from '@setl/websocket-service';
+
+/* Core services*/
+import {MyUserService} from '@setl/core-req-services';
 
 /* Routes. */
 import {ROUTES} from './app.routes';
@@ -33,10 +36,15 @@ import {HomeComponent} from './home/home.component';
 /**
  * App main state
  */
-// import {
-//     AppStore,
-//     appStoreProviders
-// } from './app.store';
+import {NgRedux, NgReduxModule} from '@angular-redux/store';
+
+import {
+    createAppStore,
+    appSaga,
+    sagaMiddleware
+} from './store/app.store';
+
+import {AppState} from './store/app.reducer';
 
 
 @NgModule({
@@ -57,16 +65,21 @@ import {HomeComponent} from './home/home.component';
         RouterModule.forRoot(ROUTES),
         SelectModule,
         SetlLoginModule,
-        SidebarModule
+        SidebarModule,
+        NgReduxModule,
+        ReactiveFormsModule
     ],
     providers: [
         {provide: LocationStrategy, useClass: HashLocationStrategy},
-        // appStoreProviders,
         MemberSocketService,
-        WalletNodeSocketService
+        WalletNodeSocketService,
+        MyUserService
     ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
-
+    constructor(ngRedux: NgRedux<AppState>) {
+        ngRedux.provideStore(createAppStore());
+        sagaMiddleware.run(appSaga);
+    }
 }
