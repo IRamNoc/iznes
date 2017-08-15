15c3ad35529132de3d6b47104582b7d735c6a509
Luke Brown <luke.brown@setl.io>
2017-08-15 16:53:48 +0100

    yo

---
+++ messages.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SetlMessagesComponent} from './messages/messages.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        SetlMessagesComponent
+    ],
+    exports: [
+        SetlMessagesComponent
+    ]
+})
+export class MessagesModule {
+}
