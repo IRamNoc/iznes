b913024db6cacda2bc9cf8e406b73fed4ad7cf8b
ming <mingrui.huang@setl.io>
2017-08-16 16:36:38 +0100

    Export components properly and htmlwq

--- asset-servicing.module.ts
+++ asset-servicing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RegisterIssuerComponent} from './register-issuer/register-issuer.component';
+import {RegisterAssetComponent} from './register-asset/register-asset.component';
+import {IssueAssetComponent} from './issue-asset/issue-asset.component';
+import {SelectModule} from 'ng2-select';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [
+        CommonModule,
+        SelectModule
+    ],
+    declarations: [
+        RegisterIssuerComponent,
+        RegisterAssetComponent,
+        IssueAssetComponent
+    ],
+    exports: [
+        RegisterIssuerComponent,
+        RegisterAssetComponent,
+        IssueAssetComponent
+    ]
 })
-export class AssetServicingModule { }
+export class AssetServicingModule {
+}
