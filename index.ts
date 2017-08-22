c73808fe58d2d2087f21e9631767791474861cfd
Luke Brown <luke.brown@setl.io>
2017-08-22 15:30:08 +0100

    Message request service:

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export {MyUserService} from './my-user/my-user.service';
 export {WalletNodeRequestService} from './walletnode-request/walletnode-request.service';
 export {InitialisationService} from './initialisation/initialisation.service';
 export {MyWalletsService} from './my-wallets/my-wallets.service';
+export {MyMessagesService} from './my-messages/my-messages.service';
