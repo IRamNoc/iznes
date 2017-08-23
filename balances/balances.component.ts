d46fb27f1d7540d353f065d51c2625548831902d
Ollie Kett <ollie.kett@setl.io>
2017-08-23 14:41:25 +0100

    Core Balances

--- balances/balances.component.ts
+++ balances/balances.component.ts
@@ -10,7 +10,9 @@ export class SetlBalancesComponent implements OnInit, AfterViewInit {
 
     @ViewChild('myDataGrid') myDataGrid;
 
-    public assets;
+    public assets
+    public singleAsset;
+    public singleAssetHistory;
 
     constructor(private ref: ChangeDetectorRef) {
 
@@ -34,11 +36,199 @@ export class SetlBalancesComponent implements OnInit, AfterViewInit {
             {
                 id: '3',
                 identifier: 'EUR',
-                asset: 'XXXXXXXXXXXXXXXXXXXPayment_Bank1|EUR',
+                asset: 'Payment_Bank1|EUR',
                 total: '100',
                 encumbered: '1',
                 free: '99'
             },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '1',
+                identifier: 'FR0001',
+                asset: 'LEI0001|OFI RS Dynamique C D',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+            {
+                id: '3',
+                identifier: 'EUR',
+                asset: 'Payment_Bank1|EUR',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+        ];
+
+        this.singleAsset = [
+            {
+                id: '1',
+                address: 'd332bad22159a6ea1122f032b57cfd92',
+                total: '987,654,321',
+                encumbered: '321',
+                free: '987,654,000'
+            },
+            {
+                id: '2',
+                address: '2219a822bf8087aa5e82788ec2a87cc5',
+                total: '100',
+                encumbered: '1',
+                free: '99'
+            },
+        ];
+
+        this.singleAssetHistory = [
+            {
+                id: '1',
+                txid: '037d8a00b9',
+                asset: 'Payment_Bank1|EUR',
+                amount: '987,654,321',
+                time: '2017-04-18 10:24:16 UTC',
+                type: 'Issue Asset'
+            },
+            {
+                id: '2',
+                txid: '16a91bd771',
+                asset: 'Contract',
+                amount: '987,654,321',
+                time: '2017-04-18 10:24:16 UTC',
+                type: 'Contract Commitment'
+            },
+            {
+                id: '3',
+                txid: '423336b76f',
+                asset: 'Contract',
+                amount: '987,654,321',
+                time: '2017-04-18 10:24:16 UTC',
+                type: 'Contract Commitment'
+            }
         ];
     }
 
