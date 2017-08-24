55ad2592b5bf8bc910e4c216192e7aa1017e1f49
ming <mingrui.huang@setl.io>
2017-08-24 17:40:49 +0100

    Fix case sensitive

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export { UserAdminModule } from './userAdmin.module';
-export { AdminUsersComponent } from './users/users.component';
-export { AdminWalletsComponent } from './wallets/wallets.component';
-export { AdminPermissionsComponent } from './permissions/permissions.component';
-export { AdminWizardComponent } from './wizard/wizard.component';
+export {UserAdminModule} from './useradmin.module';
+export {AdminUsersComponent} from './users/users.component';
+export {AdminWalletsComponent} from './wallets/wallets.component';
+export {AdminPermissionsComponent} from './permissions/permissions.component';
+export {AdminWizardComponent} from './wizard/wizard.component';
