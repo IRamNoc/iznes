89e3be4704640b64f1709320b72383a709421059
ming <mingrui.huang@setl.io>
2017-08-24 14:36:15 +0100

    TG-0 Make test pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,79 +1,13 @@
 import {TestBed, async} from '@angular/core/testing';
-
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, FormControl, NgModel, ReactiveFormsModule} from '@angular/forms';
-import {SelectModule} from 'ng2-select';
-import {ClarityModule} from 'clarity-angular';
-import {NgRedux, NgReduxModule} from '@angular-redux/store';
-import {AppCoreModule} from './core/app-core.module';
-import {AppViewsModule} from './app-views.module';
-import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
-import {RouterModule} from '@angular/router';
-import {ToasterModule, ToasterService} from 'angular2-toaster';
-import {ROUTES} from './app.routes';
-import {SetlLoginModule} from '@setl/core-login';
-import {LoginGuardService} from '@setl/core-login';
-import {SetlMessagesModule} from '@setl/core-messages';
-import {SetlBalancesModule} from '@setl/core-balances';
-import {UserAdminModule} from '@setl/core-useradmin';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
-import {MemberSocketService} from '@setl/websocket-service';
-import {WalletNodeSocketService} from '@setl/websocket-service';
-import {environment} from '../environments/environment';
-import {MyUserService, WalletNodeRequestService} from '@setl/core-req-services';
-import {AssetServicingModule} from '@setl/asset-servicing';
+import {AppModule} from './app.module';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
             imports: [
-                BrowserModule,
-                BrowserAnimationsModule,
-                HttpModule,
-                FormsModule,
-                ClarityModule.forRoot(),
-                RouterModule.forRoot(ROUTES),
-                SelectModule,
-                ToasterModule,
-                NgReduxModule,
-                ReactiveFormsModule,
-                AppCoreModule,
-                AppViewsModule,
-                SetlLoginModule,
-                UserAdminModule,
-                SetlMessagesModule,
-                SetlBalancesModule,
-                AssetServicingModule
-
-            ],
-            providers: [
-                {provide: LocationStrategy, useClass: HashLocationStrategy},
-
-                {
-                    provide: MemberSocketService,
-
-                    useFactory() {
-
-                        return new MemberSocketService(
-                            environment.MEMBER_NODE_CONNECTION.host,
-                            environment.MEMBER_NODE_CONNECTION.port,
-                            environment.MEMBER_NODE_CONNECTION.path
-                        );
-                    }
-                },
-
-                WalletNodeSocketService,
-                MyUserService,
-                WalletNodeRequestService,
-                LoginGuardService,
-                ToasterService
+                AppModule
             ]
-
         }).compileComponents();
     }));
 
---
+++ src/app/app.test.module.ts
@@ -0,0 +1,131 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule, FormControl, NgModel, ReactiveFormsModule} from '@angular/forms';
+import {ClarityModule} from 'clarity-angular';
+import {AlertIconAndTypesService} from 'clarity-angular/emphasis/alert/providers/icon-and-types-service';
+import {RouterModule} from '@angular/router';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {HttpModule} from '@angular/http';
+
+/* Connection Services */
+import {MemberSocketService} from '@setl/websocket-service';
+import {WalletNodeSocketService} from '@setl/websocket-service';
+
+/* Core services*/
+import {
+    MyUserService,
+    WalletNodeRequestService,
+    MyWalletsService,
+    WalletnodeTxService
+} from '@setl/core-req-services';
+
+/* Routes. */
+import {ROUTES} from './app.routes';
+
+/* SETL Modules. */
+import {SetlLoginModule} from '@setl/core-login';
+import {LoginGuardService} from '@setl/core-login';
+import {SetlMessagesModule} from '@setl/core-messages';
+import {SetlBalancesModule} from '@setl/core-balances';
+import {UserAdminModule} from '@setl/core-useradmin';
+import {AssetServicingModule} from '@setl/asset-servicing';
+
+/* Internal App Modules. */
+import {AppCoreModule} from './core/app-core.module';
+import {AppViewsModule} from './app-views.module';
+
+/* Internal Components. */
+import {AppComponent} from './app.component';
+
+/**
+ * Vendor Modules and Services.
+ */
+import {ToasterModule, ToasterService} from 'angular2-toaster';
+import {SelectModule} from 'ng2-select';
+import {JasperoAlertsModule} from '@setl/jaspero-ng2-alerts';
+
+/**
+ * App main state
+ */
+import {NgRedux, NgReduxModule} from '@angular-redux/store';
+
+import {
+    createAppStore,
+    appSaga,
+    sagaMiddleware
+} from './store/app.store';
+
+import {AppState} from './store/app.reducer';
+
+/**
+ * Environment
+ */
+import {environment} from '../environments/environment';
+
+import {APP_BASE_HREF} from '@angular/common';
+
+@NgModule({
+    declarations: [
+        /* Components. */
+        AppComponent
+    ],
+    imports: [
+        /* Vendor/Angular */
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpModule,
+        FormsModule,
+        ClarityModule.forRoot(),
+        RouterModule.forRoot(ROUTES),
+        SelectModule,
+        ToasterModule,
+        JasperoAlertsModule,
+        NgReduxModule,
+        ReactiveFormsModule,
+
+        /* Internal modules. */
+        AppCoreModule,
+        AppViewsModule,
+
+        /* External modules */
+        SetlLoginModule,
+        UserAdminModule,
+        SetlMessagesModule,
+        AssetServicingModule,
+        SetlBalancesModule
+    ],
+    providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy},
+
+        {
+            provide: MemberSocketService,
+
+            useFactory() {
+
+                return new MemberSocketService(
+                    environment.MEMBER_NODE_CONNECTION.host,
+                    environment.MEMBER_NODE_CONNECTION.port,
+                    environment.MEMBER_NODE_CONNECTION.path
+                );
+            }
+        },
+
+        WalletNodeSocketService,
+        MyUserService,
+        MyWalletsService,
+        WalletnodeTxService,
+        WalletNodeRequestService,
+        LoginGuardService,
+        ToasterService,
+        AlertIconAndTypesService,
+        {provide: APP_BASE_HREF, useValue: '/'}
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+    constructor(ngRedux: NgRedux<AppState>) {
+        ngRedux.provideStore(createAppStore());
+        sagaMiddleware.run(appSaga);
+    }
+}
