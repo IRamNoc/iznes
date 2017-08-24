0dc6521a913e0e56c4d4ce9d87afc4d254d27bc1
Dan Sarracayo <dan.sarracayo@setl.io>
2017-08-24 12:13:01 +0100

    Added a new create user function, WIP.

--- useradmin/useradmin.service.ts
+++ useradmin/useradmin.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {MemberSocketService} from '@setl/websocket-service';
 import {SagaHelper, Common} from '@setl/utils';
 import {createMemberNodeSagaRequest} from '@setl/utils/common';
-import {RequestAdminUsersMessageBody} from './useradmin.service.model';
+import {RequestAdminUsersMessageBody, CreateUserMessageBody} from './useradmin.service.model';
 
 @Injectable()
 export class AdminUsersService {
@@ -20,6 +20,21 @@ export class AdminUsersService {
         const messageBody: RequestAdminUsersMessageBody = {
             RequestName: 'um_lu',
             token: this.memberSocketService.token
+        };
+
+        return createMemberNodeSagaRequest(this.memberSocketService, messageBody);
+    }
+
+    public createNewUser (userData:any): any {
+
+        const messageBody: CreateUserMessageBody = {
+            RequestName: 'nu',
+            token: this.memberSocketService.token,
+            username: userData.username,
+            email: userData.email,
+            account: userData.accountType,
+            userType: userData.userType,
+            password: userData.password
         };
 
         return createMemberNodeSagaRequest(this.memberSocketService, messageBody);
