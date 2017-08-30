597801417f7b7113b314d17fd4d0223d67d70143
Dan Sarracayo <dan.sarracayo@setl.io>
2017-08-30 13:24:00 +0100

    Started a little thing.

--- permission-grid/permission-grid.component.ts
+++ permission-grid/permission-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'setl-permission-grid',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermissionGridComponent implements OnInit {
 
-  constructor() { }
+    /* Imports. */
+    @Input()
+    dataStructure:any;
 
-  ngOnInit() {
-  }
+    constructor () {
+
+    }
+
+    ngOnInit () {
+
+    }
 
 }
