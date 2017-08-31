e0f074601f657c0f87aa120d975696ff3f32c210
Dan Sarracayo <dan.sarracayo@setl.io>
2017-08-31 16:11:07 +0100

    Built the component's functionality.

--- permission-grid/permission-grid.component.ts
+++ permission-grid/permission-grid.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    OnInit,
+    EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'setl-permission-grid',
@@ -7,16 +13,83 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PermissionGridComponent implements OnInit {
 
-    /* Imports. */
+    /* Permission areas, each row. */
     @Input()
-    dataStructure:any;
+    public permissionAreas:Array<any> = [];
+
+    /* Permission level, eahc column. */
+    @Input()
+    public permissionLevels:Array<any> = [];
+
+    /* Update event, the output of the data. */
+    @Output()
+    public updateEvent:EventEmitter<any> = new EventEmitter();
+
+    /* Raw Data Store. */
+    private rawComponentData:any = {};
 
     constructor () {
-
+        /* Stub. */
     }
 
     ngOnInit () {
+        // console.log(" |-- Init Permission Grid. ");
+        // console.log(" | > permissionAreas:", this.permissionAreas);
+        // console.log(" | > permissionLevels:", this.permissionLevels);
+        /* Create the new data structure, this will be the two way bound data to
+           the grid. */
+        let i, j;
+        /* Firstly loop each area and make an object to represent it. */
+        for ( i = 0; i < this.permissionAreas.length; i++ ) {
+            /* This is set with a key of the area's ID. */
+            this.rawComponentData[this.permissionAreas[i].id] = {};
 
+            /* Then lets loop over each permission level and set a key with
+               their ID in this new area object. */
+            for ( j = 0; j < this.permissionLevels.length; j++ ) {
+                /* Equal it to zero by default. */
+                this.rawComponentData[this.permissionAreas[i].id][this.permissionLevels[j].id] = "0";
+            }
+            /* Done! */
+        }
+        // console.log(' | < setup raw data: ', this.rawComponentData);
+    }
+
+    /**
+     * Trigger Data Emit
+     * -----------------
+     * Processes the raw data bindings and emits a data structure to anything
+     * listening.
+     *
+     * @return {void}
+     */
+    public triggerDataEmit ():void {
+        /* Ok, so we've edited some data, let's tidy the raw data and remove
+           unchanged areas. */
+        let areaId, levelId, score, emitData = {};
+
+        /* First, lets loop over the raw areas. */
+        for ( areaId in this.rawComponentData ) {
+            /* Then check if each level is not 0, the default. */
+            score = 0;
+
+            for ( levelId in this.rawComponentData[areaId] ) {
+                /* If one of them is not 0, then add to score. */
+                if ( this.rawComponentData[areaId][levelId] !== "0" ) {
+                    // console.log(" | > ", areaId+"-"+levelId+" as not 0");
+                    score += 1;
+                }
+            }
+
+            /* Check if the area scored 0 and add it to the emitData if it did. */
+            if ( score > 0 ) {
+                emitData[areaId] = this.rawComponentData[areaId];
+            }
+        }
+
+        // console.log(" | > data to emit: ", emitData);
+        /* Lastly, emit the data. */
+        this.updateEvent.emit( emitData );
     }
 
 }
