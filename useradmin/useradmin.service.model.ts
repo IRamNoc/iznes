b2d15f5922bc0f910d18a0a93ff9569fd4bfeb59
Dan Sarracayo <dan.sarracayo@setl.io>
2017-09-05 12:41:36 +0100

    Added loads about groups, add, edit, set permissions etc. Working on editing permissions.

--- useradmin/useradmin.service.model.ts
+++ useradmin/useradmin.service.model.ts
@@ -1,15 +1,14 @@
 import {MemberNodeRequest, MemberNodeMessageBody} from '@setl/utils/common';
 
-export interface RequestAdminUsersMessageBody extends MemberNodeMessageBody {
-    token: string;
-}
-
-export interface GetPermissionAreaListBody extends MemberNodeMessageBody {
-    token: string;
-}
-
 export interface RequestOwnWalletsMessage extends MemberNodeRequest {
     MessageBody: RequestAdminUsersMessageBody;
+}
+
+/*
+    Users.
+*/
+export interface RequestAdminUsersMessageBody extends MemberNodeMessageBody {
+    token: string;
 }
 
 export interface CreateUserMessageBody extends MemberNodeMessageBody {
@@ -28,4 +27,58 @@ export interface EditUserMessageBody extends MemberNodeMessageBody {
     userType: string;
     userId: string;
     status: string;
+}
+
+/*
+    Permissions Areas
+*/
+export interface GetPermissionAreaListBody extends MemberNodeMessageBody {
+    token: string;
+}
+
+/*
+    Permission Groups
+*/
+export interface CreateNewGroupBody extends MemberNodeMessageBody {
+    token: string;
+    groupName: string;
+    groupDescription: string;
+    groupType: string;
+}
+
+export interface UpdateGroupBody extends MemberNodeMessageBody {
+    token: string;
+    groupId: string;
+    groupName: string;
+    groupDescription: string;
+    groupType: string;
+}
+
+export interface UpdateAdminPermissionsBody extends MemberNodeMessageBody {
+    token: string;
+    entityId: number;
+    isGroup: number;
+    toAdd: any;
+    toUpdate: any;
+    toDelete: any;
+    isAdmin: string;
+}
+
+export interface UpdateTxPermissionsBody extends MemberNodeMessageBody {
+    token: string;
+    entityId: string;
+    isGroup: string;
+    chainId: string;
+    toAdd: string;
+    toUpdate: string;
+    toDelete: string;
+    isAdmin: string;
+}
+
+export interface RequestPermissionBody extends MemberNodeMessageBody {
+    token: string;
+    entityId: string;
+    isGroup: string;
+    permissionId: string;
+    includeGroup: string;
 }
