3590733cab1a9b483e6e5ea5154186a9f2e94560
Ollie Kett <ollie.kett@setl.io>
2017-09-06 17:03:50 +0100

    Messages sort of fixed

--- messages/messages.component.ts
+++ messages/messages.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Pipe} from '@angular/core';
 
 import {SagaHelper, Common} from '@setl/utils';
-import {NgRedux} from '@angular-redux/store';
+import {NgRedux, select} from '@angular-redux/store';
 
 import {
     SET_MESSAGE_LIST,
@@ -24,6 +24,8 @@ import {fromJS} from "immutable";
 })
 export class SetlMessagesComponent {
 
+    @select(['asset', 'myInstruments', 'newIssueAssetRequest']) newIssuerAssetRequest;
+
     public messages;
     public categories;
     public currentMessage;
@@ -32,13 +34,19 @@ export class SetlMessagesComponent {
     public currentWalletId;
     public walletDirectoryList;
     public walletWithCommuPub;
+    public currentMessageIndex;
+
+    public items: Array<string> = [];
+
+    private value: any = ['Athens'];
+    private _disabledV: string = '0';
+    private disabled: boolean = false;
 
     constructor(private ngRedux: NgRedux<any>,
                 private myMessageService: MyMessagesService) {
 
         ngRedux.subscribe(() => this.updateState());
         this.updateState();
-        this.requestMessages();
 
         // these are the categories that appear along the left hand side as buttons
         this.categories = [
@@ -46,41 +54,237 @@ export class SetlMessagesComponent {
                 name: 'All Messages',
                 desc: 'View your global inbox',
                 icon: 'inbox',
+                type: 'inbox',
                 active: true
             },
             {
                 name: 'Action Messages',
                 desc: 'Messages that require actions',
                 icon: 'balance',
+                type: 'action',
                 active: false
             },
             {
                 name: 'Workflow Messages',
                 desc: 'Messages with multiple actions',
                 icon: 'organization',
+                type: 'workflow',
                 active: false
             },
             {
                 name: 'Sent Messages',
                 desc: 'Messages sent by your account',
                 icon: 'pop-out',
+                type: 'sent',
                 active: false
             },
             {
                 name: 'Deleted Messages',
                 desc: 'View messages that you deleted',
                 icon: 'trash',
+                type: 'deleted',
                 active: false
             },
         ];
+
+        this.currentCategory = 0;
     }
 
 
-    public items: Array<string> = [];
+    /**
+     * Requests Messages
+     *
+     * @param {boolean} isAction
+     * @param {boolean} isDeleted
+     * @param {boolean} isSent
+     * @returns {boolean}
+     */
+    requestMessages(isAction = false, isDeleted = false, isSent = false) {
 
-    private value: any = ['Athens'];
-    private _disabledV: string = '0';
-    private disabled: boolean = false;
+        console.log('------ current wallet id');
+        console.log(this.currentWalletId);
+
+        const requestIsAction = isAction === true ? 1 : 0;
+        const requestIsDeleted = isDeleted === true ? 1 : 0;
+
+        const fromWallet = isSent === true ? this.currentWalletId : 0;
+        const toWallet = isSent === true ? 0 : this.currentWalletId;
+
+        // Create a saga pipe.
+        const asyncTaskPipe = this.myMessageService.requestOwnMessages(
+            0,
+            fromWallet,
+            toWallet,
+            0,
+            5,
+            0,
+            0,
+            requestIsDeleted,
+            requestIsAction,
+            ''
+        );
+
+        // Send a saga action.
+        this.ngRedux.dispatch(SagaHelper.runAsync(
+            [SET_MESSAGE_LIST],
+            [],
+            asyncTaskPipe, {}));
+
+        return false;
+    }
+
+    /**
+     * Decrypts a message
+     *
+     * @param mailId
+     * @param walletId
+     * @param bobPub
+     * @param encryptedMessage
+     */
+    decrypt(mailId, walletId, bobPub, encryptedMessage) {
+        const asyncTaskPipe = this.myMessageService.decryptMessage(
+            walletId, bobPub, encryptedMessage
+        );
+
+        // Send a saga action.
+        this.ngRedux.dispatch(SagaHelper.runAsync(
+            [],
+            [],
+            asyncTaskPipe, {}, (response) => {
+                this.ngRedux.dispatch(setDecryptedContent(mailId, response));
+                return true;
+            })
+        );
+    }
+
+    /**
+     * Handles incoming Redux state when Updated
+     */
+    updateState() {
+        const newState = this.ngRedux.getState();
+        const newMessages = getMyMessagesList(newState);
+
+        const newWalletId = getConnectedWallet(newState);
+
+        console.log(newWalletId);
+        console.log(this.currentWalletId);
+
+        if (newWalletId !== this.currentWalletId) {
+            console.log('i shouldnt hit here');
+            this.currentWalletId = newWalletId;
+            this.requestMessages();
+
+            this.walletDirectoryList = getWalletDirectoryList(newState);
+            this.walletWithCommuPub = this.walletListToSelectItem(this.walletDirectoryList);
+
+            this.items = [this.walletWithCommuPub];
+
+            console.log('messages ------');
+            console.log(this.messages);
+        }
+
+        // if (getNeedRunDecryptState(newState) && newMessages !== this.messages) {
+        //     // this.decrypt(this.messages[0].recipientId, this.messages[0].senderPub, this.messages[0].content);
+        //     this.ngRedux.dispatch({type: DONE_RUN_DECRYPT});
+        //     for (const i in this.messages) {
+        //         const message = this.messages[i];
+        //
+        //         this.decrypt(message.mailId, message.recipientId, message.senderPub, message.content);
+        //
+        //         // sent box
+        //         // this.decrypt(message.mailId, message.senderId, message.recipientPub, message.content);
+        //     }
+        // }
+
+
+        if (newMessages.length > 0) {
+            this.messages = newMessages;
+
+            if (typeof this.currentMessage == "undefined") {
+                this.currentMessage = this.messages[0];
+                this.showMessage(0);
+                return;
+            }
+
+            const id = this.currentMessage.id;
+            this.currentMessage = this.messages[id];
+        }
+    }
+
+    /**
+     * Show message when clicked
+     *
+     * @param index
+     */
+    showMessage(index) {
+
+        // set message to active to apply message-active css class
+        this.messages[index].active = true;
+
+        // set the current message that appears on the right hand side
+        this.currentMessage = this.messages[index];
+
+        // set the id so that message-active an be compared to index and set
+        this.currentMessage.id = index;
+
+        this.currentMessage.hello = 'Ming';
+
+        const message = this.currentMessage;
+
+        this.decrypt(message.mailId, message.recipientId, message.senderPub, message.content);
+    }
+
+    showCategory(index, composeSelected = false) {
+
+        console.log(index);
+
+        if (composeSelected) {
+            this.composeSelected = true;
+        } else {
+            // set message to active to apply active css class
+            this.categories[index].active = true;
+            this.composeSelected = false;
+
+            const type = this.categories[index].type;
+
+            if (type === 'inbox') {
+                this.requestMessages();
+            } else if (type === 'action') {
+                this.requestMessages(
+                    true
+                );
+            } else if (type === 'sent') {
+                this.requestMessages(
+                    false,
+                    false,
+                    true
+                );
+            } else if (type === 'deleted') {
+                this.requestMessages(
+                    false,
+                    true
+                );
+            }
+        }
+
+        // set the current message that appears on the right hand side
+        this.currentCategory = index;
+    }
+
+    walletListToSelectItem(walletsList: Array<any>): Array<any> {
+        const walletListImu = fromJS(walletsList);
+        const walletsSelectItem = walletListImu.map(
+            (thisWallet) => {
+                return {
+                    id: thisWallet.get('commuPub'),
+                    text: thisWallet.get('walletName')
+                };
+            }
+        );
+
+        return walletsSelectItem.toJS();
+    }
+
 
     private get disabledV(): string {
         return this._disabledV;
@@ -109,131 +313,4 @@ export class SetlMessagesComponent {
                 return item.text;
             }).join(',');
     }
-
-    requestMessages() {
-        // Create a saga pipe.
-        const asyncTaskPipe = this.myMessageService.requestOwnMessages(
-            0,
-            0,
-            this.currentWalletId,
-            0,
-            8,
-            0,
-            0,
-            0,
-            0,
-            ''
-        );
-
-        // Send a saga action.
-        // Actions to dispatch, when request success:  LOGIN_SUCCESS.
-        // Actions to dispatch, when request fail:  RESET_LOGIN_DETAIL.
-        // saga pipe function descriptor.
-        // Saga pipe function arguments.
-        this.ngRedux.dispatch(SagaHelper.runAsync(
-            [SET_MESSAGE_LIST],
-            [],
-            asyncTaskPipe, {}));
-
-        return false;
-    }
-
-
-    decrypt(mailId, walletId, bobPub, encryptedMessage) {
-        const asyncTaskPipe = this.myMessageService.decryptMessage(
-            walletId, bobPub, encryptedMessage
-        );
-
-        // Send a saga action.
-        // Actions to dispatch, when request success:  LOGIN_SUCCESS.
-        // Actions to dispatch, when request fail:  RESET_LOGIN_DETAIL.
-        // saga pipe function descriptor.
-        // Saga pipe function arguments.
-        this.ngRedux.dispatch(SagaHelper.runAsync(
-            [],
-            [],
-            asyncTaskPipe, {}, (response) => {
-                this.ngRedux.dispatch(setDecryptedContent(mailId, response));
-                return true;
-            })
-        );
-    }
-
-    updateState() {
-        const newState = this.ngRedux.getState();
-        this.messages = getMyMessagesList(newState);
-        this.currentMessage = this.messages[0];
-        this.currentCategory = 0;
-
-        this.currentWalletId = getConnectedWallet(newState);
-        this.walletDirectoryList = getWalletDirectoryList(newState);
-        this.walletWithCommuPub = this.walletListToSelectItem(this.walletDirectoryList);
-
-        this.items = [this.walletWithCommuPub];
-
-        console.log(this.currentWalletId);
-        console.log(this.walletDirectoryList);
-        console.log(this.walletWithCommuPub);
-
-        if (getNeedRunDecryptState(newState)) {
-            // this.decrypt(this.messages[0].recipientId, this.messages[0].senderPub, this.messages[0].content);
-            this.ngRedux.dispatch({type: DONE_RUN_DECRYPT});
-
-            for (const i in this.messages) {
-                const message = this.messages[i];
-
-                this.decrypt(message.mailId, message.recipientId, message.senderPub, message.content);
-
-            }
-
-        }
-
-    }
-
-    showMessage(index) {
-
-        // set message to active to apply message-active css class
-        this.messages[index].active = true;
-
-        // set the current message that appears on the right hand side
-        this.currentMessage = this.messages[index];
-
-        // set the id so that message-active an be compared to index and set
-        this.currentMessage.id = index;
-    }
-
-    showCategory(index, composeSelected) {
-
-        console.log(index);
-
-        if (composeSelected) {
-            this.composeSelected = true;
-        } else {
-            // set message to active to apply active css class
-            this.categories[index].active = true;
-
-            this.composeSelected = false;
-        }
-
-
-        // set the current message that appears on the right hand side
-        this.currentCategory = index;
-
-
-    }
-
-    walletListToSelectItem(walletsList: Array<any>): Array<any> {
-        const walletListImu = fromJS(walletsList);
-        const walletsSelectItem = walletListImu.map(
-            (thisWallet) => {
-                return {
-                    id: thisWallet.get('commuPub'),
-                    text: thisWallet.get('walletName')
-                };
-            }
-        );
-
-        return walletsSelectItem.toJS();
-    }
-
 }
