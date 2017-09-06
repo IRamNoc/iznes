b8493d00cb48d89a4f526bad53e6f7bc5890f1e8
Dan Sarracayo <dan.sarracayo@setl.io>
2017-09-06 09:48:03 +0100

    Added the ability to pass an emitter into the permissions grid, emitting on it will allow you to pass data that'll be used at init.

--- permission-grid/permission-grid.component.ts
+++ permission-grid/permission-grid.component.ts
@@ -3,6 +3,7 @@ import {
     Input,
     Output,
     OnInit,
+    AfterViewInit,
     EventEmitter
 } from '@angular/core';
 
@@ -11,15 +12,19 @@ import {
   templateUrl: './permission-grid.component.html',
   styleUrls: ['./permission-grid.component.scss']
 })
-export class PermissionGridComponent implements OnInit {
+export class PermissionGridComponent implements OnInit, AfterViewInit {
 
     /* Permission areas, each row. */
     @Input()
     public permissionAreas:Array<any> = [];
 
-    /* Permission level, eahc column. */
+    /* Permission level, each column. */
     @Input()
     public permissionLevels:Array<any> = [];
+
+    /* Init Data, the data to show. */
+    @Input()
+    public initData:EventEmitter<any>;
 
     /* Update event, the output of the data. */
     @Output()
@@ -36,6 +41,7 @@ export class PermissionGridComponent implements OnInit {
         // console.log(" |-- Init Permission Grid. ");
         // console.log(" | > permissionAreas:", this.permissionAreas);
         // console.log(" | > permissionLevels:", this.permissionLevels);
+        // console.log(" | > initData:", this.initData);
         /* Create the new data structure, this will be the two way bound data to
            the grid. */
         let i, j;
@@ -53,6 +59,35 @@ export class PermissionGridComponent implements OnInit {
             /* Done! */
         }
         // console.log(' | < setup raw data: ', this.rawComponentData);
+    }
+
+    ngAfterViewInit () {
+        /* Subscribe to the initData event. */
+        // console.log(" | init data: ", this.initData);
+        if ( this.initData ) {
+            // console.log(" | init data emitter is set.");
+            // console.log( "subscribe: ", new Date() );
+            this.initData.subscribe((data) => {
+                /* Loop over the object and set this raw data to it, then emit data again. */
+                // console.log(" | < got new init data: ", data);
+                let i, j;
+                /* Firstly loop each permission area. */
+                for ( i = 0; i < this.permissionAreas.length; i++ ) {
+                    /* Then each permission level. */
+                    for ( j = 0; j < this.permissionLevels.length; j++ ) {
+                        /* Now let's check if the permission area is in the new data. */
+                        if ( data[this.permissionAreas[i].id] ) {
+                            /* Now let's check if the permission level is in the permission area in the new data. */
+                            if (  data[this.permissionAreas[i].id][this.permissionLevels[j].id] ) {
+                                /* Set the raw data and trigger event update. */
+                                this.rawComponentData[this.permissionAreas[i].id][this.permissionLevels[j].id] = data[this.permissionAreas[i].id][this.permissionLevels[j].id];
+                            }
+                        }
+                    }
+                    /* Done! */
+                }
+            });
+        }
     }
 
     /**
