90ef649bcf9e85d167ad44ef7f35c3f9c1ac9582
ming <mingrui.huang@setl.io>
2017-09-07 09:47:47 +0100

    Add account list

--- manage-account/component.ts
+++ manage-account/component.ts
@@ -1,15 +1,35 @@
+// Vendor
 import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
+import {fromJS} from 'immutable';
+import {select, NgRedux} from '@angular-redux/store';
+
+// Internal
+import {
+    AccountsService
+} from '@setl/core-req-services';
+import {
+    setRequestedAccountList,
+    SET_ACCOUNT_LIST
+} from '@setl/core-store';
+import {AlertsService} from '@setl/jaspero-ng2-alerts';
+import {SagaHelper} from '@setl/utils';
 
 @Component({
     selector: 'app-manage-account',
-    templateUrl: 'component.html'
+    templateUrl: './component.html'
 })
 
 export class ManageAccountComponent implements OnInit {
     tabsControl: Array<object>;
+    accountList: Array<object>;
 
-    constructor() {
+    @select(['account', 'accountList', 'accountList']) accountListOb;
+    @select(['account', 'requestedAccountList']) requestedAccountListOb;
+
+    constructor(private ngRedux: NgRedux<any>,
+                private alertsService: AlertsService,
+                private accountService: AccountsService) {
         /* Default tabs. */
         this.tabsControl = [
             {
@@ -18,7 +38,7 @@ export class ManageAccountComponent implements OnInit {
                 active: true
             },
             {
-                title: '<i class="fa fa-plus"></i> Add New Member',
+                title: '<i class="fa fa-plus"></i> Add New Account',
                 userId: -1,
                 formControl: new FormGroup(
                     {
@@ -32,6 +52,38 @@ export class ManageAccountComponent implements OnInit {
                 active: false
             }
         ];
+
+        this.accountListOb.subscribe((accountList) => this.updateAccountList(accountList));
+        this.requestedAccountListOb.subscribe((requestedState) => this.requestAccountList(requestedState));
+    }
+
+    updateAccountList(accountList) {
+        this.accountList = accountList;
+
+        const accountListImu = fromJS(accountList);
+        this.accountList = accountListImu.reduce(function (result, thisAccount) {
+            result.push(thisAccount.toJS());
+            return result;
+        }, []);
+    }
+
+    requestAccountList(requestedState) {
+
+        // If the state is false, that means we need to request the list.
+        if (!requestedState) {
+            // Set the state flag to true. so we do not request it again.
+            this.ngRedux.dispatch(setRequestedAccountList());
+
+            // Request the list.
+            const asyncTaskPipe = this.accountService.requestAccountList();
+
+            this.ngRedux.dispatch(SagaHelper.runAsync(
+                [SET_ACCOUNT_LIST],
+                [],
+                asyncTaskPipe,
+                {}
+            ));
+        }
     }
 
     ngOnInit() {
