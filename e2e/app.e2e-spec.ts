6ad5cf2bc74b5b818cb8bd52199c74ba5e2a2e82
Jordan <jordan.miller@setl.io>
2017-09-11 10:51:41 +0100

    jordan : Added ID's to login page

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,20 @@
 import { ClaritySamplePage } from './app.po';
+import {browser} from 'protractor';
 
 describe('clarity-sample App', () => {
   let page: ClaritySamplePage;
 
   beforeEach(() => {
     page = new ClaritySamplePage();
+    page.navigateTo();
+    const baseUrl = 'http://localhost:4200';
+    browser.driver.get(baseUrl);
+    expect(page.getParagraphText()).toEqual('Welcome to the SETL Intranet');
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    const baseUrl = 'http://localhost:4200';
+    browser.driver.get(baseUrl);
+    page.login();
   });
 });
