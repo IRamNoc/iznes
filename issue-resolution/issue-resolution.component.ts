9edbcb9963386dc401a2a8d75c8486ee9c042bce
Dan Sarracayo <dan.sarracayo@setl.io>
2017-09-28 10:43:49 +0100

    Created the new corp actions module and components.

---
+++ issue-resolution/issue-resolution.component.ts
@@ -0,0 +1,17 @@
+/* Core/Angular imports. */
+import {Component} from '@angular/core';
+
+/* Decorator. */
+@Component({
+    styleUrls: ['./issue-resolution.component.css'],
+    templateUrl: './issue-resolution.component.html',
+})
+
+/* Class. */
+export class IssueResolutionComponent {
+
+    constructor () {
+        /* Stub. */
+    }
+
+}
