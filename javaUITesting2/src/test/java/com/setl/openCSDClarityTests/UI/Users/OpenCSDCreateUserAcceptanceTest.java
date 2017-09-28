e0545c01cae89f0087d51a45ddcfd71fbdcdf6f8
Jordan <jordan.miller@setl.io>
2017-09-28 10:26:39 +0100

    jordan: added more UI tests & Changed url to uk-lon-li-006

--- javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/Users/OpenCSDCreateUserAcceptanceTest.java
+++ javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/Users/OpenCSDCreateUserAcceptanceTest.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 
 import static com.setl.UI.common.SETLUIHelpers.AccountsDetailsHelper.*;
 import static com.setl.UI.common.SETLUIHelpers.SetUp.*;
-import static com.setl.UI.common.SETLUIHelpers.UserDetailsHelper.navigateToAddUser;
+import static com.setl.UI.common.SETLUIHelpers.UserDetailsHelper.*;
 
 @RunWith(OrderedJUnit4ClassRunner.class)
 public class OpenCSDCreateUserAcceptanceTest {
@@ -39,62 +39,22 @@ public class OpenCSDCreateUserAcceptanceTest {
         testSetUp();
         screenshotRule.setDriver(driver);
     }
-
-    public static void enterManageUserUsername(String username){
-      driver.findElement(By.id("new-user-username")).sendKeys(username);
-    }
-    public static void enterManageUserEmail(String email){
-      driver.findElement(By.id("new-user-email")).sendKeys(email);
-    }
-    public static void selectManageUserAccountDropdown(){
-      driver.findElement(By.id("new-user-account-select")).click();
-      driver.findElement(By.xpath("//*[@id=\"new-user-account-select\"]/div/ul/li[1]")).click();
-    }
-    public static void selectManageUserUserDropdown(){
-      driver.findElement(By.id("new-user-usertype-select")).click();
-      driver.findElement(By.xpath("//*[@id=\"new-user-usertype-select\"]/div/ul/li[1]")).click();
-    }
-    public static void enterManageUserPassword(String password){
-      driver.findElement(By.id("new-user-password")).sendKeys(password);
-    }
-    public static void enterManageUserPasswordRepeat(String password){
-      driver.findElement(By.id("new-user-password-repeat")).sendKeys(password);
-    }
-    public static void clickManageUserSubmit(){
-      driver.findElement(By.id("new-user-submit")).click();
-    }
-
     @Test
     public void shouldCreateUser() throws IOException, InterruptedException {
         navigateToAddUser();
-        enterManageUserUsername("TestUser5");
-        enterManageUserEmail("TestEmail@gmail.com");
-        selectManageUserAccountDropdown();
-        selectManageUserUserDropdown();
-        enterManageUserPassword("Testpass123");
-        enterManageUserPasswordRepeat("Testpass123");
+        enterAllUserDetails();
         clickManageUserSubmit();
     }
     @Test
     public void shouldCreateRandomUser() throws IOException, InterruptedException {
         navigateToAddUser();
-        enterManageUserUsername("TestUser5");
-        enterManageUserEmail("TestEmail@gmail.com");
-        selectManageUserAccountDropdown();
-        selectManageUserUserDropdown();
-        enterManageUserPassword("Testpass123");
-        enterManageUserPasswordRepeat("Testpass123");
+        enterAllUserDetails();
         clickManageUserSubmit();
     }
     @Test
     public void shouldNotCreateDuplicateUser() throws IOException, InterruptedException {
         navigateToAddUser();
-        enterManageUserUsername("TestUser5");
-        enterManageUserEmail("TestEmail@gmail.com");
-        selectManageUserAccountDropdown();
-        selectManageUserUserDropdown();
-        enterManageUserPassword("Testpass123");
-        enterManageUserPasswordRepeat("Testpass123");
+        enterAllUserDetails();
         clickManageUserSubmit();
     }
     @Test
@@ -119,86 +79,103 @@ public class OpenCSDCreateUserAcceptanceTest {
     }
     @Test
     public void shouldNotCreateUserWithoutAccountTypeSelected() throws IOException, InterruptedException {
-      navigateToAddUser();
-      enterManageUserUsername("TestUser5");
-      enterManageUserEmail("TestEmail@gmail.com");
-      selectManageUserUserDropdown();
-      enterManageUserPassword("Testpass123");
-      enterManageUserPasswordRepeat("Testpass123");
-      clickManageUserSubmit();
+        navigateToAddUser();
+        enterManageUserUsername("TestUser5");
+        enterManageUserEmail("TestEmail@gmail.com");
+        selectManageUserUserDropdown();
+        enterManageUserPassword("Testpass123");
+        enterManageUserPasswordRepeat("Testpass123");
+        clickManageUserSubmit();
     }
     @Test
     public void shouldNotCreateUserWithoutUserTypeSelected() throws IOException, InterruptedException {
-      navigateToAddUser();
-      enterManageUserUsername("TestUser5");
-      enterManageUserEmail("TestEmail@gmail.com");
-      selectManageUserAccountDropdown();
-      enterManageUserPassword("Testpass123");
-      enterManageUserPasswordRepeat("Testpass123");
-      clickManageUserSubmit();
+        navigateToAddUser();
+        enterManageUserUsername("TestUser" + Math.random());
+        enterManageUserEmail("TestEmail@gmail.com");
+        selectManageUserAccountDropdown();
+        enterManageUserPassword("Testpass123");
+        enterManageUserPasswordRepeat("Testpass123");
+        clickManageUserSubmit();
     }
     @Test
     public void shouldNotCreateUserWithoutPasswordEntered() throws IOException, InterruptedException {
-      navigateToAddUser();
-      enterManageUserUsername("TestUser5");
-      enterManageUserEmail("TestEmail@gmail.com");
-      selectManageUserAccountDropdown();
-      selectManageUserUserDropdown();
-      enterManageUserPasswordRepeat("Testpass123");
-      clickManageUserSubmit();
+        navigateToAddUser();
+        enterManageUserUsername("TestUser5");
+        enterManageUserEmail("TestEmail@gmail.com");
+        selectManageUserAccountDropdown();
+        selectManageUserUserDropdown();
+        enterManageUserPasswordRepeat("Testpass123");
+        clickManageUserSubmit();
     }
     @Test
     public void shouldNotCreateUserWithoutPasswordRepeated() throws IOException, InterruptedException {
-      navigateToAddUser();
-      enterManageUserUsername("TestUser5");
-      enterManageUserEmail("TestEmail@gmail.com");
-      selectManageUserAccountDropdown();
-      selectManageUserUserDropdown();
-      enterManageUserPassword("Testpass123");
-      clickManageUserSubmit();
+        navigateToAddUser();
+        enterManageUserUsername("TestUser5");
+        enterManageUserEmail("TestEmail@gmail.com");
+        selectManageUserAccountDropdown();
+        selectManageUserUserDropdown();
+        enterManageUserPassword("Testpass123");
+        clickManageUserSubmit();
     }
     @Test
     public void shouldEditUserWhenEditButtonIsClicked() throws IOException, InterruptedException {
-      navigateToAddUser();
-      driver.findElement(By.id("user-tab-0")).click();
-      driver.findElement(By.id("edit-1")).click();
-      driver.findElement(By.id("user-tab-2")).isDisplayed();
-      driver.findElement(By.xpath("//*[@id=\"edit-user2-form\"]/div/div[5]/div/button[2]")).click();
+        navigateToAddUser();
+        navigateToUserSearch();
+        navigateToEditUsers();
+        closeUserDetails();
     }
     @Test
     public void shouldEditEmail() throws IOException, InterruptedException {
-      navigateToAddUser();
-      driver.findElement(By.id("user-tab-0")).click();
-      driver.findElement(By.id("edit-1")).click();
-      driver.findElement(By.id("user-tab-2")).isDisplayed();
-      driver.findElement(By.id("edit-user2-email")).clear();
-      driver.findElement(By.id("edit-user2-email")).sendKeys("test@test.com");
-      driver.findElement(By.xpath("//*[@id=\"edit-user2-form\"]/div/div[5]/div/button[1]")).click();
+        navigateToAddUser();
+        navigateToUserSearch();
+        navigateToEditUsers();
+        editUserEmail("test@setl.io");
+        saveUserDetails();
     }
     @Test
     public void shouldResetEmail() throws IOException, InterruptedException {
-      navigateToAddUser();
-      driver.findElement(By.id("user-tab-0")).click();
-      driver.findElement(By.id("edit-1")).click();
-      driver.findElement(By.id("user-tab-2")).isDisplayed();
-      driver.findElement(By.id("edit-user2-email")).clear();
-      driver.findElement(By.id("edit-user2-email")).sendKeys("anthony.culligan@setl.io");
-      driver.findElement(By.xpath("//*[@id=\"edit-user2-form\"]/div/div[5]/div/button[1]")).click();
+        navigateToAddUser();
+        navigateToUserSearch();
+        navigateToEditUsers();
+        editUserEmail("anthony.culligan@setl.io");
+        saveUserDetails();
+    }
+    @Test
+    public void shouldChangeUserPermission() throws IOException, InterruptedException {
+        navigateToAddUser();
+        navigateToUserSearch();
+    }
+    @Test
+    public void shouldCreateUserWithPermissionAdministrative() throws IOException, InterruptedException {
+        navigateToAddUser();
+        enterAllUserDetails();
+    }
+    @Test
+    public void shouldCreateUserWithPermissionTransactional() throws IOException, InterruptedException {
+        navigateToAddUser();
+        enterAllUserDetails();
+    }
+    @Test
+    public void shouldCreateUserWithPermissionWallets() throws IOException, InterruptedException {
+        navigateToAddUser();
+        enterAllUserDetails();
+    }
+    @Test
+    public void shouldCreateUserWithPermissionChain() throws IOException, InterruptedException {
+        navigateToAddUser();
+        enterAllUserDetails();
+    }
+    @Test
+    public void shouldDeleteUser() throws IOException, InterruptedException {
+        navigateToAddUser();
+        navigateToUserSearch();
+        navigateToPage5();
+        clickDeleteUser("102");
     }
 
     @Test
-    public void shouldDeleteUser() throws IOException, InterruptedException {
-      navigateToAddUser();
-      driver.findElement(By.id("user-tab-0")).click();
-      navigateToPage5();
-      Thread.sleep(5000);
-      driver.findElement(By.id("delete-102")).click();
-    }
-
-    public static void navigateToPage5(){
-      driver.findElement(By.className("pagination-next")).click();
-      driver.findElement(By.className("pagination-next")).click();
-      driver.findElement(By.className("pagination-next")).click();
-      driver.findElement(By.className("pagination-next")).click();
+    public void shouldNotDeleteUser() throws IOException, InterruptedException{
+        navigateToAddUser();
+        navigateToUserSearch();
     }
 }
