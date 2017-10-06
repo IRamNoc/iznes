ab773ef8b6ebf61f7f962411e608060e82288751
ming <mingrui.huang@setl.io>
2017-10-06 15:26:33 +0100

    Subscription form and share characteristic helper

---
+++ ofi-fund-invest/common-service/model.ts
@@ -0,0 +1,25 @@
+export interface FundCharacteristic {
+    sCutoffDate: number;
+    sCutoffTime: string;
+    rCutoffDate: number;
+    rCutoffTime: string;
+    sValuationDate: number;
+    sValuationTime: string;
+    rValuationDate: number;
+    rValuationTime: string;
+    sSettlementDate: number;
+    sSettlementTime: string;
+    rSettlementDate: number;
+    rSettlementTime: string;
+    knownNav: boolean;
+    entryFee: number;
+    sAcquiredFee: number;
+    exitFee: number;
+    rAcquiredFee: number;
+    platformFee: number;
+    sAllowType: string;
+    rAllowType: string;
+    decimalisation: number;
+    sMinValue: number;
+    sMinUnit: number;
+}
