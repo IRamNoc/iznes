4e5c8daf17cd7ce799992a071b731e3a41b18ab3
Dan Sarracayo <dan.sarracayo@setl.io>
2017-10-09 15:57:46 +0100

    Tidided up some code.

--- multilingual.module.ts
+++ multilingual.module.ts
@@ -7,10 +7,11 @@ import {MultilingualDirective} from './multilingual.directive';
 /* Service. */
 import {MultilingualService} from './multilingual.service';
 
+/* Module declaration. */
 @NgModule({
     declarations: [MultilingualDirective],
     imports: [],
     exports: [MultilingualDirective],
     providers: [MultilingualService]
 })
-export class MultilingualModule {}
+export class MultilingualModule {  }
