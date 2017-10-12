4b9b85a2230e50244a88f741a7f937a85547c71b
Dan Sarracayo <dan.sarracayo@setl.io>
2017-10-12 14:15:59 +0100

    Added some translations.

--- translations.ts
+++ translations.ts
@@ -1,15 +1,44 @@
 export const Translations = {
     "core": {
         "eng": {
-            "txt_home": "Home"
+            "txt_home": "Home",
+            "txt_registrar": "Registrar",
+            "txt_fund_share": "Fund Share",
+            "txt_isin_code": "ISIN Code",
+            "txt_currency": "Currency",
+            "txt_quantity": "Quanitity",
+            "txt_net_asset_value": "Net Asset Value",
+            "txt_cut-off_date": "Cut-Off Date",
+            "txt_date": "Date",
+            "txt_time_utc": "Time (UTC)",
+            "txt_valuation_date": "Valuation Date",
+            "txt_settlement_date": "Settlement Date",
+            "txt_entry_fee_percent": "Entry Fee",
+            "txt_comment": "Comment",
+            "txt_gross_amount": "Gross Amount",
         },
         "fra": {
-            "txt_home": "Accueil"
+            "txt_home": "Accueil",
+            "txt_registrar": "Teneur de registre",
+            "txt_fund_share": "Part de fonds",
+            "txt_isin_code": "Code ISIN",
+            "txt_currency": "Devise",
+            "txt_quantity": "Quantité",
+            "txt_net_asset_value": "Valeur liquidative",
+            "txt_cut-off_date": "Date de centralisation",
+            "txt_date": "Date",
+            "txt_time_utc": "Temps (UTC)",
+            "txt_valuation_date": "Date de cours",
+            "txt_settlement_date": "Date de règlement",
+            "txt_entry_fee_percent": "Droits d’entrée",
+            "txt_comment": "Commentaire",
+            "txt_gross_amount": "Gross Amount",
         },
         "esp": {
             "txt_home": "Casas"
         }
     },
+    /* These OFI entries override any above. */
     "ofi": {
         "eng": {
             "txt_home": "Home"
