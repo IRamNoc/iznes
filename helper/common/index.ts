75b85d0ef0c624a4dacf29d7c51b08efdcb47c2e
ming <mingrui.huang@setl.io>
2017-10-16 16:01:35 +0100

    Capitialisation pipe

--- helper/common/index.ts
+++ helper/common/index.ts
@@ -1,3 +1,22 @@
+/**
+ * capitialise first letter.
+ * @param string
+ * @return {string}
+ */
 export function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ *  padding number from left.
+ *
+ * @param num
+ * @param width
+ * @param fill
+ * @return {string}
+ */
+export function pad(num: number, width: number, fill: string): string {
+    const numberStr = num.toString();
+    const len = numberStr.length;
+    return len >= width ? numberStr : new Array(width - len + 1).join(fill) + numberStr;
 }
--- pipes/index.ts
+++ pipes/index.ts
@@ -88,18 +88,32 @@ export class MoneyValueOfiPipe implements PipeTransform {
     }
 }
 
+@Pipe({name: 'capitalize'})
+export class CapitalizePipe implements PipeTransform {
+
+    transform(value: string) {
+        if (value) {
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        }
+        return value;
+    }
+
+}
+
 @NgModule({
     declarations: [
         TruncatePipe,
         AssetPipe,
         MoneyValuePipe,
-        MoneyValueOfiPipe
+        MoneyValueOfiPipe,
+        CapitalizePipe
     ],
     exports: [
         TruncatePipe,
         AssetPipe,
         MoneyValuePipe,
-        MoneyValueOfiPipe
+        MoneyValueOfiPipe,
+        CapitalizePipe
     ]
 })
 
