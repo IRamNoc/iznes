5c38020dadf325137d04dcf1d5b5b8fb1e5c3d9f
ming <mingrui.huang@setl.io>
2017-10-23 09:25:32 +0100

    html mock up

--- ofi-report-module/module.ts
+++ ofi-report-module/module.ts
@@ -1,10 +1,28 @@
+// vendor imports
 import {NgModule} from '@angular/core';
+import {ClarityModule} from 'clarity-angular';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DpDatePickerModule} from '@setl/utils';
+
+// Local components
 import {OfiTaxReportComponent} from './ofi-tax-report/component';
 import {OfiPnlReportComponent} from './ofi-pnl-report/component';
+import {SelectModule, SetlPipesModule, SetlComponentsModule, SetlDirectivesModule} from '@setl/utils';
 
 
 @NgModule({
-    imports: [],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        ClarityModule,
+        CommonModule,
+        SelectModule,
+        SetlPipesModule,
+        DpDatePickerModule,
+        SetlComponentsModule,
+        SetlDirectivesModule
+    ],
     exports: [],
     declarations: [OfiTaxReportComponent, OfiPnlReportComponent],
     providers: [],
