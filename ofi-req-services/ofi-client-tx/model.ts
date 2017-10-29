4b5357c43075e5bf6ad06b8c666d0a7e76f91d39
ming <mingrui.huang@setl.io>
2017-10-29 14:51:55 +0000

    Client tx service. request tx

---
+++ ofi-req-services/ofi-client-tx/model.ts
@@ -0,0 +1,11 @@
+import {MemberNodeMessageBody} from '@setl/utils/common';
+
+
+export interface RequestClientTxRequestBody extends MemberNodeMessageBody {
+    token: string;
+    walletId: number;
+    shareName: string;
+}
+
+
+
