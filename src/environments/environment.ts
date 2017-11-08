a913ba72ee10283c0006092be0ce3e4ee26c1d6d
Dan Sarracayo <dan.sarracayo@setl.io>
2017-11-08 15:44:44 +0000

    Fixed local conflicts.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ import {menuSpec} from './menuSpec';
 export const environment = {
     production: false,
     MEMBER_NODE_CONNECTION: {
+        // host: 'devnet.setl.io',
         // host: 'uk-lon-li-006.opencsd.io',
         host: window.location.hostname,
         // host: '10.0.1.106', // Ming's Socket Server
