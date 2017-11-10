64496239f93acfe347a8722f8b0de28f9b7f04ed
Ollie Kett <ollie.kett@setl.io>
2017-11-10 10:46:25 +0000

    Menu Redux

--- user/site-settings/actions.ts
+++ user/site-settings/actions.ts
@@ -6,15 +6,26 @@ import {
 import {name} from './__init__';
 
 export const SET_LANGUAGE = `${name}/SET_LANGUAGE`;
-
+export const SET_MENU_SHOWN = `${name}/SET_MENU_SHOWN`;
 
 export interface SetLanguageAction extends Action {
     language: string;
+}
+
+export interface SetMenuShownAction extends Action {
+    menuShown: boolean;
 }
 
 export const setLanguage: ActionCreator<SetLanguageAction> =
     (language) => ({
         type: SET_LANGUAGE,
         language: language,
+    });
+
+
+export const setMenuShown: ActionCreator<SetMenuShownAction> =
+    (menuShown) => ({
+        type: SET_MENU_SHOWN,
+        menuShown: menuShown,
     });
 
