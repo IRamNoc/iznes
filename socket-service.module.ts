1a73500cbd06329446edc864d670e32d985da550
ming <mingrui.huang@setl.io>
2017-11-28 10:29:55 +0000

    Fix Typescript error allow build to run

--- socket-service.module.ts
+++ socket-service.module.ts
@@ -1,19 +1,13 @@
 import {NgModule} from "@angular/core";
 import {ToasterModule, ToasterService} from 'angular2-toaster';
-import {SocketClusterWrapper} from '@setl/socketcluster-wrapper';
-import {MemberSocketService} from './member-socket.service';
 import {WalletNodeSocketService} from './wallet-node-socket.service';
 
 @NgModule({
     declarations: [],
     imports: [
-        ToasterModule,
-        SocketClusterWrapper
+        ToasterModule
     ],
-    exports: [
-        MemberSocketService,
-        WalletNodeSocketService
-    ],
+    exports: [],
     providers: [
         ToasterService
     ]
