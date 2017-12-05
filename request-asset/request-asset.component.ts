a2078dde3bb85d316437d9f5e81660e812268e03
Phil Zeelte <philzeelte@IM00152.local>
2017-12-05 15:58:36 +0000

    added asset and recipient fields

--- request-asset/request-asset.component.ts
+++ request-asset/request-asset.component.ts
@@ -9,12 +9,14 @@ import {
     MyWalletsService
 } from '@setl/core-req-services';
 import {
-    getConnectedWallet
+    getConnectedWallet,
+    setRequestedWalletToRelationship
 } from '@setl/core-store';
 import {AlertsService} from '@setl/jaspero-ng2-alerts';
 import {Unsubscribe} from 'redux';
 import _ from 'lodash';
 import {Subscription} from 'rxjs/Subscription';
+import {Observable} from 'rxjs';
 
 @Component({
     selector: 'app-request-asset',
@@ -29,8 +31,24 @@ export class RequestAssetComponent implements OnInit, OnDestroy {
 
     connectedWalletId: number;
 
+    allInstrumentList: Array<any>;
+
+    walletAddressList: Array<any>;
+    walletRelationshipType: Array<any>;
+
+    // Wallet ID
+    @select(['wallet', 'connected', 'connectedWallet']) walletIdOb: Observable<any>;
+
     // Asset
-    
+    @select(['asset', 'allInstruments', 'requested']) requestedAllInstrumentOb: Observable<boolean>;
+    @select(['asset', 'allInstruments', 'instrumentList']) allInstrumentOb: Observable<any>;
+
+    // Wallet
+    @select(['wallet', 'myWalletAddress', 'requestedAddressList']) requestedWalletAddressListOb: Observable<any>;
+    @select(['wallet', 'myWalletAddress', 'requestedLabel']) requestedWalletAddressLabelsOb: Observable<any>;
+    @select(['wallet', 'myWalletAddress', 'addressList']) walletAddressListOb: Observable<any>;
+    @select(['wallet', 'walletRelationship', 'requestedToRelationship']) requestedToRelationshipOb: Observable<any>;
+    @select(['wallet', 'walletRelationship', 'toRelationshipList']) walletRelationshipOb: Observable<any>;
 
     // Redux unsubscription
     reduxUnsubscribe: Unsubscribe;
@@ -43,15 +61,60 @@ export class RequestAssetComponent implements OnInit, OnDestroy {
 
         /* send asset form */
         this.requestAssetForm = new FormGroup({
-            asset: new FormControl('', Validators.required)
+            asset: new FormControl('', Validators.required),
+            recipient: new FormControl('', Validators.required)
         });
+
+        /* data subscriptions */
+        this.initWalletIdSubscription();
+        this.initAssetSubscriptions();
+        this.initWalletRelationshipSubscriptions();
     }
 
     ngOnInit() { }
 
-    ngOnDestroy() {
-        this.reduxUnsubscribe();
+    private initWalletIdSubscription(): void {
+        this.subscriptionsArray.push(
+            this.walletIdOb.subscribe((walletId: number) => {
+                this.connectedWalletId = 6;
+            })
+        );
+    }
 
+    private initAssetSubscriptions(): void {
+        this.subscriptionsArray.push(
+            this.requestedAllInstrumentOb.subscribe((requested) => {
+                if(!requested) InitialisationService.requestAllInstruments(this.ngRedux, this.walletNodeRequestService);
+            }),
+            this.allInstrumentOb.subscribe((instrumentList) => {
+                this.allInstrumentList = walletHelper.walletInstrumentListToSelectItem(instrumentList);
+            })
+        );
+    }
+
+    private initWalletRelationshipSubscriptions(): void {        
+        this.subscriptionsArray.push(
+            this.requestedToRelationshipOb.subscribe((requested) => {
+                if(!requested) InitialisationService.requestToRelationship(this.ngRedux, this.myWalletService, this.connectedWalletId);
+            }),
+            this.requestedWalletAddressListOb.subscribe((requested) => {
+                if(!requested) InitialisationService.requestWalletAddresses(this.ngRedux, this.walletNodeRequestService, this.connectedWalletId);
+            }),
+            this.requestedWalletAddressLabelsOb.subscribe((requested) => {
+                if(!requested && this.connectedWalletId !== 0) {
+                    MyWalletsService.defaultRequestWalletLabel(this.ngRedux, this.myWalletService, this.connectedWalletId);
+                }
+            }),
+            this.walletAddressListOb.subscribe((walletAddressList) => {
+                this.walletAddressList = walletHelper.walletAddressListToSelectItem(walletAddressList, true);
+            }),
+            this.walletRelationshipOb.subscribe((walletRelationships) => {
+                this.walletRelationshipType = walletHelper.walletToRelationshipToSelectItem(walletRelationships, this.walletAddressList);
+            })
+        );
+    }
+
+    ngOnDestroy() {
         for(const subscription of this.subscriptionsArray) {
             subscription.unsubscribe();
         }
