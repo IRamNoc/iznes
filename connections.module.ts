67bdb30786d360c197a93e2411681a60fe30e9e6
David Duong <david.duong@setl.io>
2017-12-06 14:50:29 +0000

    feat(connections.module): create the connection's module

---
+++ connections.module.ts
@@ -0,0 +1,41 @@
+/* Core imports. */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+/* Clarity module. */
+import { ClarityModule } from 'clarity-angular';
+
+/* 3rd party modules. */
+import { SelectModule } from '@setl/utils';
+
+/* Multilingual coolness. */
+import { MultilingualModule } from '@setl/multilingual';
+
+/* Connection component */
+import { ConnectionComponent } from './connections/connection.component';
+import { ConnectionsService } from './connections.service';
+
+@NgModule({
+    declarations: [
+        ConnectionComponent
+    ],
+    exports: [
+        ConnectionComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        FormsModule,
+        ClarityModule,
+        SelectModule,
+        MultilingualModule
+    ],
+    providers: [ConnectionsService]
+})
+
+export class ConnectionsModule {
+
+}
