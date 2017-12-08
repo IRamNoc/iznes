718adccc2aed397c7e9240fd5b8a9fbd50d80a4c
David Duong <david.duong@setl.io>
2017-12-08 15:30:35 +0000

    feat(my-connections/index): import/export the "DELETE_CONNECTION" action

--- connection/my-connections/index.ts
+++ connection/my-connections/index.ts
@@ -1,4 +1,4 @@
-export { name } from './__init__';
-export { MyConnectionReducer } from './reducer';
-export { MyConnectionState } from './model';
-export { GET_CONNECTIONS, CREATE_CONNECTION, SET_REQUESTED_CONNECTIONS } from './actions';
+export {name} from './__init__';
+export {MyConnectionReducer} from './reducer';
+export {MyConnectionState} from './model';
+export {GET_CONNECTIONS, CREATE_CONNECTION, SET_REQUESTED_CONNECTIONS, DELETE_CONNECTION} from './actions';
