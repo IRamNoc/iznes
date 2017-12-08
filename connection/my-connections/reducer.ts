3912b7df54d059116f42a9ced7a3a7119a7dffc9
David Duong <david.duong@setl.io>
2017-12-08 09:23:01 +0000

    feat(my-connections/reducer): implement the new actions into reducer

--- connection/my-connections/reducer.ts
+++ connection/my-connections/reducer.ts
@@ -3,20 +3,73 @@ import * as ConnectionActions from './actions';
 import { MyConnectionState } from './model';
 
 const initialState: MyConnectionState = {
+    requestedConnections: false,
     connectionList: []
 };
 
-const handleCreateConnection = (state, action) => {
-    console.log('handleCreateConnection : ', state, action.payload);
-};
+/**
+ * Get existing connections
+ * @param state
+ * @param action
+ */
+function handleGetConnections(state, action) {
+    let connectionList = [];
+    const response = action.payload[1].Data;
+
+    response.map((connection) => {
+        const data = {
+            leiId: connection.LeiID,
+            leiSender: connection.LeiSender,
+            keyDetail: connection.keyDetail,
+            connectionId: connection.connectionID,
+            status: connection.status === '1',
+        };
+
+        connectionList.push(data);
+    });
+
+    return Object.assign({}, state, { connectionList });
+}
+
+/**
+ * Set the "requestedConnections" flag to true
+ * @param state
+ * @returns {{} & any & {requestedConnections: boolean}}
+ */
+function handleSetRequestedConnections(state) {
+    const requestedConnections = true;
+
+    return Object.assign({}, state, { requestedConnections });
+}
+
+/**
+ * Create a new connection
+ *
+ * @param state
+ * @param action
+ * @returns {{} & any & {walletId: any; connectionId: *}}
+ */
+function handleCreateConnection(state, action) {
+    const response = action.payload[1].Data[0];
+
+    const connection = {
+        walletId: response.leiId,
+        connectionId: response.leiSender,
+    };
+
+    return Object.assign({}, state, connection);
+}
 
 export const MyConnectionReducer = (state: MyConnectionState = initialState, action: AsyncTaskResponseAction) => {
     switch (action.type) {
         case ConnectionActions.GET_CONNECTIONS:
-            return state;
+            return handleGetConnections(state, action);
 
         case ConnectionActions.CREATE_CONNECTION:
-            return this.handleCreateConnection(state, action);
+            return handleCreateConnection(state, action);
+
+        case ConnectionActions.SET_REQUESTED_CONNECTIONS:
+            return handleSetRequestedConnections(state);
 
         default:
             return state;
