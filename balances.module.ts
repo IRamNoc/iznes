cfe52761a7469183a8fb4a1efa6795171b25bc36
Phil Zeelte <philzeelte@IM00152.local>
2017-12-14 13:45:40 +0000

    added new component to balances module

--- balances.module.ts
+++ balances.module.ts
@@ -2,6 +2,7 @@ import {NgModule, Pipe, PipeTransform} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SetlBalancesComponent} from './balances/balances.component';
 import {SetlIssueComponent} from './issue/issue.component';
+import {SetlTransactionsComponent} from './transactions/transactions.component';
 
 /* Clarity module. */
 import {ClarityModule} from 'clarity-angular';
@@ -19,10 +20,12 @@ import {MultilingualModule} from '@setl/multilingual';
     declarations: [
         SetlBalancesComponent,
         SetlIssueComponent,
+        SetlTransactionsComponent
     ],
     exports: [
         SetlBalancesComponent,
-        SetlIssueComponent
+        SetlIssueComponent,
+        SetlTransactionsComponent
     ]
 })
 export class SetlBalancesModule {
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export {SetlBalancesModule} from './balances.module';
 export {SetlBalancesComponent} from './balances/balances.component';
 export {SetlIssueComponent} from './issue/issue.component';
+export {SetlTransactionsComponent} from './transactions/transactions.component';
