650746c777fa243696aa4523400b97141a02ebcc
Jordan <jordan.miller@setl.io>
2017-12-19 11:33:19 +0000

    jordan: commenting out api tests

--- javaUITesting2/src/test/java/com/setl/RESTAPITests/User/Login/loginTest.java
+++ javaUITesting2/src/test/java/com/setl/RESTAPITests/User/Login/loginTest.java
@@ -62,25 +62,25 @@ public class loginTest {
 
     }
 
-    @Test
-    @Ignore
-    public void loginFailureWithInvalidCredentials() throws InterruptedException, ExecutionException {
-        SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alx01");
-        SetlSocketClusterClient ws = new SetlSocketClusterClient(socket);
-        CountDownLatch l  = new CountDownLatch(1);
-
-        socket.registerHandler(Type.Login.name(),message->{
-            JSONArray data = (JSONArray) message.get("Data");
-            JSONObject resp =(JSONObject) data.get(0);
-            Object token = resp.get("Token");
-            assertTrue(token.toString().equalsIgnoreCase("fail"));
-            l.countDown();
-            return "";});
-        Future<Connection> connection = ws.start(address);
-        l.await();
-      connection.get().disconnect();
-
-    }
+//    @Test
+//    @Ignore
+//    public void loginFailureWithInvalidCredentials() throws InterruptedException, ExecutionException {
+//        SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alx01");
+//        SetlSocketClusterClient ws = new SetlSocketClusterClient(socket);
+//        CountDownLatch l  = new CountDownLatch(1);
+//
+//        socket.registerHandler(Type.Login.name(),message->{
+//            JSONArray data = (JSONArray) message.get("Data");
+//            JSONObject resp =(JSONObject) data.get(0);
+//            Object token = resp.get("Token");
+//            assertTrue(token.toString().equalsIgnoreCase("fail"));
+//            l.countDown();
+//            return "";});
+//        Future<Connection> connection = ws.start(address);
+//        l.await();
+//      connection.get().disconnect();
+//
+//    }
 
     @Test
     @Ignore
