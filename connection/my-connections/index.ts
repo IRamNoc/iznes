d85a0ae9fe7f955ed1036d7fe14b6c8766e4c142
David Duong <david.duong@setl.io>
2017-12-22 09:59:09 +0100

    refact(my-connections): import new actions from my-connections actions

--- connection/my-connections/index.ts
+++ connection/my-connections/index.ts
@@ -1,4 +1,4 @@
 export {name} from './__init__';
 export {MyConnectionReducer} from './reducer';
 export {MyConnectionState} from './model';
-export {GET_CONNECTIONS, CREATE_CONNECTION, SET_REQUESTED_CONNECTIONS, DELETE_CONNECTION} from './actions';
+export {SET_CONNECTIONS_LIST, SET_REQUESTED_CONNECTIONS, CLEAR_REQUESTED_CONNECTIONS} from './actions';
