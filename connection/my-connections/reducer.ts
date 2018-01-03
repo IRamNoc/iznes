bc99532cc61018e4ea6d51a9c0711969498bf884
David Duong <david.duong@setl.io>
2018-01-03 12:40:15 +0100

    feat(my-connection/reducer): handle new actions into connection reducer

--- connection/my-connections/reducer.ts
+++ connection/my-connections/reducer.ts
@@ -3,18 +3,20 @@ import * as ConnectionActions from './actions';
 import {MyConnectionState} from './model';
 
 const initialState: MyConnectionState = {
-    requestedConnections: false,
-    connectionList: []
+    requestedFromConnectionList: false,
+    requestedToConnectionList: false,
+    fromConnectionList: [],
+    toConnectionList: [],
 };
 
 /**
- * Get existing connections
+ * Get the "from" connections
  * @param state
  * @param action
  */
-function handleSetConnectionList(state, action) {
-    let connectionList = [];
+function handleSetFromConnectionList(state, action) {
     const response = action.payload[1].Data;
+    let fromConnectionList = [];
 
     response.map((connection) => {
         const data = {
@@ -22,49 +24,108 @@ function handleSetConnectionList(state, action) {
             leiSender: connection.LeiSender,
             keyDetail: connection.keyDetail,
             connectionId: connection.connectionID,
-            status: connection.status === '1',
+            status: connection.status,
         };
 
-        connectionList.push(data);
+        fromConnectionList.push(data);
     });
 
-    return Object.assign({}, state, {connectionList});
+    return Object.assign({}, state, { fromConnectionList });
 }
 
 /**
- * Set the "requestedConnections" flag to true
+ * Get the "to" connections
  * @param state
- * @returns {{} & any & {requestedConnections: boolean}}
+ * @param action
  */
-function handleSetRequestedConnections(state) {
-    const requestedConnections = true;
+function handleSetToConnectionList(state, action) {
+    const response = action.payload[1].Data;
+    let toConnectionList = [];
 
-    return Object.assign({}, state, {requestedConnections});
+    console.log('=========================================');
+    console.log('handle set to connection list: ', response);
+    console.log('=========================================');
+
+    response.map((connection) => {
+        const data = {
+            leiId: connection.LeiID,
+            leiSender: connection.LeiSender,
+            keyDetail: connection.keyDetail,
+            connectionId: connection.connectionID,
+            status: connection.status,
+        };
+
+        toConnectionList.push(data);
+    });
+
+    return Object.assign({}, state, { toConnectionList });
 }
 
 /**
- * Set the "requestedConnections" flag to false
+ * Set the "requestedFromConnectionList" flag to true
+ * @param state
+ * @returns {{} & any & {requestedFromConnectionList: boolean}}
+ */
+function handleSetRequestedFromConnections(state) {
+    const requestedFromConnectionList = true;
+
+    return Object.assign({}, state, { requestedFromConnectionList });
+}
+
+/**
+ * Set the "requestedFromConnectionList" flag to false
  *
  * @param state
- * @returns {{} & any & {requestedConnections: boolean}}
+ * @returns {{} & any & {requestedFromConnectionList: boolean}}
  */
-function handleClearRequestedConnections(state) {
-    const requestedConnections = false;
+function handleClearRequestedFromConnections(state) {
+    const requestedFromConnectionList = false;
 
-    return Object.assign({}, state, {requestedConnections});
+    return Object.assign({}, state, { requestedFromConnectionList });
 }
 
-//
+/**
+ * Set the "requestedToConnectionList" flag to true
+ * @param state
+ * @returns {{} & any & {requestedToConnectionList: boolean}}
+ */
+function handleSetRequestedToConnections(state) {
+    const requestedToConnectionList = true;
+
+    return Object.assign({}, state, { requestedToConnectionList });
+}
+
+/**
+ * Set the "requestedToConnectionList" flag to false
+ *
+ * @param state
+ * @returns {{} & any & {requestedToConnectionList: boolean}}
+ */
+function handleClearRequestedToConnections(state) {
+    const requestedToConnectionList = false;
+
+    return Object.assign({}, state, { requestedToConnectionList });
+}
+
 export const MyConnectionReducer = (state: MyConnectionState = initialState, action: AsyncTaskResponseAction) => {
     switch (action.type) {
-        case ConnectionActions.SET_CONNECTIONS_LIST:
-            return handleSetConnectionList(state, action);
+        case ConnectionActions.SET_FROM_CONNECTION_LIST:
+            return handleSetFromConnectionList(state, action);
 
-        case ConnectionActions.SET_REQUESTED_CONNECTIONS:
-            return handleSetRequestedConnections(state);
+        case ConnectionActions.SET_TO_CONNECTION_LIST:
+            return handleSetToConnectionList(state, action);
 
-        case ConnectionActions.CLEAR_REQUESTED_CONNECTIONS:
-            return handleClearRequestedConnections(state);
+        case ConnectionActions.SET_REQUESTED_FROM_CONNECTIONS:
+            return handleSetRequestedFromConnections(state);
+
+        case ConnectionActions.CLEAR_REQUESTED_FROM_CONNECTIONS:
+            return handleClearRequestedFromConnections(state);
+
+        case ConnectionActions.SET_REQUESTED_TO_CONNECTIONS:
+            return handleSetRequestedToConnections(state);
+
+        case ConnectionActions.CLEAR_REQUESTED_TO_CONNECTIONS:
+            return handleClearRequestedToConnections(state);
 
         default:
             return state;
