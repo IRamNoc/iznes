258ce02e5d932da94a313725c28a9dd313191367
David Duong <david.duong@setl.io>
2018-01-03 12:30:06 +0100

    feat(connection.service.model): add an interface for the from and to connections

--- connection/connection.service.model.ts
+++ connection/connection.service.model.ts
@@ -1,8 +1,13 @@
 import {MemberNodeMessageBody} from '@setl/utils/common';
 
-export interface GetConnectionsMessageBody extends MemberNodeMessageBody {
+export interface GetFromConnectionMessageBody extends MemberNodeMessageBody {
     token: string;
     leiId: string;
+}
+
+export interface GetToConnectionMessageBody extends MemberNodeMessageBody {
+    token: string;
+    senderLei: string;
 }
 
 export interface CreateConnectionMessageBody extends MemberNodeMessageBody {
