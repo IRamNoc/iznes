644a67f96d8a9d7a63b31a3858ef61c223b7413b
Stuart Fyfe <stuart.fyfe@setl.io>
2018-01-04 12:47:13 +0000

    Refactor: Extract WalletIssuerDetail interface
    Extracted WalletIssuerDetail interface from the MyIssuersState interface
    so it can be used on it's own elsewhere in the project.

--- assets/my-issuers/index.ts
+++ assets/my-issuers/index.ts
@@ -1,6 +1,6 @@
 export {name} from './__init__';
 export {MyIssuersReducer} from './reducer';
-export {MyIssuersState} from './model';
+export {MyIssuersState, WalletIssuerDetail} from './model';
 export {
     REGISTER_ISSUER_SUCCESS, REGISTER_ISSUER_FAIL,
     finishRegisterIssuerNotification,
