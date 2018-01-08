242ce93a53538836cb1b90672ff4af0ec3292378
Jordan <jordan.miller@setl.io>
2018-01-08 10:20:50 +0000

    jordan: unignoring db test

--- javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/Accounts/OpenCSDAccountsAcceptanceTest.java
+++ javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/Accounts/OpenCSDAccountsAcceptanceTest.java
@@ -84,7 +84,6 @@ public class OpenCSDAccountsAcceptanceTest {
     myAccountSendKeys("DisplayName", "Testing");
     clickMyAccountSubmit();
   }
-  @Ignore
   @Test
   //Looks like theres a problem connecting to the database, ignored for now
   public void shouldEditFirstname() throws IOException, InterruptedException, SQLException {
