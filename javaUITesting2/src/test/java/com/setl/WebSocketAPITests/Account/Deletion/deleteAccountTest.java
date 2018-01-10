4d014af6f1bfe8af85d2cf8f6805cc4170995749
bill mackie <bill.mackie@setl.io>
2018-01-10 09:01:45 +0000

    Bill : Add Jenkinsfile for ATs. Updated WebSocket API Tests

--- javaUITesting2/src/test/java/com/setl/WebSocketAPITests/Account/Deletion/deleteAccountTest.java
+++ javaUITesting2/src/test/java/com/setl/WebSocketAPITests/Account/Deletion/deleteAccountTest.java
@@ -11,6 +11,7 @@ import io.setl.wsclient.socketsrv.MessageFactory;
 import io.setl.wsclient.socketsrv.SocketServerEndpoint;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,42 +19,58 @@ import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 
 import static SETLAPIHelpers.WebSocketAPI.AccountHelper.createAccount;
 import static SETLAPIHelpers.WebSocketAPI.AccountHelper.deleteAccount;
 import static SETLAPIHelpers.WebSocketAPI.LoginHelper.login;
+import static junit.framework.Assert.fail;
 
 
 @RunWith(OrderedJUnit4ClassRunner.class)
 public class deleteAccountTest {
 
     private static final Logger logger = LogManager.getLogger(deleteAccountTest.class);
+    static ExecutorService executor  = Executors.newSingleThreadExecutor();
 
-  @Rule
-  public Timeout globalTimeout = Timeout.millis(30000);;
+    @AfterClass
+    public static void stop(){
+        executor.shutdown();;
+    }
 
-  KeyHolder holder = new KeyHolder();
-  MessageFactory factory = new MessageFactory(holder);
-  SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alex01");
-  String localAddress = "ws://uk-lon-li-006.opencsd.io:9788/db/";
+
+    KeyHolder holder = new KeyHolder();
+    MessageFactory factory = new MessageFactory(holder);
+    SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alex01");
+    String localAddress = "ws://uk-lon-li-006.opencsd.io:9788/db/";
+
+
+    private void runTest(Runnable r){
+        Connection connection = login(socket, localAddress, LoginHelper::loginResponse);
+        for (int i=0;i<3;i++)
+            try {
+                executor.submit(r).get(30, TimeUnit.SECONDS);
+                break;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        connection.disconnect();
+    }
 
   @Test
-  @Ignore
   public void deleteAccountTest() throws InterruptedException, ExecutionException {
-     int MAXTRIES=20;
-      for(int i=0; i<MAXTRIES; i++) {
+      runTest(()-> {
           try {
-              Connection connection = login(socket, localAddress, LoginHelper::loginResponse);
               Account account = createAccount(factory, socket, 1);
               deleteAccount(factory, socket, account.getAccountID());
-              connection.disconnect();
-          } catch (Exception ex) {
-              logger.error("Login:", ex);
-              if(i>=MAXTRIES-1)
-                  throw(ex);
+          } catch (InterruptedException| ExecutionException e) {
+              fail(e.getMessage());
           }
-          break;
-      }
+      });
+
   }
 }
--- javaUITesting2/src/test/java/com/setl/WebSocketAPITests/Member/Creation/createMemberTest.java
+++ javaUITesting2/src/test/java/com/setl/WebSocketAPITests/Member/Creation/createMemberTest.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.junit.AfterClass;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,75 +23,83 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import SETLAPIHelpers.WebSocketAPI.LoginHelper;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
+import static SETLAPIHelpers.WebSocketAPI.AccountHelper.createAccount;
 import static SETLAPIHelpers.WebSocketAPI.LoginHelper.login;
 import static SETLAPIHelpers.MemberDetailsHelper.generateMemberDetails;
 import static SETLAPIHelpers.WebSocketAPI.MemberHelper.createMember;
 import static SETLAPIHelpers.WebSocketAPI.MemberHelper.createMemberAndCaptureDetails;
+import static junit.framework.Assert.fail;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 
 @RunWith(OrderedJUnit4ClassRunner.class)
 public class createMemberTest {
   private static final Logger logger = LogManager.getLogger(createMemberTest.class);
+  static ExecutorService executor  = Executors.newSingleThreadExecutor();
 
-  @Rule
-  public Timeout globalTimeout = Timeout.millis(30000);;
-  KeyHolder holder = new KeyHolder();
-  MessageFactory factory = new MessageFactory(holder);
-  SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alex01");
-  SetlSocketClusterClient ws = new SetlSocketClusterClient(socket);
-  String localAddress = "ws://uk-lon-li-006.opencsd.io:9788/db/";
+    @AfterClass
+    public static void stop(){
+        executor.shutdown();;
+    }
+
+
+    KeyHolder holder = new KeyHolder();
+    MessageFactory factory = new MessageFactory(holder);
+    SocketClientEndpoint socket = new SocketServerEndpoint(holder, factory, "emmanuel", "alex01");
+    String localAddress = "ws://uk-lon-li-006.opencsd.io:9788/db/";
+
+
+    private void runTest(Runnable r){
+        Connection connection = login(socket, localAddress, LoginHelper::loginResponse);
+        for (int i=0;i<3;i++)
+            try {
+                executor.submit(r).get(30, TimeUnit.SECONDS);
+                break;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        connection.disconnect();
+    }
 
   @Test
-  @Ignore
+
   public void createNewMember() throws ExecutionException, InterruptedException {
 
-      int MAXTRIES=20;
-      for(int i=0; i<MAXTRIES; i++) {
+      runTest(()-> {
           try {
-              Connection connection = login(socket, localAddress, LoginHelper::loginResponse);
-              createMember(factory, socket);
-
-              connection.disconnect();
-          } catch (Exception ex) {
-              logger.error("Login:", ex);
-              if(i>=MAXTRIES-1)
-                  throw(ex);
+              createMember(factory, socket, 1);
+          } catch (InterruptedException| ExecutionException e) {
+              fail(e.getMessage());
           }
-          break;
-      }
+      });
   }
 
+
   @Test
-  @Ignore
   public void createNewMemberAndVerifySuccess() throws ExecutionException, InterruptedException {
-      int MAXTRIES=20;
-      for(int i=0; i<MAXTRIES; i++) {
-          try {
-                Connection connection = login(socket, localAddress, LoginHelper::loginResponse);
+
+        runTest(()-> {
+            try {
                 String memberDetails[] = generateMemberDetails();
                 String memberName = memberDetails[0];
                 String email = memberDetails[1];
                 Member member = createMemberAndCaptureDetails(factory, socket, memberName, email);
                 assertTrue(member.getMemberName().equals(memberName));
-
-    connection.disconnect();
-          } catch (Exception ex) {
-              logger.error("Login:", ex);
-              if(i>=MAXTRIES-1)
-                  throw(ex);
-          }
-          break;
-      }
-  }
+            } catch (InterruptedException| ExecutionException e) {
+                fail(e.getMessage());
+            }
+        });
+    }
 
   @Test
-  @Ignore
   public void createMemberWithValidDataTest() throws InterruptedException, ExecutionException {
 
   final AtomicInteger atomicInt = new AtomicInteger(0);
