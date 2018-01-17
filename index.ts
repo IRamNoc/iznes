1001ba29def543b20747bad172811a616150a7e2
David Duong <david.duong@setl.io>
2018-01-17 17:44:12 +0100

    TG-81 feat(index): export some interface from message connection model

--- index.ts
+++ index.ts
@@ -2,4 +2,13 @@ export {SetlMessagesModule} from './messages.module';
 export {SetlMessagesComponent} from './messages/messages.component';
 export {MessagesService} from './messages.service';
 
-export {MessageAction, MessageActionsConfig, MessageField} from './messages/message-components/message-form-action/message-form-action.model';
+export {
+    MessageAction,
+    MessageActionsConfig,
+    MessageField
+} from './messages/message-components/message-form-action/message-form-action.model';
+
+export {
+    MessageConnection,
+    MessageConnectionConfig
+} from './messages/message-components/message-connection/message-connection.model';
