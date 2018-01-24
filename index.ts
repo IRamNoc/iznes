9fd429634c7543988bce9a419aac556095049fed
Stuart Fyfe <stuart.fyfe@setl.io>
2018-01-24 14:34:16 +0000

    Fix: Added missing export for reporting service

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export {SetlBalancesModule} from './balances.module';
 export {SetlBalancesComponent} from './balances/balances.component';
 export {SetlIssueComponent} from './issue/issue.component';
 export {SetlTransactionsComponent} from './transactions/transactions.component';
+export {ReportingService} from './reporting.service';
