a296c067968ed4ab39ec6884f50809ed4f2c7197
Laurent HAUCK <laurent.hauck@setl.io>
2018-01-25 15:38:13 +0100

    TG-143-146 fix(utils-pipes): try to fix jenkins

--- pipes/index.ts
+++ pipes/index.ts
@@ -75,7 +75,7 @@ export class MoneyValuePipe implements PipeTransform {
         }
     }
 
-    parse(value: any, fractionSize: number = 2): number {
+    parse(value: any, fractionSize: number = 2): any {
         if (typeof value !== 'undefined' && !isNaN(value.toString().replace(/ /g, ''))) {
             // console.log('parse', value, fractionSize);
             const newValue = (this.ROUND_UP_DECIMALS.indexOf(Number(fractionSize)) !== -1)
