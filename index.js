e213051e10d09bb6cf9dcdad01bb5f5e7af866f7
Laurent HAUCK <laurent.hauck@setl.io>
2018-02-01 16:36:35 +0100

    TG-[163,166,169,170,171,172,202] feat(forgotten-password): login component

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export {SetlLoginModule} from './login.module';
 export {SetlLoginComponent} from './login.component';
 export {LoginGuardService} from './login-guard.service';
 export {name} from './__init__';
-
