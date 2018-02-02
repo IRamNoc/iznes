833b8f86cfb30ab79dbde8fe14441f9682e5e085
ming <mingrui.huang@setl.io>
2018-02-02 15:40:59 +0000

    TG-XXX fix: appConfig interface fix

--- appConfig/appConfig.model.ts
+++ appConfig/appConfig.model.ts
@@ -24,6 +24,9 @@ export interface AppConfig {
     menuSpec: MenuSpec;
     reportingNodeUrl: string;
     messagesMenu: Array<MessagesMenuItem>;
+    logoUrl: string;
+    logoID: string;
+    platform: string;
 }
 
 export interface MessagesMenuItem {
