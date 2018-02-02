d78eb0696bf6bdac25de95a1220c3a0308612355
Ollie Kett <ollie.kett@setl.io>
2018-02-02 12:23:35 +0000

    TG-XXX feature(ui-form): standard layout

---
+++ layouts/ui-form/ui-form.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-blank',
+    templateUrl: './ui-form.component.html',
+    styleUrls: ['./ui-form.component.scss'],
+})
+export class UiFormComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
