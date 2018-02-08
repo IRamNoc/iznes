542f864b416a7a312dd65264c44e7316c91b13c1
ming <mingrui.huang@setl.io>
2018-02-08 11:57:51 +0000

    TG-XXX feat: Adapt to angular 5

--- mock/toaster-service/index.ts
+++ mock/toaster-service/index.ts
@@ -1,8 +1,8 @@
-import {ToasterService} from 'angular2-toaster';
+import {ToasterService, Toast} from 'angular2-toaster';
 import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ToasterServiceMock extends ToasterService {
-    pop() {
+    pop(type: string | Toast, title?: string, body?: string): any {
     }
 }
