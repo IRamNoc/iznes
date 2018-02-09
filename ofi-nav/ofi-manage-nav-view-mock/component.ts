8c94f63ae660abc919fbc737bbdc6a59b9651222
Phil Zeelte <philzeelte@IM00152.local>
2018-02-09 10:14:09 +0000

    modified time period dropdown

--- ofi-nav/ofi-manage-nav-view-mock/component.ts
+++ ofi-nav/ofi-manage-nav-view-mock/component.ts
@@ -10,8 +10,12 @@ export class OfiManageNavView implements OnInit, OnDestroy {
     
     currentDate: string = moment().format('DD-MM-YY');
 
+    searchDateFrom = moment().add(-10, 'days');
+    searchDateTo = moment().add(-1, 'days');
+
     // mock data
     mockDataGridItems: any[];
+    mockDatePeriodItems: any[];
     exportOptions: any[];
 
     constructor() {}
@@ -27,6 +31,23 @@ export class OfiManageNavView implements OnInit, OnDestroy {
         for(let i = 1; i < 11; i++) {
             this.mockDataGridItems.push(this.getGridDataItem(i));
         }
+
+        this.mockDatePeriodItems = [{
+            id: '3',
+            text: 'last 3 days'
+        }, {
+            id: '7',
+            text: 'last 7 days'
+        }, {
+            id: '14',
+            text: 'last 14 days'
+        }, {
+            id: '30',
+            text: 'last 30 days'
+        }, {
+            id: '60',
+            text: 'last 60 days'
+        }]
 
         this.exportOptions = [{
             id: 'csv',
