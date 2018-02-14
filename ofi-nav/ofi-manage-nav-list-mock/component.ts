e2cf219de4263a91fd2370d32dc438bc9b46e57c
Phil Zeelte <philzeelte@IM00152.local>
2018-02-14 16:14:12 +0000

    added real data to the component and fleshed out the grid more

--- ofi-nav/ofi-manage-nav-list-mock/component.ts
+++ ofi-nav/ofi-manage-nav-list-mock/component.ts
@@ -1,8 +1,19 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ChangeDetectorRef} from '@angular/core';
 import {Router} from '@angular/router';
+import {select, NgRedux} from '@angular-redux/store';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 import * as moment from 'moment';
 
 import * as model from '../OfiManageNav';
+
+import {OfiCorpActionService} from '../../ofi-req-services/ofi-corp-actions/service';
+import {OfiNavService} from '../../ofi-req-services/ofi-product/nav/service';
+import {
+    ofiSetCurrentManageNavRequest,
+    clearRequestedManageNavList,
+    getOfiManageNavListCurrentRequest
+} from '../../ofi-store/ofi-product/nav';
 
 @Component({
     selector: 'app-nav-manage-list',
@@ -10,6 +21,9 @@ import * as model from '../OfiManageNav';
     styleUrls: ['./component.css']
 })
 export class OfiManageNavList implements OnInit, OnDestroy {
+
+    shareListItems: any[];
+    navListItems: any[];
     
     dateTypes: any[];
     searchDate: Date;
@@ -24,15 +38,81 @@ export class OfiManageNavList implements OnInit, OnDestroy {
     navPopupMode: model.NavPopupMode = model.NavPopupMode.ADD;
     addNavShare: model.NavModel = null;
 
-    // mock data
-    mockDataGridItems: model.NavInfoModel[];
+    private subscriptionsArray: Subscription[] = [];
 
-    constructor(private router: Router) {}
+    @select(['ofi', 'ofiProduct', 'ofiManageNav', 'ofiManageNavList', 'requested']) navRequestedOb: Observable<any>;
+    @select(['ofi', 'ofiProduct', 'ofiManageNav', 'ofiManageNavList', 'navList']) navListOb: Observable<any>;
 
+    constructor(private router: Router,
+        private redux: NgRedux<any>,
+        private changeDetectorRef: ChangeDetectorRef,
+        private ofiCorpActionService: OfiCorpActionService,
+        private ofiNavService: OfiNavService) {
+        
+        this.initSubscriptions();
+    }
+        
     ngOnInit() {
         this.initDataTypes();
-        this.generateMockData();
     }
+
+    private initSubscriptions(): void {
+        this.subscriptionsArray.push(this.navRequestedOb.subscribe(requested => {
+            this.requestNavList(requested);
+        }));
+        this.subscriptionsArray.push(this.navListOb.subscribe(navList => {
+            this.updateNavList(navList);
+        }));
+    }    
+
+    /**
+     * Request my fund access base of the requested state.
+     * @param requested boolean
+     * @return void
+     */
+    private requestUserIssuedAssets(requested: boolean): void {
+        if (!requested) {
+            OfiCorpActionService.defaultRequestUserIssuedAsset(this.ofiCorpActionService, this.redux);
+        }
+    }
+
+    /**
+     * update asset list
+     * @param assetList ShareModel
+     * @return void
+     */
+    private updateUserIssuedAssets(assetList: model.ShareModel[]): void {
+        this.shareListItems = assetList;
+        this.changeDetectorRef.markForCheck();
+    }
+
+    /**
+     * request the nav list
+     * @param requested boolean
+     * @return void
+     */
+    private requestNavList(requested: boolean): void {
+        const requestData = {
+            fundName: '',
+            navDate: moment().format('YY-MM-DD 00:00:00'),
+            status: 0,
+            pageNum: 0,
+            pageSize: 1000
+        }
+
+        OfiNavService.defaultRequestNavList(this.ofiNavService, this.redux, requestData);
+    }
+
+    /**
+     * update the nav list
+     * @param navList NavList
+     * @return void
+     */
+    private updateNavList(navList: model.NavModel[]): void {
+        this.navListItems = navList;
+        this.changeDetectorRef.markForCheck();
+    }
+    
 
     private initDataTypes(): void {
         this.dateTypes = [{
@@ -42,28 +122,6 @@ export class OfiManageNavList implements OnInit, OnDestroy {
             id: 'navPubDate',
             text: 'NAV Published Date'
         }];
-    }
-
-    // mock data
-    private generateMockData(): void {
-        this.mockDataGridItems = [];
-
-        for(let i = 1; i < 11; i++) {
-            this.mockDataGridItems.push(this.getGridDataItem(i));
-        }
-    }
-
-    private getGridDataItem(num: number): model.NavInfoModel {
-        return {
-            name: `Mock Share ${num}`,
-            ISIN: parseInt(`100000${num}`),
-            value: 0,
-            lastValue: Math.random(),
-            currency: 'GBP',
-            date: moment().format('DD-MM-YY'),
-            pubDate: moment().format('DD-MM-YY'),
-            status: 'Estimated'
-        }
     }
     
     addNav(share: model.NavInfoModel): void {
