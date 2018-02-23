a612b62328013d4a0eb15a9f14e812386edf4201
Albert Oudompheng <albert.oudompheng@setl.io>
2018-02-23 13:06:25 +0100

    TG-266 feat(ofi-kyc): added unit test to text-input-list component

--- ofi-kyc/text-input-list/component.spec.ts
+++ ofi-kyc/text-input-list/component.spec.ts
@@ -1,12 +1,12 @@
 import {TextInputListComponent} from './component';
-import {ComponentFixture, TestBed, async, fakeAsync, tick} from '@angular/core/testing';
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {DebugElement} from '@angular/core';
 import { By } from '@angular/platform-browser';
 
 
 
-fdescribe('TextInputListComponent', () => {
+describe('TextInputListComponent', () => {
 
     let fixture: ComponentFixture<TextInputListComponent>;
     let comp:    TextInputListComponent;
@@ -14,6 +14,14 @@ fdescribe('TextInputListComponent', () => {
     let el:      HTMLElement;
 
     const mockTitle = 'test title';
+    const mockAdditionnalText = 'test additionnalText';
+    const mockContent = {
+        test1: {value: 'value1', label: 'label1'},
+        test2: {value: 'value2', label: 'label2'},
+        test3: {value: 'value3', label: 'label3'},
+        test4: {value: 'value4', label: 'label4'},
+        test5: {value: 'value5', label: 'label5'},
+    };
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -29,14 +37,8 @@ fdescribe('TextInputListComponent', () => {
         el = de.nativeElement;
 
         comp.title = mockTitle;
-        comp.content = {
-            test1: {value: 'value1', label: 'label1'},
-            test2: {value: 'value2', label: 'label2'},
-            test3: {value: 'value3', label: 'label3'},
-            test4: {value: 'value4', label: 'label4'},
-            test5: {value: 'value5', label: 'label5'},
-        };
-        comp.additionnalText = 'test additionnalText';
+        comp.content = mockContent;
+        comp.additionnalText = mockAdditionnalText;
 
         fixture.detectChanges();
     });
@@ -50,10 +52,36 @@ fdescribe('TextInputListComponent', () => {
     });
 
     describe('interface', () => {
-        it('should display a title', () => {
+        it('should display the @Input title as header', () => {
             const header = fixture.debugElement.query(By.css('h4'));
 
             expect(header.nativeElement.textContent).toBe(mockTitle);
+        });
+
+        it('should display the @Input additionnalText as additionnal text', () => {
+            const p = fixture.debugElement.query(By.css('form + p'));
+
+            expect(p.nativeElement.textContent).toBe(mockAdditionnalText);
+        });
+
+        it('should display the @Input content as form inputs', () => {
+            const inputs = fixture.debugElement.queryAll(By.css('input'));
+            const labels = fixture.debugElement.queryAll(By.css('label'));
+            Object.keys(mockContent).forEach((key, idx) => {
+                expect(inputs[idx].nativeElement.value).toBe(mockContent[key].value);
+                expect(labels[idx].nativeElement.textContent).toBe(mockContent[key].label);
+            });
+        });
+
+        it('should display no form when @Input content is an empty object', () => {
+            comp.content = {};
+            fixture.detectChanges();
+
+            const form = fixture.debugElement.queryAll(By.css('form'));
+            const inputs = fixture.debugElement.queryAll(By.css('input'));
+
+            expect(form.length).toBe(0);
+            expect(inputs.length).toBe(0);
         });
     });
 });
