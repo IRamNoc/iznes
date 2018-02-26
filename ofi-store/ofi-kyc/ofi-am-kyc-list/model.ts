2c70985d0c0ceb181cf19bcaa628e138f3ab12e3
David Duong <david.duong@setl.io>
2018-02-26 15:38:40 +0100

    TG-XXX refact(ofi-store): update the AmKycDetail interface structure

--- ofi-store/ofi-kyc/ofi-am-kyc-list/model.ts
+++ ofi-store/ofi-kyc/ofi-am-kyc-list/model.ts
@@ -1,13 +1,27 @@
 export interface AmKycDetail {
+    kycID: number;
+    investorUserID: number;
+    investorUserName: string;
+    investorFirstName: string;
+    investorLastName: string;
+    investorEmail: string;
+    investorPhoneCode: string;
+    investorPhoneNumber: string;
+    investorCompanyName: string;
+    amUserName: string;
+    amFirstName: string;
+    amLastName: string;
+    lastUpdated: string;
+    lastReviewBy: string;
+    investorWalletID: number;
+    walletName: string;
+    amManagementCompanyID: number;
+    isInvited: boolean;
+    invitedID: number;
     status: string;
-    companyName: string;
-    actionDate: string;
-    kycDate: string;
-    reviewBy: string;
-    invited: string;
 }
 
 export interface AmKycListState {
-    amKycList:Array<AmKycDetail>;
+    amKycList: Array<AmKycDetail>;
     requested: boolean;
 }
