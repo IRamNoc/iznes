dab1df1bd911119b500f42293a58d26e95309b31
Albert Oudompheng <albert.oudompheng@setl.io>
2018-02-27 13:57:39 +0100

    TG-307 feat(ofi-profile): implemented backend calls to the profile/my-information view

--- ofi-profile/profile-my-informations/component.ts
+++ ofi-profile/profile-my-informations/component.ts
@@ -4,8 +4,10 @@ import {NgRedux, select} from '@angular-redux/store';
 import {ToasterService} from 'angular2-toaster';
 
 import {APP_CONFIG, AppConfig} from '@setl/utils';
-import {KycMyInformations, setInformations} from '../../ofi-store/ofi-kyc/my-informations';
+import {KycMyInformations} from '@ofi/ofi-main/ofi-store/ofi-kyc/my-informations';
 import {Observable} from 'rxjs/Observable';
+import {SagaHelper} from '@setl/utils/index';
+import {MyUserService} from '@setl/core-req-services/index';
 
 @Component({
     selector: 'app-profile-my-informations',
@@ -26,6 +28,7 @@ export class OfiProfileMyInformationsComponent implements OnInit {
         private _ngRedux: NgRedux<any>,
         private toasterService: ToasterService,
         private location: Location,
+        private myUserService: MyUserService,
         @Inject(APP_CONFIG) appConfig: AppConfig,
     ) {
         this.appConfig = appConfig;
@@ -41,8 +44,35 @@ export class OfiProfileMyInformationsComponent implements OnInit {
     }
 
     saveUserInformations(userInformations: KycMyInformations) {
-        this._ngRedux.dispatch(setInformations(userInformations));
-        this.toasterService.pop('success', 'Saved changes');
+        const user = {
+            displayName: '',
+            firstName: userInformations.firstName,
+            lastName: userInformations.lastName,
+            mobilePhone: '',
+            addressPrefix: '',
+            address1: '',
+            address2: '',
+            address3: '',
+            address4: '',
+            postalCode: '',
+            country: '',
+            memorableQuestion: '',
+            memorableAnswer: '',
+            profileText: '',
+            phoneCode: userInformations.phoneCode,
+            phoneNumber: userInformations.phoneNumber,
+            companyName: userInformations.companyName,
+        };
+        const asyncTaskPipe = this.myUserService.saveMyUserDetails(user);
+        this._ngRedux.dispatch(SagaHelper.runAsyncCallback(
+            asyncTaskPipe,
+            () => {
+                this.toasterService.pop('success', 'Saved changes');
+            },
+            (data) => {
+                this.toasterService.pop('error', JSON.stringify(data));
+            })
+        );
     }
 
     closeUserInformations() {
