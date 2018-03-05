08caa21d78d2da20e03d653c247dad3acb0029a9
David Duong <david.duong@setl.io>
2018-03-05 17:41:45 +0100

    TG-314-315 refact(message-kyc): add some styles and create a event listener on the redirect button

--- messages/message-components/message-kyc/message-kyc.component.ts
+++ messages/message-components/message-kyc/message-kyc.component.ts
@@ -1,14 +1,25 @@
 import {Component, Input} from '@angular/core';
 import {MessageKycConfig} from './message-kyc.model';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'setl-message-kyc',
     templateUrl: './message-kyc.component.html',
+    styles: [
+            `.padding {
+            padding: 5px 0;
+        }`
+    ]
 })
 export class SetlMessageKycComponent {
     @Input() config: MessageKycConfig;
 
-    constructor() {
-        console.log('config: ', this.config);
+    constructor(private router: Router) {
+
+    }
+
+    handleRedirectButtonClick() {
+        console.log('coucou');
+        this.router.navigate(['kyc-am-documents']);
     }
 }
