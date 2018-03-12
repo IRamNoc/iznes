76c8ed91658e7caa99f87da81b96055b8cfec102
Phil Zeelte <philzeelte@IM00152.local>
2018-03-12 14:21:49 +0000

    added new models

--- ofi-product/fund-share/model.ts
+++ ofi-product/fund-share/model.ts
@@ -1,14 +1,18 @@
 import {ShareCharacteristicMandatory, ShareCharacteristicOptional} from './models/characteristic';
+import {ShareCalendarMandatory} from './models/calendar';
 import {ShareFeesMandatory, ShareFeesOptional} from './models/fees';
 import {ShareKeyFactsMandatory, ShareKeyFactsOptional} from './models/keyFacts';
-import {ShareListing} from './models/listing';
-import {SharePRIIP} from './models/priip';
-import {ShareProfile} from './models/profile';
-import {ShareRepresentation} from './models/representation';
-import {ShareSolvency} from './models/solvency';
-import {ShareTaxation} from './models/taxation';
+import {ShareListingOptional} from './models/listing';
+import {SharePRIIPOptional} from './models/priip';
+import {ShareProfileMandatory, ShareProfileOptional} from './models/profile';
+import {ShareRepresentationOptional} from './models/representation';
+import {ShareSolvencyOptional} from './models/solvency';
+import {ShareTaxationOptional} from './models/taxation';
 
 export class FundShare {
+    calendar = {
+        mandatory: new ShareCalendarMandatory()
+    }
     characteristic = {
         mandatory: new ShareCharacteristicMandatory(),
         optional: new ShareCharacteristicOptional()
@@ -21,10 +25,23 @@ export class FundShare {
         mandatory: new ShareKeyFactsMandatory(),
         optional: new ShareKeyFactsOptional()
     }
-    listing = new ShareListing();
-    priip = new SharePRIIP();
-    profile = new ShareProfile();
-    representation = new ShareRepresentation();
-    solvency = new ShareSolvency();
-    taxation = new ShareTaxation();
+    listing = {
+        optional: new ShareListingOptional()
+    }
+    priip = {
+        optional: new SharePRIIPOptional()
+    }
+    profile = {
+        mandatory: new ShareProfileMandatory(),
+        optional: new ShareCharacteristicOptional()
+    }
+    representation = {
+        optional: new ShareRepresentationOptional()
+    }
+    solvency = {
+        optional: new ShareSolvencyOptional()
+    }
+    taxation = {
+        optional: new ShareTaxationOptional()
+    }
 }
