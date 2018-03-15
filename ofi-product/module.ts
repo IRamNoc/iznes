abbd14850b1ea0223a7b480d854bb34553d8b5a8
David Duong <david.duong@setl.io>
2018-03-15 10:31:49 +0100

    TG-XXX refact(ofi-product): tidy up the ofi-product module

--- ofi-product/module.ts
+++ ofi-product/module.ts
@@ -1,15 +1,18 @@
-
 /* Core imports. */
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 /* Clarity module. */
 import {ClarityModule} from '@clr/angular';
-import {SetlPipesModule, DpDatePickerModule, SetlDirectivesModule} from '@setl/utils';
+import {DpDatePickerModule, SetlDirectivesModule, SetlPipesModule} from '@setl/utils';
 import {SelectModule} from 'ng2-select';
+import {RouterModule} from '@angular/router';
+import {OfiKYCModule} from '@ofi/ofi-main/ofi-kyc/module';
+
+/* Graphs. */
+import {ChartsModule} from 'ng2-charts';
 
 /* Multilingual coolness. */
 import {MultilingualModule} from '@setl/multilingual';
@@ -17,15 +20,20 @@ import {MultilingualModule} from '@setl/multilingual';
 /* Components. */
 import {ProductHomeComponent} from './home/component';
 import {UmbrellaFundComponent} from './umbrella-fund/component';
+import {FundCreateComponent} from './fundCreate/component';
+// import {FundEditComponent} from './fundEdit/component';
 
-/* Graphs. */
-import {ChartsModule} from 'ng2-charts';
+/* fundItems config ( should be served by backend TT ) */
+import fundItems from './fundConfig';
+
 
 /* Am Dashboard service. */
 @NgModule({
     declarations: [
         ProductHomeComponent,
         UmbrellaFundComponent,
+        FundCreateComponent,
+        // FundEditComponent,
     ],
     exports: [
         ProductHomeComponent,
