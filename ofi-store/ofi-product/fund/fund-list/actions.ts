b6994479b9d2e7b0772fb0e9144f327faef76789
David Duong <david.duong@setl.io>
2018-03-15 17:20:37 +0100

    TG-373 refact(ofi-store/ofi-product/fund/fund-list): create the action that gets the iznes funds from the SP

--- ofi-store/ofi-product/fund/fund-list/actions.ts
+++ ofi-store/ofi-product/fund/fund-list/actions.ts
@@ -1,6 +1,5 @@
 import {name} from './__init__';
 import {kAction} from '@setl/utils/common';
-import {Common, SagaHelper} from '@setl/utils';
 
 /**
  * Set Fund List
@@ -21,7 +20,6 @@ export const SET_FUND_SHARE_LIST = `${name}/SET_FUND_SHARE_LIST`;
 export const SET_REQUESTED_FUND = `${name}/SET_REQUESTED_FUND`;
 export const setRequestedFund = kAction(SET_REQUESTED_FUND);
 
-
 /**
  * Clear (set to false) request
  * Flag that to indicate we will ned to request it when we need it.
@@ -29,4 +27,9 @@ export const setRequestedFund = kAction(SET_REQUESTED_FUND);
 export const CLEAR_REQUESTED_FUND = `${name}/CLEAR_REQUESTED_FUND`;
 export const clearRequestedFund = kAction(CLEAR_REQUESTED_FUND);
 
-
+/* Iznes funds */
+export const SET_REQUESTED_IZN_FUNDS = `${name}/SET_REQUESTED_IZN_FUNDS`;
+export const setRequestedIznesFunds = kAction(SET_REQUESTED_IZN_FUNDS);
+export const CLEAR_REQUESTED_IZN_FUNDS = `${name}/CLEAR_REQUESTED_IZN_FUNDS`;
+export const clearRequestedIznesFunds = kAction(CLEAR_REQUESTED_IZN_FUNDS);
+export const GET_IZN_FUND_LIST = `${name}/GET_IZN_FUND_LIST`;
