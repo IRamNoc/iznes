7994f593857ac5ebdc8d78f6cf2a48d9eb9161bc
David Duong <david.duong@setl.io>
2018-03-22 18:54:30 +0100

    TG-628 refact(message-kyc/component.ts): refactor so that it now handles the message in both english and french

--- messages/message-components/message-kyc/message-kyc.component.ts
+++ messages/message-components/message-kyc/message-kyc.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MessageKycConfig} from './message-kyc.model';
 import {Router} from '@angular/router';
+import {kycMessages} from './message-kyc';
 
 @Component({
     selector: 'setl-message-kyc',
@@ -11,11 +12,49 @@ import {Router} from '@angular/router';
         }`
     ]
 })
-export class SetlMessageKycComponent {
+export class SetlMessageKycComponent implements OnInit {
     @Input() config: MessageKycConfig;
+    messageBody: Object;
 
     constructor(private router: Router) {
+        this.messageBody = null;
+    }
 
+    ngOnInit() {
+        switch (this.config.type) {
+            case 'kycInvestorYes':
+                this.messageBody = (this.config.lang === 'fr-Latn') ?
+                    this.replaceMessageValue(kycMessages.kycInvestorYes.fr) :
+                    this.replaceMessageValue(kycMessages.kycInvestorYes.en);
+                break;
+
+            case 'kycInvestorNo':
+                this.messageBody = (this.config.lang === 'fr-Latn') ?
+                    this.replaceMessageValue(kycMessages.kycInvestorNo.fr) :
+                    this.replaceMessageValue(kycMessages.kycInvestorNo.en);
+                break;
+        }
+
+        console.log('messageBody: ', this.messageBody);
+    }
+
+    replaceMessageValue(data) {
+        const count = Object.keys(data).length;
+        const message = Object.assign({}, data);
+
+        for (let i = 0; i < count; i++) {
+            const key = Object.keys(data)[i];
+            const value = data[key].replace('{{amFirstName}}', this.config.amFirstName)
+                                   .replace('{{amCompanyName}}', this.config.amCompanyName)
+                                   .replace('{{investorCompanyName}}', this.config.investorCompanyName)
+                                   .replace('{{investorEmail}}', this.config.investorEmail)
+                                   .replace('{{investorPhoneNumber}}', this.config.investorPhoneNumber);
+
+
+            message[key] = value;
+        }
+
+        return message;
     }
 
     handleRedirectButtonClick() {
