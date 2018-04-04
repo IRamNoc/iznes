c1c79d05a23d6e5f7904922ff2ca8a2280bbf849
Laurent HAUCK <laurent.hauck@setl.io>
2018-04-04 13:51:47 +0200

    TG-XXX refact(Orders): am/inv almost finished need real datas to test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,10 +11,12 @@ export const environment = {
         // host: 'uk-lon-li-006.opencsd.io',
         // host: 'staging.iznes.io',
         host: window.location.hostname,
+        // host: 'frontend2.opencsd.io',
         // host: '10.0.1.106', // Ming's Socket Server
         // host: '10.0.1.163',  // Ollie's Socket Server
         // host: '10.0.1.176', // Dan's Socket Server
         // host: '10.0.1.175', // Mat's Socket Server
+        // host: '10.0.25.87', // Mat's Socket Server
         port: 9788,
         path: 'db'
     },
