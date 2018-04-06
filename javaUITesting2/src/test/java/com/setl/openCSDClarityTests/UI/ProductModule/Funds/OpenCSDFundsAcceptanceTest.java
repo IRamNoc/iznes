6e527705d101613f093c7e13b216362650e84250
bill mackie <bill.mackie@setl.io>
2018-04-06 17:25:52 +0100

    Bill : fixed grid headings

--- javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/ProductModule/Funds/OpenCSDFundsAcceptanceTest.java
+++ javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/ProductModule/Funds/OpenCSDFundsAcceptanceTest.java
@@ -88,6 +88,9 @@ public class OpenCSDFundsAcceptanceTest {
         assertTrue(driver.findElement(By.xpath("//app-ofi-am-product-home/div[2]/div[2]/div/clr-datagrid/div/div/div/clr-dg-table-wrapper/div[1]/div/clr-dg-column[5]/div/button")).getText().contentEquals(sharesHeadings[4]));
         assertTrue(isElementPresent(By.xpath("//app-ofi-am-product-home/div[2]/div[2]/div/clr-datagrid/div/div/div/clr-dg-table-wrapper/div[1]/div/clr-dg-column[6]/div/button")));
         assertTrue(driver.findElement(By.xpath("//app-ofi-am-product-home/div[2]/div[2]/div/clr-datagrid/div/div/div/clr-dg-table-wrapper/div[1]/div/clr-dg-column[6]/div/button")).getText().contentEquals(sharesHeadings[5]));
+        assertTrue(isElementPresent(By.xpath("//app-ofi-am-product-home/div[2]/div[2]/div/clr-datagrid/div/div/div/clr-dg-table-wrapper/div[1]/div/clr-dg-column[7]/div/button")));
+        assertTrue(driver.findElement(By.xpath("//app-ofi-am-product-home/div[2]/div[2]/div/clr-datagrid/div/div/div/clr-dg-table-wrapper/div[1]/div/clr-dg-column[7]/div/button")).getText().contentEquals(sharesHeadings[6]));
+
     }
 
     private void validateFundsDataGridHeadings (String [] fundsHeadings) {
