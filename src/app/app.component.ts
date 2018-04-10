a75b6af7b8225b3f27dd06aaac955c121219231b
Stephen Strudwick <stephen.strudwick@setl.io>
2018-04-10 11:25:00 +0100

    TG-XXX fix(signuplanguage): initial language check removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,8 +84,6 @@ export class AppComponent implements AfterViewInit, OnInit {
             this.ofiMemberNodeChannelService.resolveChannelUpdate(data);
         });
 
-        this.checkUserLanguage();
-
     }
 
     /**
@@ -103,36 +101,6 @@ export class AppComponent implements AfterViewInit, OnInit {
 
     ngAfterViewInit() {
 
-    }
-
-    private checkUserLanguage(): void {
-        let langFromLocalStorage = '';
-        const validLanguages = [
-            'en',
-            'fr'
-        ];
-
-        if ((navigator) && navigator.language) {
-            const lang = navigator.language.split('-')[0];
-
-            if (typeof(Storage) !== 'undefined') {
-                if (localStorage.getItem('lang') !== null) {
-                    langFromLocalStorage = localStorage.getItem('lang');
-                }
-            }
-
-            if (langFromLocalStorage !== '') {
-                this.ngRedux.dispatch(setLanguage(langFromLocalStorage));
-            } else {
-                if (validLanguages.indexOf(lang) !== -1) {
-                    if (lang === 'en') {
-                        this.ngRedux.dispatch(setLanguage('en-Latn'));
-                    } else if (lang === 'fr') {
-                        this.ngRedux.dispatch(setLanguage('fr-Latn'));
-                    }
-                }
-            }
-        }
     }
 }
 
