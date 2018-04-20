e5bcf974e6d55788f32227867a34d903bf0c2032
Adam Crowe <adam.crowe@setl.io>
2018-04-20 16:03:57 +0100

    TG-XXX fix(chain-management): Fix chain management tab behaviour and UI popup standards

--- manage-chains/chains.component.ts
+++ manage-chains/chains.component.ts
@@ -1,26 +1,16 @@
-/*
- * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
- * This software is released under MIT license.
- * The full license information can be found in LICENSE in the root directory of this project.
- */
-
 import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ConfirmationService, SagaHelper} from '@setl/utils';
 import {NgRedux, select} from '@angular-redux/store';
+import {Subscription} from 'rxjs/Subscription';
 import {fromJS} from 'immutable';
 import * as _ from 'lodash';
 
 import {ChainInterface} from './chains.interface';
 import {ChainModel} from './chains.model';
-/* Alerts and confirms. */
 import {AlertsService} from '@setl/jaspero-ng2-alerts';
-// Internal
-import {Subscription} from 'rxjs/Subscription';
-// Services
-import {ChainService} from "@setl/core-req-services/chain/service";
-/*  */
-import {PersistService} from "@setl/core-persist";
+import {ChainService} from '@setl/core-req-services/chain/service';
+import {PersistService} from '@setl/core-persist';
 
 @Component({
     selector: 'app-manage-chains',
@@ -30,27 +20,14 @@ import {PersistService} from "@setl/core-persist";
 })
 
 export class ManageChainsComponent implements OnInit, AfterViewInit, OnDestroy {
-
+    tabsControl: Array<object>;
     language = 'en';
-
-    multiChainForm = [];
-    chainForm: FormGroup;
-    modelForm: any;
-    editForm = false;
-    showSearchTab = false;
-    showModal = false;
-    modalTitle = '';
-    modalText = '';
-
-    showConfirmModal = false;
-    chainToDelete: any = 0;
-
     chainsList = [];
 
     // List of observable subscription
     subscriptionsArray: Array<Subscription> = [];
 
-    // List of redux observable.
+    // List of redux observable
     @select(['user', 'siteSettings', 'language']) requestLanguageObj;
     @select(['chain', 'chainList', 'requested']) requestedChainOb;
     @select(['chain', 'chainList', 'chainList']) chainListOb;
@@ -58,35 +35,39 @@ export class ManageChainsComponent implements OnInit, AfterViewInit, OnDestroy {
     constructor(private _fb: FormBuilder,
                 private ngRedux: NgRedux<any>,
                 private _changeDetectorRef: ChangeDetectorRef,
-                private _alertsService: AlertsService,
+                private alertsService: AlertsService,
                 private _chainService: ChainService,
                 private _confirmationService: ConfirmationService,
                 private _persistService: PersistService) {
+
+        this.tabsControl = [
+            {
+                title: '<i class="fa fa-chain"></i> Chains',
+                chainId: -1,
+                active: true
+            },
+            {
+                title: '<i class="fa fa-plus"></i> Add New Chain',
+                chainId: -1,
+                formControl: this._persistService.watchForm('manageMember/manageChains', new FormGroup(
+                    {
+                        chainId: new FormControl('', Validators.compose([Validators.required, this.isInteger])),
+                        chainName: new FormControl('', Validators.required)
+                    }
+                )),
+                active: false
+            }
+        ];
+
         this.subscriptionsArray.push(this.requestLanguageObj.subscribe((locale) => this.getLanguage(locale)));
         this.subscriptionsArray.push(this.requestedChainOb.subscribe((requested) => this.getChainsListRequested(requested)));
         this.subscriptionsArray.push(this.chainListOb.subscribe((chainsList) => this.getChainsListFromRedux(chainsList)));
     }
 
     ngOnInit() {
-        this.chainForm = this._fb.group({
-            chainId: [
-                '',
-                Validators.compose([
-                    Validators.required,
-                    this.isInteger,
-                ])
-            ],
-            chainName: [
-                '',
-                Validators.compose([
-                    Validators.required,
-                ])
-            ],
-        });
     }
 
     ngAfterViewInit() {
-        this._persistService.watchForm('manageMember/manageChains', this.chainForm);
     }
 
     ngOnDestroy() {
@@ -96,7 +77,6 @@ export class ManageChainsComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     getChainsListRequested(requested): void {
-        // console.log('Sicav requested', requested);
         if (!requested) {
             ChainService.defaultRequestChainsList(this._chainService, this.ngRedux);
         }
@@ -106,7 +86,6 @@ export class ManageChainsComponent implements OnInit, AfterViewInit, OnDestroy {
         const listImu = fromJS(chainsList);
 
         this.chainsList = listImu.reduce((result, item) => {
-
             result.push({
                 chainId: item.get('chainId', 0),
                 chainName: item.get('chainName', ''),
@@ -143,167 +122,190 @@ export class ManageChainsComponent implements OnInit, AfterViewInit, OnDestroy {
         }
     }
 
-    editChain(chain: object) {
-        this.modelForm = new ChainModel();
-        for (const result in chain) {
-            switch (result) {
-                case 'chainId':
-                    this.modelForm.chainId = chain[result];
-                    break;
-                case 'chainName':
-                    this.modelForm.chainName = chain[result];
-                    break;
-            }
-        }
-        this.showSearchTab = false;
-        this.editForm = true;
-    }
+    /**
+     * Handle add chain
+     *
+     * @param {tabid} - The tab id that the form is in
+     *
+     * @return {void}
+     */
 
-    save(formValues: ChainInterface) {
-        this.showSearchTab = false; // reset
+    handleAddChain(tabId: number): void {
+        if (this.tabsControl[tabId]['formControl'].valid) {
+            const chainId = this.tabsControl[tabId]['formControl'].value.chainId;
+            const chainName = this.tabsControl[tabId]['formControl'].value.chainName;
 
-        if (this.editForm) {
-            // update
-            const asyncTaskPipe = this._chainService.updateChain(
-                {
-                    chainId: parseInt(formValues.chainId),
-                    chainName: formValues.chainName,
-                },
-                this.ngRedux);
-
-            this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
-                asyncTaskPipe,
-                (data) => {
-                    ChainService.setRequested(false, this.ngRedux);
-                    // console.log('success update chain', data); // success
-                    this.showSuccess('Chain has successfully been updated');
-                    this.resetForm();
-                    this.showSearchTab = true;
-                },
-                (data) => {
-                    console.log('error: ', data);
-                })
-            );
-        } else {
-            // insert
+            // Create a saga pipe
             const asyncTaskPipe = this._chainService.saveChain(
                 {
-                    chainId: parseInt(formValues.chainId),
-                    chainName: formValues.chainName,
+                    chainId,
+                    chainName
                 },
-                this.ngRedux);
+                this.ngRedux
+            );
 
             this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
                 asyncTaskPipe,
                 (data) => {
                     ChainService.setRequested(false, this.ngRedux);
-                    this.showSuccess('Chain has successfully been created');
-                    this.resetForm();
-                    this.showSearchTab = true;
+                    this.showSuccessResponse('Chain created');
                 },
                 (data) => {
-                    console.log('error: ', data);
-                })
-            );
+                    this.showErrorResponse(data);
+                }
+            ));
         }
     }
 
-    deleteChain(chain: any) {
-        // console.log(sicav.sicavID);
+    /**
+     * Handle edit chain
+     *
+     * @param {tabId}
+     *
+     * @return {void}
+     */
+    handleEditChain(tabId: number): void {
+        if (this.tabsControl[tabId]['formControl'].valid) {
+            const chainId = this.tabsControl[tabId]['formControl'].value.chainId;
+            const chainName = this.tabsControl[tabId]['formControl'].value.chainName;
+
+            // Create a saga pipe
+            const asyncTaskPipe = this._chainService.updateChain(
+                {
+                    chainId,
+                    chainName
+                },
+                this.ngRedux
+            );
+
+            this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
+                asyncTaskPipe,
+                (data) => {
+                    ChainService.setRequested(false, this.ngRedux);
+                    this.showSuccessResponse('Chain updated');
+                },
+                (data) => {
+                    this.showErrorResponse(data);
+                }
+            ));
+        }
+    }
+
+    /**
+     * Handle edit click
+     *
+     * @param index
+     */
+    handleEdit(index: number): void {
+        /* Check if the tab is already open */
+        let i;
+        for (i = 0; i < this.tabsControl.length; i++) {
+            if (this.tabsControl[i]['chainId'] === this.chainsList[index].chainId) {
+                this.setTabActive(i);
+                return;
+            }
+        }
+
+        /* Push the edit tab into the array */
+        const chain = this.chainsList[index];
+
+        this.tabsControl.push({
+            title: '<i class="fa fa-chain"></i> ' + chain.chainName,
+            chainId: chain.chainId,
+            formControl: new FormGroup(
+                {
+                    chainId: new FormControl(chain.chainId),
+                    chainName: new FormControl(chain.chainName)
+                }
+            ),
+            active: false
+        });
+
+        // Activate the new tab
+        this.setTabActive(this.tabsControl.length - 1);
+    }
+
+    /**
+     * Handle delete chain
+     *
+     * @param chain
+     */
+    handleDelete(chain: any): void {
         const asyncTaskPipe = this._chainService.deleteChain(
             {
                 chainId: chain.chainId
             },
-            this.ngRedux);
+            this.ngRedux
+        );
 
         this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
             asyncTaskPipe,
             (data) => {
                 ChainService.setRequested(false, this.ngRedux);
-                this.showSuccess('Your Chain has been deleted successfully!');
-                this.showSearchTab = true;
+                this.showSuccessResponse('Chain deleted');
             },
             (data) => {
-                console.log('error: ', data);
-            })
-        );
+                this.showErrorResponse(data);
+            }
+        ));
     }
 
-    resetForm(): void {
-        this.editForm = false;
-        this.showSearchTab = false;
-        this.modelForm = {};
-        this.chainForm.reset();
-    }
-
-    confirmModal(response): void {
-        this.showConfirmModal = false;
-        if (response === 1) {
-            this.deleteChain(this.chainToDelete);
+    /**
+    * Handle close tab click
+    *
+    * @param {index} number - the tab index to close
+    *
+    * @return {void}
+    */
+    closeTab(index: number): void {
+        if (!index && index !== 0) {
+            return;
         }
+
+        this.tabsControl = [
+            ...this.tabsControl.slice(0, index),
+            ...this.tabsControl.slice(index + 1, this.tabsControl.length)
+        ];
+
+        // Reset tabs
+        this.setTabActive(0);
+
+        return;
     }
 
-    /**
-     * Show Error Message
-     * ------------------
-     * Shows an error popup.
-     *
-     * @param  {message} string - the string to be shown in the message.
-     * @return {void}
-     */
-    showError(message) {
-        /* Show the error. */
-        this._alertsService.create('error', `
-              <table class="table grid">
-                  <tbody>
-                      <tr>
-                          <td class="text-center text-danger">${message}</td>
-                      </tr>
-                  </tbody>
-              </table>
-          `);
+    setTabActive(index: number): void {
+        const tabControlImu = fromJS(this.tabsControl);
+        const newTabControlImu = tabControlImu.map((item, thisIndex) => {
+            return item.set('active', thisIndex === index);
+        });
+
+        this.tabsControl = newTabControlImu.toJS();
     }
 
-    /**
-     * Show Warning Message
-     * ------------------
-     * Shows a warning popup.
-     *
-     * @param  {message} string - the string to be shown in the message.
-     * @return {void}
-     */
-    showWarning(message) {
-        /* Show the error. */
-        this._alertsService.create('warning', `
-              <table class="table grid">
-                  <tbody>
-                      <tr>
-                          <td class="text-center text-warning">${message}</td>
-                      </tr>
-                  </tbody>
-              </table>
-          `);
+    showErrorResponse(response) {
+        const message = _.get(response, '[1].Data[0].Message', '');
+
+        this.alertsService.create('error', `
+                    <table class="table grid">
+                        <tbody>
+                            <tr>
+                                <td class="text-center text-danger">${message}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    `);
     }
 
-    /**
-     * Show Success Message
-     * ------------------
-     * Shows an success popup.
-     *
-     * @param  {message} string - the string to be shown in the message.
-     * @return {void}
-     */
-    showSuccess(message) {
-        /* Show the message. */
-        this._alertsService.create('success', `
-              <table class="table grid">
-                  <tbody>
-                      <tr>
-                          <td class="text-center text-success">${message}</td>
-                      </tr>
-                  </tbody>
-              </table>
-          `);
+    showSuccessResponse(message) {
+        this.alertsService.create('success', `
+                    <table class="table grid">
+                        <tbody>
+                            <tr>
+                                <td class="text-center text-success">${message}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    `);
     }
 
     markForCheck() {
