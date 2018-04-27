a3b5a2c3e2ceb3afd6000766e1a12d7e61bffe9e
David Duong <david.duong@setl.io>
2018-04-27 18:49:31 +0200

    TG-XXX refact(holders/reducer): refactor a bit and implement holder detail's actions

--- ofi-store/ofi-reports/holders/reducer.ts
+++ ofi-store/ofi-reports/holders/reducer.ts
@@ -1,34 +1,24 @@
 /* Core/Redux imports. */
 import {Action} from 'redux';
-
 /* Local types. */
-import {AmHolders, AmHoldersDetails} from './model';
+import {AmHoldersDetails, HolderDetailStructure, OfiHolderState, ShareHolderItem} from './model';
 import * as ofiAmHoldersActions from './actions';
-import {immutableHelper} from '@setl/utils';
-import {List, fromJS, Map} from 'immutable';
+import {List} from 'immutable';
 import * as _ from 'lodash';
 
 /* Initial state. */
-const initialState: AmHolders = {
-    amHoldersList: {},
+const initialState: OfiHolderState = {
+    amHoldersList: List<AmHoldersDetails>(),
     requested: false,
+    holderDetailRequested: false,
+    shareHolderDetail: List<HolderDetailStructure>(),
 };
 
 /* Reducer. */
-export const OfiAmHoldersListReducer = function (state: AmHolders = initialState, action: Action) {
+export const OfiAmHoldersListReducer = (state: OfiHolderState = initialState, action: Action) => {
     switch (action.type) {
-        /* Set Coupon List. */
         case ofiAmHoldersActions.OFI_SET_AM_HOLDERS_LIST:
-
-            const data = _.get(action, 'payload[1].Data', []);    // use [] not {} for list and Data not Data[0]
-
-            if (data.Status !== 'Fail' && data.Message !== 'No holders found') {
-                const list = formatDataResponse(data);
-                return Object.assign({}, state, {
-                    list
-                });
-            }
-            return state;
+            return handleGetAmHolders(state, action);
 
         case ofiAmHoldersActions.OFI_SET_REQUESTED_AM_HOLDERS:
             return toggleRequestState(state, true);
@@ -36,43 +26,108 @@ export const OfiAmHoldersListReducer = function (state: AmHolders = initialState
         case ofiAmHoldersActions.OFI_CLEAR_REQUESTED_AM_HOLDERS:
             return toggleRequestState(state, false);
 
-        /* Default. */
+        case ofiAmHoldersActions.OFI_SET_HOLDER_DETAIL_REQUESTED:
+            return toggleHolderDetaiRequestState(state, true);
+
+        case ofiAmHoldersActions.OFI_CLEAR_HOLDER_DETAIL_REQUESTED:
+            return toggleHolderDetaiRequestState(state, false);
+
+        case ofiAmHoldersActions.OFI_GET_SHARE_HOLDER_DETAIL:
+            return handleGetShareHolderDetail(state, action);
+
         default:
             return state;
     }
 };
 
-function formatDataResponse(rawData: Array<any>): Array<AmHoldersDetails> {
-    const rawDataList = fromJS(rawData);
-    let i = 0;
+const handleGetAmHolders = (state, action) => {
+    const data = _.get(action, 'payload[1].Data', []);    // use [] not {} for list and Data not Data[0]
 
-    const response = Map(rawDataList.reduce(
-        function (result, item) {
-            result[i] = {
-                fundId: item.get('fundId'),
-                fundName: item.get('fundName'),
-                fundLei: item.get('fundLei'),
-                fundCurrency: item.get('fundCurrency'),
-                fundAum: item.get('fundAum'),
-                fundHolderNumber: item.get('fundHolderNumber'),
-                shareId: item.get('shareId'),
-                shareName: item.get('shareName'),
-                shareIsin: item.get('shareIsin'),
-                shareNav: item.get('shareNav'),
-                shareUnitNumber: item.get('shareUnitNumber'),
-                shareCurrency: item.get('shareCurrency'),
-                shareAum: item.get('shareAum'),
-                shareHolderNumber: item.get('shareHolderNumber'),
-                shareRatio: item.get('shareRatio'),
-            };
-            i++;
-            return result;
-        },
-        {}));
+    if (data.Status !== 'Fail' && data.Message !== 'No holders found') {
+        const amHoldersList = formatDataResponse(data);
 
-    return response.toJS();
+        return Object.assign({}, state, {
+            amHoldersList
+        });
+    }
+
+    return state;
+};
+
+const formatDataResponse = (rawData: Array<AmHoldersDetails>): List<AmHoldersDetails> => {
+    let response: List<AmHoldersDetails> = List();
+
+    rawData.forEach((iteratee) => {
+        const holderItem = {
+            isFund: iteratee.isFund,
+            fundId: iteratee.fundId,
+            fundName: iteratee.fundName,
+            fundLei: iteratee.fundLei,
+            fundCurrency: iteratee.fundCurrency,
+            fundAum: iteratee.fundAum,
+            fundHolderNumber: iteratee.fundHolderNumber,
+            shareId: iteratee.shareId,
+            shareName: iteratee.shareName,
+            shareIsin: iteratee.shareIsin,
+            shareNav: iteratee.shareNav,
+            shareUnitNumber: iteratee.shareUnitNumber,
+            shareCurrency: iteratee.shareCurrency,
+            shareAum: iteratee.shareAum,
+            shareHolderNumber: iteratee.shareHolderNumber,
+            shareRatio: iteratee.shareRatio
+        };
+
+        response = response.push(holderItem);
+    });
+
+    return response;
 }
 
-function toggleRequestState(state: AmHolders, requested: boolean): AmHolders {
+function toggleRequestState(state: OfiHolderState, requested: boolean): OfiHolderState {
     return Object.assign({}, state, {requested});
 }
+
+// Holder detail
+const toggleHolderDetaiRequestState = (state: OfiHolderState, holderDetailRequested: boolean): OfiHolderState => {
+    return Object.assign({}, state, {holderDetailRequested});
+};
+
+const handleGetShareHolderDetail = (state: OfiHolderState, action: Action): OfiHolderState => {
+    const response = _.get(action, 'payload[1].Data', []);
+
+    if (response.Status !== 'Fail' && response.Message !== 'No holder\'s detail found') {
+        let shareHolderDetail: List<HolderDetailStructure> = List();
+        let holders: List<ShareHolderItem> = List();
+
+        // Share holders
+        response.holders.forEach((holder) => {
+            const holderItem: ShareHolderItem = {
+                ranking: holder.ranking,
+                investorName: holder.investorName,
+                quantity: holder.quantity,
+                amount: holder.amount,
+                ratio: holder.ratio,
+            };
+
+            holders = holders.push(holderItem);
+        });
+
+        // Share info
+        shareHolderDetail = shareHolderDetail.push({
+            id: response.id,
+            name: response.name,
+            currency: response.currency,
+            isin: response.isin,
+            nav: response.nav,
+            unitNumber: response.unitNumber,
+            aum: response.aum,
+            holderNumber: response.holderNumber,
+            ratio: response.ratio,
+            holders,
+        });
+
+        return Object.assign({}, state, {shareHolderDetail});
+    }
+
+    return state;
+};
