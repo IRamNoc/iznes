6087e6980818cf03d3bb96fa0be3f0eba7c6f4ef
Phil Zeelte <philzeelte@IM00152.local>
2018-05-03 12:29:52 +0100

    moved enum into own file, rid of dependency on ofi main

---
+++ services/nav/NavEnum.ts
@@ -0,0 +1,12 @@
+export enum ValuationFrequencyEnum {
+    Daily,
+    TwiceAWeek,
+    Weekly,
+    TwiceAMonth,
+    Monthly,
+    Quarterly,
+    TwiceAYear,
+    Annually,
+    AtLeastAnnualy,
+    Other,
+}
