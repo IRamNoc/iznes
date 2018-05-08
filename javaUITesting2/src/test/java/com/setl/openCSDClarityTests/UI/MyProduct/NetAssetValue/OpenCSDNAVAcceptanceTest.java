4056f43bfd55995a0391b546f0e4b21687e0ef94
Jordan <jordan.miller@setl.io>
2018-05-08 14:53:40 +0100

    jordan: moved around methods and fixed tests

--- javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/MyProduct/NetAssetValue/OpenCSDNAVAcceptanceTest.java
+++ javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/MyProduct/NetAssetValue/OpenCSDNAVAcceptanceTest.java
@@ -1,34 +1,20 @@
-package com.setl.openCSDClarityTests.UI.ProductModule.Funds;
+package com.setl.openCSDClarityTests.UI.MyProduct.NetAssetValue;
 import com.setl.UI.common.SETLUtils.RepeatRule;
 import com.setl.UI.common.SETLUtils.ScreenshotRule;
 import com.setl.UI.common.SETLUtils.TestMethodPrinterRule;
 import custom.junit.runners.OrderedJUnit4ClassRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.LocalFileDetector;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
-import java.sql.SQLException;
 
 import static com.setl.UI.common.SETLUIHelpers.FundsDetailsHelper.*;
-import static com.setl.UI.common.SETLUIHelpers.MemberDetailsHelper.isElementPresent;
-import static com.setl.UI.common.SETLUIHelpers.PageHelper.verifyCorrectPageById;
-import static com.setl.UI.common.SETLUIHelpers.PageHelper.waitForNewFundShareTitle;
-import static com.setl.UI.common.SETLUIHelpers.PageHelper.waitForNewShareButton;
 import static com.setl.UI.common.SETLUIHelpers.SetUp.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 
 @RunWith(OrderedJUnit4ClassRunner.class)
