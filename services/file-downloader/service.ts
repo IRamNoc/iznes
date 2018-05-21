91b4223a1945312dfc5be7635e8100c5d12c6152
ming <mingrui.huang@setl.io>
2018-05-21 10:00:11 +0100

    TG-XXX refact: add file name

--- services/file-downloader/service.ts
+++ services/file-downloader/service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http';
 import {Inject, Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 import {AppConfig} from '../../appConfig/appConfig.model';
@@ -22,35 +22,43 @@ export class FileDownloader {
     downLoaderFile(body: any) {
         this.http.request('POST', this.getFileDownloadBaseUrl(), {
             body: body,
-            responseType: 'blob'
-        }).subscribe((data) => {
-
+            responseType: 'blob',
+            observe: 'response'
+        }).subscribe((response) => {
             // generate temporary link to down file.
-            const downloadUrl = window.URL.createObjectURL(data);
+            const downloadUrl = window.URL.createObjectURL(response.body);
             const a = document.createElement('a');
+            const fileName = response.headers.get('content-filename');
             document.body.appendChild(a);
             a.setAttribute('style', 'display: none');
             a.href = downloadUrl;
-            a.download = '';
+            a.download = fileName;
             a.click();
             window.URL.revokeObjectURL(downloadUrl);
             console.log(downloadUrl);
             a.remove(); // remove the element
+        }, (err) => {
         });
     }
 
     getDownLoaderUrl(body: any) {
-        const $downLoadUrl: Subject<string> = new Subject();
+        const $downLoadUrl: Subject<{ filename: string, url: string }> = new Subject();
 
         this.http.request('POST', this.getFileDownloadBaseUrl(), {
             body: body,
-            responseType: 'blob'
-        }).subscribe((data) => {
+            responseType: 'blob',
+            observe: 'response'
+        }).subscribe((response) => {
 
             // generate temporary link to down file.
-            const downloadUrl = window.URL.createObjectURL(data);
+            const downloadUrl = window.URL.createObjectURL(response.body);
+            const filename = response.headers.get('content-filename');
 
-            $downLoadUrl.next(downloadUrl);
+            $downLoadUrl.next({
+                filename,
+                url: downloadUrl
+            });
+        }, (err) => {
         });
 
         return $downLoadUrl;
