47f6215b3ae3a97bf90307c242c1218e4c192591
ming <mingrui.huang@setl.io>
2018-05-24 12:06:59 +0100

    TG-XXX fix: url access control

--- login-guard.service.ts
+++ login-guard.service.ts
@@ -1,39 +1,47 @@
-import {Inject, Injectable, OnDestroy, OnInit} from '@angular/core';
+import { Inject, Injectable, OnDestroy, OnInit } from '@angular/core';
 import {
     Router,
     CanActivate,
     ActivatedRouteSnapshot,
-    RouterStateSnapshot,
+    RouterStateSnapshot, CanDeactivate,
 
 } from '@angular/router';
-import {Observable} from 'rxjs/Observable';
-import {NgRedux, select} from '@angular-redux/store';
-import {ToasterService} from 'angular2-toaster';
-import {MyUserService} from '@setl/core-req-services';
-import {Subscription} from 'rxjs/Subscription';
-import {APP_CONFIG, AppConfig, immutableHelper} from '@setl/utils';
+import { Observable } from 'rxjs/Observable';
+import { NgRedux, select } from '@angular-redux/store';
+import { ToasterService } from 'angular2-toaster';
+import { MyUserService } from '@setl/core-req-services';
+import { Subscription } from 'rxjs/Subscription';
+import { APP_CONFIG, AppConfig, immutableHelper, MenuItem } from '@setl/utils';
 import * as _ from 'lodash';
 
 @Injectable()
 export class LoginGuardService implements CanActivate {
+
     isLogin: boolean;
     redirect = '';
 
     // List of observable subscription
-    subscriptionsArray: Array<Subscription> = [];
+    subscriptionsArray: Subscription[] = [];
+
+    userTypeStr: string;
 
     @select(['user', 'authentication', 'isLogin']) isLoginOb;
+    @select(['user', 'myDetail', 'userTypeStr']) userTypeOb;
 
     constructor(private ngRedux: NgRedux<any>,
                 @Inject(APP_CONFIG) public appConfig: AppConfig,
                 private toasterService: ToasterService,
                 private router: Router,
-                private _myUserService: MyUserService) {
+                private myUserService: MyUserService) {
         this.isLogin = false;
 
         // Reduce observable subscription
-        this.subscriptionsArray.push(this.isLoginOb.subscribe(isLogin => {
+        this.subscriptionsArray.push(this.isLoginOb.subscribe((isLogin) => {
             this.isLogin = isLogin;
+        }));
+
+        this.subscriptionsArray.push(this.userTypeOb.subscribe((userTypeStr) => {
+            this.userTypeStr = userTypeStr;
         }));
     }
 
@@ -46,20 +54,126 @@ export class LoginGuardService implements CanActivate {
             this.router.navigateByUrl('');
         } else {
             // refresh token.
-            this._myUserService.defaultRefreshToken(this.ngRedux);
+            this.myUserService.defaultRefreshToken(this.ngRedux);
 
             if (this.isMenuDisabled(state['url'])) {
-                this.toasterService.pop('warning', 'This page is not available in the current version.');
+                this.toasterService.pop(
+                    'warning',
+                    'This page is not available in the current version.');
 
                 return false;
             }
+
+            // check if the url is allow for the user
+            const applyRestrictUrl = this.appConfig.applyRestrictUrl || false;
+            if (applyRestrictUrl) {
+                const allowedUrls = this.getUserAllowUrl();
+                if (!isUrlAllow(allowedUrls, state['url'])) {
+                    return false;
+                }
+            }
         }
+
         return this.isLogin;
     }
 
     isMenuDisabled(url: string): boolean {
-        const disabledMenus: Array<string> = _.get(this.appConfig, ['menuSpec', 'disabled'], []);
+        const disabledMenus: string[] = _.get(this.appConfig, ['menuSpec', 'disabled'], []);
         return disabledMenus.indexOf(url) !== -1;
     }
 
+    getUserAllowUrl(): {static_link: string, dynamic_link: string}[] {
+        let allowUrls: {static_link: string, dynamic_link: string}[] = [];
+        const menuSpecs = this.appConfig.menuSpec;
+        const topProfileMenu = _.get(menuSpecs, ['top', 'profile', this.userTypeStr], []);
+        const sideMenu = _.get(menuSpecs, ['side', this.userTypeStr], []);
+        const disabledMenu = _.get(menuSpecs, 'disabled', []);
+        const nonMenuLink = _.get(this.appConfig, ['nonMenuLink', this.userTypeStr], []);
+
+        // top profile menu urls
+        topProfileMenu.forEach((item) => {
+            allowUrls.push({
+                static_link: item.router_link,
+                dynamic_link: item.dynamic_link,
+            });
+        });
+
+        // side menu urls
+        sideMenu.forEach((item) => {
+            allowUrls.push(...getSideMenuUrl(item));
+        });
+
+        // non-menu link
+        nonMenuLink.forEach((item) => {
+            allowUrls.push({
+                static_link: '',
+                dynamic_link: item,
+            });
+        });
+
+        // disabled
+        allowUrls = allowUrls.filter((item) => {
+            return !disabledMenu.includes(item.dynamic_link) &&
+                !disabledMenu.includes(item.static_link);
+        });
+
+        return allowUrls;
+    }
+
+}
+
+/**
+ * get side menu url recursively.
+ * @param {MenuItem} sideMenu
+ * @return {string[]}
+ */
+function getSideMenuUrl(sideMenu: MenuItem): {static_link: string, dynamic_link: string}[] {
+    const urls: {static_link: string, dynamic_link: string}[] = [];
+
+    if (typeof sideMenu.children === 'undefined') {
+        urls.push({
+            static_link: sideMenu.router_link,
+            dynamic_link: sideMenu.dynamic_link,
+        });
+        return urls;
+    }
+
+    for (const child of sideMenu.children) {
+        urls.push(...getSideMenuUrl(child));
+    }
+
+    return urls;
+}
+
+/**
+ * check if url is allowed.
+ * The router_link and dynamic_link will be check.
+ * dynamic_link is a regex pattern
+ * @param {{static_link: string; dynamic_link: string}[]} allowUrls
+ * @param {string} urlToCheck
+ * @return {boolean}
+ */
+function isUrlAllow(allowUrls: {static_link: string, dynamic_link: string}[],
+                    urlToCheck: string): boolean {
+    let rVal = false;
+
+    for (const item of allowUrls) {
+        // static link match
+        if (item.static_link === urlToCheck) {
+            rVal = true;
+            break;
+        }
+
+        // dynamic link match
+        const regExStr = _.get(item, 'dynamic_link', false);
+        if (regExStr) {
+            const dynamicLinkRegex = new RegExp(regExStr);
+            if (dynamicLinkRegex.test(urlToCheck)) {
+                rVal = true;
+                break;
+            }
+        }
+    }
+
+    return rVal;
 }
