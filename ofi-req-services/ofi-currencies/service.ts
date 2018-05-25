37f31cb99eb90bf06650f966459701a977910b70
David Duong <david.duong@setl.io>
2018-05-25 15:30:18 +0200

    TG-896 refact(ofi-req-services/ofi-currencies): improve how the service fetch currencies

--- ofi-req-services/ofi-currencies/service.ts
+++ ofi-req-services/ofi-currencies/service.ts
@@ -1,58 +1,46 @@
-import {Injectable} from '@angular/core';
-import {NgRedux} from '@angular-redux/store';
-import {MemberSocketService} from '@setl/websocket-service';
-import {createMemberNodeSagaRequest} from '@setl/utils/common';
-import {SagaHelper} from '@setl/utils';
-import {CurrencyActions} from '../../ofi-store/';
-import {OfiCurrenciesRequestBody} from './model';
+import { Injectable } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { Subject } from 'rxjs/Subject';
+import { MemberSocketService } from '@setl/websocket-service';
+import { createMemberNodeSagaRequest } from '@setl/utils/common';
+import { SagaHelper } from '@setl/utils';
+import { CurrencyActions } from '../../ofi-store/';
+import { OfiCurrenciesRequestBody } from './model';
 
 @Injectable()
 export class OfiCurrenciesService {
-    constructor(private memberSocketService: MemberSocketService) {
+    loaded: boolean;
+    unSubscribe: Subject<any> = new Subject();
+
+    // Observable
+    @select(['ofi', 'ofiCurrencies', 'loaded']) loadedObs$;
+
+    constructor(private memberSocketService: MemberSocketService, private ngRedux: NgRedux<any>) {
+        this.loadedObs$
+            .takeUntil(this.unSubscribe)
+            .subscribe(d => this.loaded = d);
     }
 
     /**
-     * Set or reset the flag which informs wheter currencies are already requested or not
-     *
-     * @param {boolean} boolValue
-     * @param {NgRedux<any>} ngRedux
+     * Get the list of currencies from the backend
      */
-    static setRequestedCurrencyList(boolValue: boolean, ngRedux: NgRedux<any>) {
-        const action = (!boolValue) ? CurrencyActions.loadCurrencies() : CurrencyActions.resetCurrencies();
+    getCurrencyList() {
+        if (this.loaded) {
+            return;
+        }
 
-        ngRedux.dispatch(action);
-    }
-
-    /**
-     * Fetch currencies list from the backend
-     *
-     * @param {OfiCurrenciesService} ofiCurrenciesService
-     * @param {NgRedux<any>} ngRedux
-     */
-    static defaultRequestCurrencyList(ofiCurrenciesService: OfiCurrenciesService, ngRedux: NgRedux<any>) {
-        ngRedux.dispatch(CurrencyActions.loadCurrencies());
-
-        const asyncTaskPipe = ofiCurrenciesService.requestCurrenciesList();
-
-        ngRedux.dispatch(SagaHelper.runAsync(
-            [CurrencyActions.GET_CURRENCIES],
-            [],
-            asyncTaskPipe,
-            {},
-        ));
-    }
-
-    /**
-     * Request the list of currencies
-     *
-     * @return {any}
-     */
-    requestCurrenciesList() {
         const messageBody: OfiCurrenciesRequestBody = {
             RequestName: 'getcurrencylist',
             token: this.memberSocketService.token,
         };
 
-        return createMemberNodeSagaRequest(this.memberSocketService, messageBody);
+        const asyncTaskPipe = createMemberNodeSagaRequest(this.memberSocketService, messageBody);
+
+        this.ngRedux.dispatch(SagaHelper.runAsync(
+            [CurrencyActions.LOAD_CURRENCIES],
+            [],
+            asyncTaskPipe,
+            {},
+        ));
     }
 }
