8f1f494ee5c76b9eba115a5f9d6329c6892443f5
David Duong <david.duong@setl.io>
2018-05-25 15:29:29 +0200

    TG-896 refact(ofi-store/ofi-currencies/reducer): refactor currency reducer accordingly to actions/model

--- ofi-store/ofi-currencies/reducer.ts
+++ ofi-store/ofi-currencies/reducer.ts
@@ -1,22 +1,14 @@
-import {List, Map} from 'immutable';
-import {Action, Reducer} from 'redux';
+import { List, Map } from 'immutable';
+import { Action, Reducer } from 'redux';
 import * as CurrencyActions from './actions';
-import {CurrencyState, CurrencyType} from './model';
+import { CurrencyState, CurrencyType } from './model';
 
 const initialState: CurrencyState = {
-    isRequested: false,
-    currencies: List<CurrencyType>()
+    loaded: false,
+    currencies: List<CurrencyType>(),
 };
 
-const handleLoadCurrencies = (state) => {
-    return Object.assign({}, state, {isRequested: true});
-};
-
-const handleResetCurrencies = (state) => {
-    return Object.assign({}, state, {isRequested: false});
-};
-
-const handleGetCurrencies = (state, action) => {
+const handleLoadCurrencies = (state, action) => {
     const response = action.payload[1].Data;
     let currencies = List();
 
@@ -31,19 +23,16 @@ const handleGetCurrencies = (state, action) => {
         });
     }
 
-    return Object.assign({}, state, {currencies});
+    return Object.assign({}, state, {
+        currencies,
+        loaded: true,
+    });
 };
 
 export const CurrencyReducer: Reducer<CurrencyState> = (state = initialState, action: Action) => {
     switch (action.type) {
         case CurrencyActions.LOAD_CURRENCIES:
-            return handleLoadCurrencies(state);
-
-        case CurrencyActions.RESET_CURRENCIES:
-            return handleResetCurrencies(state);
-
-        case CurrencyActions.GET_CURRENCIES:
-            return handleGetCurrencies(state, action);
+            return handleLoadCurrencies(state, action);
 
         default:
             return state;
