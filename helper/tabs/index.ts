090280c1d1d396fb042931154b93a57a7ef361a3
Phil Zeelte <philzeelte@IM00152.local>
2018-05-30 10:57:52 +0100

    renamed a method

--- helper/tabs/index.ts
+++ helper/tabs/index.ts
@@ -1,7 +1,7 @@
 import {ClrTabs} from '@clr/angular';
 
 export class ClrTabsHelper {
-    static getActiveTab(tabsRef: ClrTabs): number {
+    static getActiveTabIndex(tabsRef: ClrTabs): number {
         return tabsRef.tabLinkDirectives.toArray().findIndex(x => x.active);
     }
 
--- index.ts
+++ index.ts
@@ -6,26 +6,36 @@ export {
     SagaHelper,
 };
 
-export { MoneyValuePipe } from './pipes';
-export { SetlPipesModule } from './pipes';
+export {MoneyValuePipe} from './pipes';
+export {SetlPipesModule} from './pipes';
 
-export { SetlComponentsModule } from './components';
-export { ConfirmationService } from './components/jaspero-confirmation/confirmations.service';
+export {SetlComponentsModule} from './components';
+export {ConfirmationService} from './components/jaspero-confirmation/confirmations.service';
 
-export { SelectModule } from './components/ng2-select/select.module';
-export { DpDatePickerModule } from './components/ng2-date-picker/date-picker.module';
+export {SelectModule} from './components/ng2-select/select.module';
+export {DpDatePickerModule} from './components/ng2-date-picker/date-picker.module';
 
-export { SetlDirectivesModule } from './directives';
-export { NumberConverterService } from './services/number-converter/service';
-export { BlockchainContractService } from './services/blockchain-contract/service';
-export { NavHelperService } from './services/nav/service';
-export { ConditionType, ArrangementActionType } from './services/blockchain-contract/model';
-export { SetlServicesModule } from './services';
+export {SetlDirectivesModule} from './directives';
+export {NumberConverterService} from './services/number-converter/service';
+export {BlockchainContractService} from './services/blockchain-contract/service';
+export {NavHelperService} from './services/nav/service';
+export {ConditionType, ArrangementActionType} from './services/blockchain-contract/model';
+export {FileDownloader} from './services/file-downloader/service';
+export {SetlServicesModule} from './services';
+export {LogService} from './services/log/service';
+export {LogServiceMock} from './services/log/service-mock';
 
-export { walletHelper, WalletTxHelper, immutableHelper, mDateHelper, commonHelper, WalletTxHelperModel } from './helper';
-export { ClrTabsHelper } from './helper/tabs';
+export {walletHelper, WalletTxHelper, immutableHelper, mDateHelper, commonHelper, WalletTxHelperModel} from './helper';
+export {ClrTabsHelper} from './helper/tabs';
 
-export { APP_CONFIG } from './appConfig/appConfig';
-export { AppConfig, MenuSpec, MenuItem } from './appConfig/appConfig.model';
+export {APP_CONFIG} from './appConfig/appConfig';
+export {AppConfig, MenuSpec, MenuItem} from './appConfig/appConfig.model';
 
-export { FormItem, FormItemDropdown, FormItemStyle, FormItemType, DynamicFormsModule, DynamicFormsValidator } from './components/dynamic-forms';
+export {
+    FormItem,
+    FormItemDropdown,
+    FormItemStyle,
+    FormItemType,
+    DynamicFormsModule,
+    DynamicFormsValidator
+} from './components/dynamic-forms';
