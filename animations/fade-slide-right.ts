af0a90fb97161b029b85524704f7177cab4a1c4b
Thomas Bianchi <thomas.bianchi@setl.io>
2018-05-31 15:23:11 +0200

    TG-1048 fix(layouts): Permit scroll while page is animating after load

--- animations/fade-slide-right.ts
+++ animations/fade-slide-right.ts
@@ -3,46 +3,23 @@ import {trigger, animate, transition, style, query} from '@angular/animations';
 const slideDistance = 20;
 const slideDuration = 1500;
 
-const FullSizeAnimation = [
+const animation = [
     style({
-        position: 'fixed',
-        width: 'calc(100% - 230px)',
         opacity: 0,
         transform: `translateX(${slideDistance}px)`,
     }),
     animate(`${slideDuration}ms ease`, style({
-        position: 'fixed',
-        width: 'calc(100% - 230px)',
         opacity: 1,
         transform: 'translateX(0%)'
-    })),
-    style({
-        position: 'relative',
-    })
-];
-
-const SmallSizeAnimation = [
-    style({
-        position: 'fixed',
-        width: 'calc(100% - 80px)',
-        opacity: 0,
-        transform: `translateX(${slideDistance}px)`,
-    }),
-    animate(`${slideDuration}ms ease`, style({
-        position: 'fixed',
-        width: 'calc(100% - 80px)',
-        opacity: 1,
-        transform: 'translateX(0%)'
-    })),
-    style({
-        position: 'relative',
-    })
+    }))
 ];
 
 export const FadeSlideRight =
     trigger('routerTransition', [
         transition('* <=> *', [
-            query('main:not(.menuHideContent) .fade-slide-right', FullSizeAnimation, {optional: true}),
-            query('.menuHideContent .fade-slide-right', SmallSizeAnimation, {optional: true})
+            query('.router-container', style({overflow : 'hidden'})),
+            query('.fade-slide-right', animation, {optional: true}),
+            query('.router-container', style({overflow : 'visible'}))
         ])
-    ]);
+    ])
+;
