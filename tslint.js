9dfe1202288f502334980ef527b95394651c4493
Ollie Kett <ollie.kett@setl.io>
2018-06-01 12:46:04 +0100

    Initial commit

---
+++ tslint.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    "extends": "tslint-config-airbnb",
+    "rules": {
+        "indent": [
+            true,
+            "space"
+        ],
+        "ter-indent": [
+            true,
+            4
+        ],
+        "import-blacklist": [
+            true,
+            "rxjs"
+        ],
+        "directive-selector": [
+            true,
+            "element",
+            "app",
+            "kebab-case"
+        ],
+        "max-line-length": [
+            true,
+            120
+        ],
+    }
+};
