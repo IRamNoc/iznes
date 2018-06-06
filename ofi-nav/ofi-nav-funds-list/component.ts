55fe865a9c3a016dff43285e3ac9405785ac2a95
David Duong <david.duong@setl.io>
2018-06-06 12:28:33 +0200

    TG-1137-1141 refact(ofi-nav-funds-list): implement global nav upload

--- ofi-nav/ofi-nav-funds-list/component.ts
+++ ofi-nav/ofi-nav-funds-list/component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, OnDestroy, ChangeDetectorRef, Inject} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {ChangeDetectorRef, Component, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
-import {select, NgRedux} from '@angular-redux/store';
+import {NgRedux, select} from '@angular-redux/store';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
 import 'rxjs/add/operator/debounceTime';
@@ -14,18 +14,16 @@ import {OfiManageNavPopupService} from '../ofi-manage-nav-popup/service';
 import {OfiCorpActionService} from '../../ofi-req-services/ofi-corp-actions/service';
 import {OfiNavService} from '../../ofi-req-services/ofi-product/nav/service';
 import {
-    ofiSetCurrentNavFundsListRequest,
     clearRequestedNavFundsList,
     getOfiNavFundsListCurrentRequest,
-    ofiSetCurrentNavFundViewRequest,
-    clearRequestedNavFundView,
-    getOfiNavFundViewCurrentRequest
+    ofiSetCurrentNavFundsListRequest,
+    ofiSetCurrentNavFundViewRequest
 } from '../../ofi-store/ofi-product/nav';
-import {CurrencyValue} from '../../ofi-product/fund-share/fundShareValue';
 import {CurrencyEnum} from '../../ofi-product/fund-share/FundShareEnum';
-import {NumberConverterService, MoneyValuePipe, APP_CONFIG, AppConfig, FileDownloader} from '@setl/utils';
+import {APP_CONFIG, AppConfig, FileDownloader, MoneyValuePipe, NumberConverterService} from '@setl/utils';
 
 import {MultilingualService} from '@setl/multilingual';
+import {AlertsService} from "@setl/jaspero-ng2-alerts/src/alerts.service";
 
 @Component({
     selector: 'app-nav-manage-list',
@@ -48,17 +46,20 @@ export class OfiNavFundsList implements OnInit, OnDestroy {
         disableKeypress: true,
         locale: null
     };
+    isNavUploadModalDisplayed: boolean;
+    navCsvFile: any;
+
+    @ViewChild('globalNavCsvFile')
+    globalNavCsvFile: any;
 
     navPopupMode: model.NavPopupMode = model.NavPopupMode.ADD;
 
     appConfig: AppConfig;
-
-    private subscriptionsArray: Subscription[] = [];
-
     @select(['ofi', 'ofiProduct', 'ofiManageNav', 'ofiNavFundsList', 'requested']) navRequestedOb: Observable<any>;
     @select(['ofi', 'ofiProduct', 'ofiManageNav', 'ofiNavFundsList', 'navFundsList']) navListOb: Observable<any>;
     @select(['user', 'authentication', 'token']) tokenOb;
     @select(['user', 'myDetail', 'userId']) userOb;
+    private subscriptionsArray: Subscription[] = [];
 
     constructor(private router: Router,
                 private redux: NgRedux<any>,
@@ -68,111 +69,19 @@ export class OfiNavFundsList implements OnInit, OnDestroy {
                 private numberConverterService: NumberConverterService,
                 private moneyPipe: MoneyValuePipe,
                 private popupService: OfiManageNavPopupService,
+                private alertService: AlertsService,
                 private _fileDownloader: FileDownloader,
                 public _translate: MultilingualService,
                 @Inject(APP_CONFIG) appConfig: AppConfig) {
         this.appConfig = appConfig;
-
+        this.isNavUploadModalDisplayed = false;
+        this.navCsvFile = null;
     }
 
     ngOnInit() {
         this.initDataTypes();
         this.initSearchForm();
         this.initSubscriptions();
-    }
-
-    private initSubscriptions(): void {
-        this.subscriptionsArray.push(this.navRequestedOb.subscribe(requested => {
-            this.requestNavList(requested);
-        }));
-        this.subscriptionsArray.push(this.navListOb.subscribe(navList => {
-            this.updateNavList(navList);
-        }));
-        this.subscriptionsArray.push(this.tokenOb.subscribe(token => {
-            this.socketToken = token;
-        }));
-        this.subscriptionsArray.push(this.userOb.subscribe(userId => {
-            this.userId = userId;
-        }));
-    }
-
-    private initSearchForm(): void {
-        this.searchForm = new FormGroup({
-            shareName: new FormControl(''),
-            dateType: new FormControl([this.dateTypes[0]]),
-            date: new FormControl(moment().format('YYYY-MM-DD'))
-        });
-
-        this.subscriptionsArray.push(this.searchForm.valueChanges.debounceTime(1000).subscribe(() => {
-            this.clearRequestedList();
-        }));
-    }
-
-    /**
-     * request the nav list
-     * @param requested boolean
-     * @return void
-     */
-    private requestNavList(requested: boolean): void {
-        if (requested) return;
-
-        this.changeDetectorRef.detectChanges();
-
-        const requestData = this.getRequestNavListData();
-
-        OfiNavService.defaultRequestNavList(this.ofiNavService, this.redux, requestData);
-
-        this.redux.dispatch(ofiSetCurrentNavFundsListRequest(requestData));
-    }
-
-    private getRequestNavListData(): any {
-        return {
-            fundName: this.searchForm.value.shareName,
-            navDateField: this.searchForm.value.dateType[0].id,
-            navDate: `${this.searchForm.value.date} 00:00:00`
-        }
-    }
-
-    /**
-     * update the nav list
-     * @param navList NavList
-     * @return void
-     */
-    private updateNavList(navList: model.NavModel[]): void {
-        this.navListItems = _.orderBy(this.processNavList(navList),['shareId'],['desc']);
-        this.changeDetectorRef.markForCheck();
-    }
-
-    private processNavList(navList: model.NavModel[]): model.NavModel[] {
-        const uniques = _.uniqBy(navList, (e) => {
-            return e.fundShareName;
-        });
-
-        _.forEach(uniques, (item: model.NavModel) => {
-            if(item.navValidated) {
-                item.nav = item.navValidated;
-            } else if(item.navTechnical) {
-                item.nav = item.navTechnical;
-            } else {
-                item.nav = item.navEstimated;
-            }
-        });
-
-        return uniques;
-    }
-
-    private navToFrontEndString(nav: number): string {
-        return this.moneyPipe.transform(this.numberConverterService.toFrontEnd(nav));
-    }
-
-    private initDataTypes(): void {
-        this.dateTypes = [{
-            id: 'navDate',
-            text: 'NAV Date'
-        }, {
-            id: 'navPubDate',
-            text: 'NAV Published Date'
-        }];
     }
 
     getCurrencySymbol(currency: number): string {
@@ -273,4 +182,171 @@ export class OfiNavFundsList implements OnInit, OnDestroy {
         }
     }
 
+    handleUploadNavSubmitClick() {
+        if (this.navCsvFile) {
+            const reader = new FileReader();
+            reader.readAsText(this.navCsvFile);
+
+            reader.onload = () => {
+                const payload = {
+                    navData: JSON.stringify(reader.result),
+                };
+
+                return this.ofiNavService.uploadNavFile(
+                    'global',
+                    payload,
+                    this.redux,
+                    res => this.handleUploadNavSuccess(res),
+                    err => this.handleUploadNavFail(err),
+                );
+            };
+        }
+    }
+
+    handleUploadNavSuccess(res) {
+        this.resetNavUploadModal();
+        const successMessage = res[1].Data[0].Message;
+
+        this.alertService.create(
+            'success',
+            `
+                <table class="table grid">
+                    <tbody>
+                        <tr>
+                            <td class="text-center text-success">${successMessage}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            `,
+            {},
+            'NAVs Upload - Success',
+        );
+    }
+
+    handleUploadNavFail(err) {
+        if (err) {
+            this.resetNavUploadModal();
+            const errorMessage = err[1].Data[0].Message;
+
+            this.alertService.create(
+                'error',
+                `
+                <table class="table grid">
+                    <tbody>
+                        <tr>
+                            <td class="text-center text-danger">NAVs upload has failed for the following reason:</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center text-danger">${errorMessage}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            `,
+                {},
+                'NAVs Upload - Error',
+            );
+        }
+    }
+
+    resetNavUploadModal() {
+        this.globalNavCsvFile.nativeElement.value = '';
+        this.navCsvFile = null;
+        this.isNavUploadModalDisplayed = false;
+        this.changeDetectorRef.markForCheck();
+    }
+
+    private initSubscriptions(): void {
+        this.subscriptionsArray.push(this.navRequestedOb.subscribe(requested => {
+            this.requestNavList(requested);
+        }));
+        this.subscriptionsArray.push(this.navListOb.subscribe(navList => {
+            this.updateNavList(navList);
+        }));
+        this.subscriptionsArray.push(this.tokenOb.subscribe(token => {
+            this.socketToken = token;
+        }));
+        this.subscriptionsArray.push(this.userOb.subscribe(userId => {
+            this.userId = userId;
+        }));
+    }
+
+    private initSearchForm(): void {
+        this.searchForm = new FormGroup({
+            shareName: new FormControl(''),
+            dateType: new FormControl([this.dateTypes[0]]),
+            date: new FormControl(moment().format('YYYY-MM-DD'))
+        });
+
+        // this.subscriptionsArray.push(this.searchForm.valueChanges.debounceTime(1000).subscribe(() => {
+        this.subscriptionsArray.push(this.searchForm.valueChanges.subscribe(() => {
+            this.clearRequestedList();
+        }));
+    }
+
+    /**
+     * request the nav list
+     * @param requested boolean
+     * @return void
+     */
+    private requestNavList(requested: boolean): void {
+        if (requested) return;
+
+        this.changeDetectorRef.detectChanges();
+
+        const requestData = this.getRequestNavListData();
+
+        OfiNavService.defaultRequestNavList(this.ofiNavService, this.redux, requestData);
+
+        this.redux.dispatch(ofiSetCurrentNavFundsListRequest(requestData));
+    }
+
+    private getRequestNavListData(): any {
+        return {
+            fundName: this.searchForm.value.shareName,
+            navDateField: this.searchForm.value.dateType[0].id,
+            navDate: `${this.searchForm.value.date} 00:00:00`
+        }
+    }
+
+    /**
+     * update the nav list
+     * @param navList NavList
+     * @return void
+     */
+    private updateNavList(navList: model.NavModel[]): void {
+        this.navListItems = _.orderBy(this.processNavList(navList), ['shareId'], ['desc']);
+        this.changeDetectorRef.markForCheck();
+    }
+
+    private processNavList(navList: model.NavModel[]): model.NavModel[] {
+        const uniques = _.uniqBy(navList, (e) => {
+            return e.fundShareName;
+        });
+
+        _.forEach(uniques, (item: model.NavModel) => {
+            if (item.navValidated) {
+                item.nav = item.navValidated;
+            } else if (item.navTechnical) {
+                item.nav = item.navTechnical;
+            } else {
+                item.nav = item.navEstimated;
+            }
+        });
+
+        return uniques;
+    }
+
+    private navToFrontEndString(nav: number): string {
+        return this.moneyPipe.transform(this.numberConverterService.toFrontEnd(nav));
+    }
+
+    private initDataTypes(): void {
+        this.dateTypes = [{
+            id: 'navDate',
+            text: 'NAV Date'
+        }, {
+            id: 'navPubDate',
+            text: 'NAV Published Date'
+        }];
+    }
 }
