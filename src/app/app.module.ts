ddfdbc98d52f7b8b9139e204ddbed09518b10a5a
Phil Zeelte <philzeelte@IM00152.local>
2018-06-18 16:41:20 +0100

    added core-account-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ErrorHandler, NgModule} from '@angular/core';
-import {ClarityModule} from '@clr/angular';
-import {RouterModule} from '@angular/router';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
-import {LoginGuardService} from '@setl/core-login';
-import {HttpModule} from '@angular/http';
-import {SidebarModule} from 'ng-sidebar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { ClarityModule } from '@clr/angular';
+import { RouterModule } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { LoginGuardService } from '@setl/core-login';
+import { HttpModule } from '@angular/http';
+import { SidebarModule } from 'ng-sidebar';
 
-import {GlobalErrorHandler} from '../error_handler';
+import { GlobalErrorHandler } from '../error_handler';
 
 import {
     APP_CONFIG,
@@ -16,12 +16,12 @@ import {
     SetlComponentsModule,
     SetlDirectivesModule,
     SetlPipesModule,
-    SetlServicesModule
+    SetlServicesModule,
 } from '@setl/utils';
 
-import {NgxPaginationModule} from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
 /* Connection Services */
-import {MemberSocketService, WalletNodeSocketService} from '@setl/websocket-service';
+import { MemberSocketService, WalletNodeSocketService } from '@setl/websocket-service';
 /* Core services*/
 import {
     AccountsService,
@@ -38,45 +38,46 @@ import {
     WalletnodeChannelService,
     WalletNodeRequestService,
     WalletnodeTxService,
-    NodeAlertsService
+    NodeAlertsService,
 } from '@setl/core-req-services';
 /* Routes. */
-import {ROUTES} from './app.routes';
+import { ROUTES } from './app.routes';
 /* SETL Modules. */
-import {SetlMessagesModule} from '@setl/core-messages';
-import {SetlAccountModule} from '@setl/core-account';
-import {SetlBalancesModule} from '@setl/core-balances';
-import {UserAdminModule} from '@setl/core-useradmin';
-import {AssetServicingModule} from '@setl/asset-servicing';
-import {PermissionGridModule} from '@setl/permission-grid';
-import {CoreManageMemberModule} from '@setl/core-manage-member';
-import {CorpActionsModule} from '@setl/core-corp-actions';
-import {ConnectionsModule} from '@setl/core-connections/connections.module';
-import {SetlLayoutModule} from '@setl/core-layout';
-import {LogService} from'@setl/utils';
+import { SetlMessagesModule } from '@setl/core-messages';
+import { SetlAccountModule } from '@setl/core-account';
+import { SetlBalancesModule } from '@setl/core-balances';
+import { UserAdminModule } from '@setl/core-useradmin';
+import { CoreAccountAdminModule } from '@setl/core-account-admin';
+import { AssetServicingModule } from '@setl/asset-servicing';
+import { PermissionGridModule } from '@setl/permission-grid';
+import { CoreManageMemberModule } from '@setl/core-manage-member';
+import { CorpActionsModule } from '@setl/core-corp-actions';
+import { ConnectionsModule } from '@setl/core-connections/connections.module';
+import { SetlLayoutModule } from '@setl/core-layout';
+import { LogService } from'@setl/utils';
 /* OFI Modules */
-import {OfiProductModule} from '@ofi/product';
-import {OfiMainModule} from '@ofi/ofi-main';
+import { OfiProductModule } from '@ofi/product';
+import { OfiMainModule } from '@ofi/ofi-main';
 /* Internal Components. */
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 /**
  * Vendor Modules and Services.
  */
-import {ToasterModule, ToasterService} from 'angular2-toaster';
-import {JasperoAlertsModule} from '@setl/jaspero-ng2-alerts';
-import {FileViewerModule} from '@setl/core-fileviewer';
+import { ToasterModule, ToasterService } from 'angular2-toaster';
+import { JasperoAlertsModule } from '@setl/jaspero-ng2-alerts';
+import { FileViewerModule } from '@setl/core-fileviewer';
 /**
  * App main state
  */
-import {NgRedux, NgReduxModule} from '@angular-redux/store';
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
 
-import {appSaga, createAppStore, sagaMiddleware} from './store/app.store';
+import { appSaga, createAppStore, sagaMiddleware } from './store/app.store';
 
-import {AppState} from './store/app.reducer';
+import { AppState } from './store/app.reducer';
 /**
  * Environment
  */
-import {environment} from '../environments/environment';
+import { environment } from '../environments/environment';
 
 import './shared/rxjs-operators';
 
@@ -88,7 +89,7 @@ export function memberSocketServiceFactory() {
     memberSocketService.updateConfig(
         environment.MEMBER_NODE_CONNECTION.host,
         environment.MEMBER_NODE_CONNECTION.port,
-        environment.MEMBER_NODE_CONNECTION.path
+        environment.MEMBER_NODE_CONNECTION.path,
     );
 
     return memberSocketService;
@@ -97,7 +98,7 @@ export function memberSocketServiceFactory() {
 @NgModule({
     declarations: [
         /* Components. */
-        AppComponent
+        AppComponent,
     ],
     imports: [
         /* Vendor/Angular */
@@ -116,6 +117,7 @@ export function memberSocketServiceFactory() {
 
         /* External modules */
         UserAdminModule,
+        CoreAccountAdminModule,
         SetlMessagesModule,
         SetlAccountModule,
         OfiProductModule,
@@ -135,18 +137,18 @@ export function memberSocketServiceFactory() {
     providers: [
         {
             provide: ErrorHandler,
-            useClass: GlobalErrorHandler
+            useClass: GlobalErrorHandler,
         },
-        {provide: LocationStrategy, useClass: HashLocationStrategy},
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
 
         {
             provide: MemberSocketService,
-            useFactory: memberSocketServiceFactory
+            useFactory: memberSocketServiceFactory,
         },
 
         {
             provide: APP_CONFIG,
-            useValue: environment
+            useValue: environment,
         },
 
         NodeAlertsService,
@@ -167,9 +169,9 @@ export function memberSocketServiceFactory() {
         FileService,
         PdfService,
         LoginGuardService,
-        LogService
+        LogService,
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {
     constructor(ngRedux: NgRedux<AppState>) {
