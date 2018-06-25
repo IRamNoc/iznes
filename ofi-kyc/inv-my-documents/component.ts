9589b39b593fa6d7989397924d4646f0552df757
Laurent HAUCK <laurent.hauck@setl.io>
2018-06-25 11:49:01 +0200

    inv my docs

--- ofi-kyc/inv-my-documents/component.ts
+++ ofi-kyc/inv-my-documents/component.ts
@@ -14,38 +14,9 @@ import {MultilingualService} from '@setl/multilingual';
 })
 export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewInit {
 
-    private formGroup:FormGroup;
-
-    @ViewChild('upload1') upload1: ElementRef;
-    @ViewChild('upload2') upload2: ElementRef;
-    @ViewChild('upload3') upload3: ElementRef;
-    @ViewChild('upload4') upload4: ElementRef;
-    @ViewChild('upload5') upload5: ElementRef;
-    @ViewChild('upload6') upload6: ElementRef;
-    @ViewChild('upload7') upload7: ElementRef;
-    @ViewChild('upload8') upload8: ElementRef;
-    @ViewChild('upload9') upload9: ElementRef;
-    @ViewChild('upload10') upload10: ElementRef;
-    @ViewChild('upload11') upload11: ElementRef;
-    @ViewChild('upload12') upload12: ElementRef;
-    @ViewChild('upload13') upload13: ElementRef;
+    private uploadMyDocumentsForm:FormGroup;
 
     private subscriptions: Array<any> = [];
-
-    private shareAllDocsToAllAM = false;
-    shareUpload1 = false;
-    shareUpload2 = false;
-    shareUpload3 = false;
-    shareUpload4 = false;
-    shareUpload5 = false;
-    shareUpload6 = false;
-    shareUpload7 = false;
-    shareUpload8 = false;
-    shareUpload9 = false;
-    shareUpload10 = false;
-    shareUpload11 = false;
-    shareUpload12 = false;
-    shareUpload13 = false;
 
     allUploadsFiles: Array<any> = [];
 
@@ -54,9 +25,38 @@ export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewI
         private _translate: MultilingualService,
         private _ngRedux: NgRedux<any>,
     ) {
-        this.formGroup = new FormGroup({
+        this.uploadMyDocumentsForm = new FormGroup({
+            shareAll: new FormControl([]),
             upload1: new FormControl([]),
+            shareUpload1: new FormControl([]),
+            upload2: new FormControl([]),
+            shareUpload2: new FormControl([]),
+            upload3: new FormControl([]),
+            shareUpload3: new FormControl([]),
+            upload4: new FormControl([]),
+            shareUpload4: new FormControl([]),
+            upload5: new FormControl([]),
+            shareUpload5: new FormControl([]),
+            upload6: new FormControl([]),
+            shareUpload6: new FormControl([]),
+            upload7: new FormControl([]),
+            shareUpload7: new FormControl([]),
+            upload8: new FormControl([]),
+            shareUpload8: new FormControl([]),
+            upload9: new FormControl([]),
+            shareUpload9: new FormControl([]),
+            upload10: new FormControl([]),
+            shareUpload10: new FormControl([]),
+            upload11: new FormControl([]),
+            shareUpload11: new FormControl([]),
+            upload12: new FormControl([]),
+            shareUpload12: new FormControl([]),
+            upload13: new FormControl([]),
+            shareUpload13: new FormControl([]),
         });
+
+
+        this.subscriptions.push(this.uploadMyDocumentsForm.valueChanges.subscribe((form) => this.getUpload(form)));
     }
 
     ngOnInit() {
@@ -67,40 +67,8 @@ export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewI
 
     }
 
-    assignShareAllDocsToAllAM() {
-        this.shareUpload1 = this.shareAllDocsToAllAM;
-        this.shareUpload2 = this.shareAllDocsToAllAM;
-        this.shareUpload3 = this.shareAllDocsToAllAM;
-        this.shareUpload4 = this.shareAllDocsToAllAM;
-        this.shareUpload5 = this.shareAllDocsToAllAM;
-        this.shareUpload6 = this.shareAllDocsToAllAM;
-        this.shareUpload7 = this.shareAllDocsToAllAM;
-        this.shareUpload8 = this.shareAllDocsToAllAM;
-        this.shareUpload9 = this.shareAllDocsToAllAM;
-        this.shareUpload10 = this.shareAllDocsToAllAM;
-        this.shareUpload11 = this.shareAllDocsToAllAM;
-        this.shareUpload12 = this.shareAllDocsToAllAM;
-        this.shareUpload13 = this.shareAllDocsToAllAM;
-    }
-
     getUpload(event) {
         console.log(event);
-    }
-
-    processUpload(field, file) {
-        // https://stackoverflow.com/questions/47936183/angular-5-file-upload
-        this.allUploadsFiles[field] = {
-            name: field,
-            file: file,
-        };
-        console.log(field, this.allUploadsFiles[field].file);
-    }
-
-    removeFile(fileName) {
-        if (this.allUploadsFiles[fileName]) {
-            this[fileName].nativeElement.value = '';
-            delete this.allUploadsFiles[fileName];
-        }
     }
 
     /* On Destroy. */
