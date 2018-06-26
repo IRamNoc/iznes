171e678b8dba09de09ea5029617c6fa9d7008186
Laurent HAUCK <laurent.hauck@setl.io>
2018-06-26 11:09:38 +0200

    my docs

--- ofi-kyc/inv-my-documents/component.ts
+++ ofi-kyc/inv-my-documents/component.ts
@@ -7,6 +7,11 @@ import {Observable} from 'rxjs/Observable';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {MultilingualService} from '@setl/multilingual';
 
+import * as _ from 'lodash';
+import * as SagaHelper from '@setl/utils/sagaHelper';
+import { ToasterService } from 'angular2-toaster';
+import { FileService } from '@setl/core-req-services/file/file.service';
+
 @Component({
     styleUrls: ['./component.scss'],
     templateUrl: './component.html',
@@ -18,14 +23,24 @@ export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewI
 
     private subscriptions: Array<any> = [];
 
-    allUploadsFiles: Array<any> = [];
+    allUploadsFiles: any = [];
     nbUploads = 13;
 
     constructor(
         private _changeDetectorRef: ChangeDetectorRef,
         private _translate: MultilingualService,
-        private _ngRedux: NgRedux<any>,
+        private fileService: FileService,
+        private toaster: ToasterService,
+        private ngRedux: NgRedux<any>,
     ) {
+        for (let i = 0; i < this.nbUploads; i++) {
+            this.allUploadsFiles[i+1] = {
+                    fileID: 0,
+                    hash: '',
+                    name: '',
+            };
+        }
+
         this.uploadMyDocumentsForm = new FormGroup({
             shareAll: new FormControl(false),
             upload1: new FormControl([]),
@@ -55,9 +70,6 @@ export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewI
             upload13: new FormControl([]),
             shareUpload13: new FormControl(false),
         });
-
-
-        this.subscriptions.push(this.uploadMyDocumentsForm.valueChanges.subscribe((form) => this.getUpload(form)));
     }
 
     ngOnInit() {
@@ -70,21 +82,105 @@ export class OfiInvMyDocumentsComponent implements OnDestroy, OnInit, AfterViewI
             if (allFileDrop[i] && allFileDrop[i] !== undefined) {
                 if (allFileDrop[i].tagName === 'DIV') {
                     allFileDrop[i].style.zIndex = '0';
+                    allFileDrop[i].style.pointerEvents = 'none';
                 }
             }
         }
     }
 
-    assignShareAll() {
+    assignAllToggles() {
         for (let i=0; i < this.nbUploads; i++) {
             this.uploadMyDocumentsForm.get('shareUpload' + (i+1)).patchValue(this.uploadMyDocumentsForm.get('shareAll').value, { emitEvent: false });
         }
         this._changeDetectorRef.markForCheck();
     }
 
-    getUpload(formResults) {
-        console.log(event);
+    checkToggles() {
+        let isAllChecked = true;
+        for (let i=0; i < this.nbUploads; i++) {
+            if (this.uploadMyDocumentsForm.get('shareUpload' + (i+1)).value === false) {
+                isAllChecked = false;
+            }
+        }
+        if (isAllChecked) {
+            if (this.uploadMyDocumentsForm.get('shareAll').value === true) {
+                for (let i=0; i < this.nbUploads; i++) {
+                    this.uploadMyDocumentsForm.get('shareUpload' + (i+1)).patchValue(this.uploadMyDocumentsForm.get('shareAll').value, { emitEvent: false });
+                }
+            }
+        } else {
+            this.uploadMyDocumentsForm.get('shareAll').patchValue(false, { emitEvent: false });
+        }
+        this._changeDetectorRef.markForCheck();
     }
+
+    getUpload(event, ix) {
+        this.uploadFile(event, ix, this._changeDetectorRef);
+    }
+
+    uploadFile(event, ix, changeDetectorRef: ChangeDetectorRef): void {
+        const asyncTaskPipe = this.fileService.addFile({
+            files: _.filter(event.files, (file) => {
+                return file.status !== 'uploaded-file';
+            }),
+        });
+
+        this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
+            asyncTaskPipe,
+            (data) => {
+                if (data[1] && data[1].Data) {
+                    let errorMessage;
+
+                    _.each(data[1].Data, (file) => {
+                        if (file.error) {
+                            errorMessage = file.error;
+                            event.target.updateFileStatus(file.id, 'file-error');
+                        } else {
+                            event.target.updateFileStatus(file[0].id, 'uploaded-file');
+                            this.allUploadsFiles[ix] = {
+                                fileID: file[0].fileID,
+                                hash: file[0].fileHash,
+                                name: file[0].fileTitle,
+                            };
+                        }
+                    });
+
+                    if (errorMessage) {
+                        this.toaster.pop('error', errorMessage);
+                    }
+
+                    if (data[1].Data.length === 0) {
+                        // removed file
+                        this.allUploadsFiles[ix] = {
+                            fileID: 0,
+                            hash: '',
+                            name: '',
+                        };
+                    }
+
+                    changeDetectorRef.markForCheck();
+                    changeDetectorRef.detectChanges();
+                }
+            },
+            (data) => {
+                let errorMessage;
+
+                _.each(data[1].Data, (file) => {
+                    if (file.error) {
+                        errorMessage += file.error + '<br/>';
+                        event.target.updateFileStatus(file.id, 'file-error');
+                    }
+                });
+
+                if (errorMessage) {
+                    if (errorMessage) {
+                        this.toaster.pop('error', errorMessage);
+                    }
+                }
+            }),
+        );
+    }
+
 
     /* On Destroy. */
     ngOnDestroy(): void {
