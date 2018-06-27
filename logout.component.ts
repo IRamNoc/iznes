a7fdf4c2eea23a9ef0ec326aae11015f006cb0f7
Stuart Fyfe <stuart.fyfe@setl.io>
2018-06-27 14:25:29 +0100

    Disconnect wallet node on logout

--- logout.component.ts
+++ logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgRedux } from '@angular-redux/store';
+import { WalletNodeSocketService } from '@setl/websocket-service';
 
 @Component({
     selector: 'app-logout',
@@ -8,11 +9,16 @@ import { NgRedux } from '@angular-redux/store';
 })
 export class SetlLogoutComponent implements OnInit {
 
-    constructor(private ngRedux: NgRedux<any>, private router: Router) {
+    constructor(
+        private ngRedux: NgRedux<any>,
+        private router: Router,
+        private walletSocket: WalletNodeSocketService,
+    ) {
     }
 
     ngOnInit() {
         this.ngRedux.dispatch({ type: 'USER_LOGOUT' });
         this.router.navigate(['login']);
+        this.walletSocket.clearConnection();
     }
 }
