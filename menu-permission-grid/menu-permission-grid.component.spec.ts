9682c6810b8926a3291fee7c6acc147651576baa
Stephen Strudwick <stephen.strudwick@setl.io>
2018-06-29 11:06:01 +0100

    TG-XXX feat(menuPermissions): menu permissions grid added.

---
+++ menu-permission-grid/menu-permission-grid.component.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { PermissionGridComponent } from './permission-grid.component';
+//
+// describe('PermissionGridComponent', () => {
+//   let component: PermissionGridComponent;
+//   let fixture: ComponentFixture<PermissionGridComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ PermissionGridComponent ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(PermissionGridComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should be created', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
