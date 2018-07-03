1de063f1e0171e5fef1f0da7e6d1d21ff5b7692e
Mathew Bland <mat.bland@setl.io>
2018-07-03 16:39:11 +0100

    Added CSV export functionality to Balances Component.

--- balances/balances.component.ts
+++ balances/balances.component.ts
@@ -1,23 +1,26 @@
-import {Component, ViewChild, AfterViewInit, OnInit, OnDestroy, ChangeDetectorRef} from '@angular/core';
-import {HoldingByAsset} from '@setl/core-store/wallet/my-wallet-holding';
-import {ReportingService} from '@setl/core-balances/reporting.service';
-import {WalletTxHelperModel} from '@setl/utils';
-import {ActivatedRoute} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
-import {Subscription} from 'rxjs/Subscription';
-import {TabControl, Tab} from '../tabs';
-import {select} from '@angular-redux/store';
-import {isEqual} from 'lodash';
+import { Component, ViewChild, AfterViewInit, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { HoldingByAsset } from '@setl/core-store/wallet/my-wallet-holding';
+import { ReportingService } from '@setl/core-balances/reporting.service';
+import { WalletTxHelperModel } from '@setl/utils';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { TabControl, Tab } from '../tabs';
+import { NgRedux, select } from '@angular-redux/store';
+import * as _ from 'lodash';
+import { isEqual } from 'lodash';
+import * as json2csv from 'json2csv';
+import * as SagaHelper from '@setl/utils/sagaHelper/index';
+import { FileService } from '@setl/core-req-services';
 
 @Component({
     selector: 'setl-balances',
     templateUrl: './balances.component.html',
-    styleUrls: ['./balances.component.css']
+    styleUrls: ['./balances.component.css'],
 })
 export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
-
     balances$: Observable<HoldingByAsset>;
-    balances: HoldingByAsset
+    balances: HoldingByAsset;
 
     @ViewChild('myDataGrid') myDataGrid;
     @select(['user', 'connected', 'connectedWallet']) getConnectedWallet;
@@ -25,36 +28,57 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
     public tabControl: TabControl;
     public tabs: Tab[];
     readonly transactionFields = new WalletTxHelperModel.WalletTransactionFields().fields;
-    private subscriptions: Array<Subscription> = [];
+    private subscriptions: Subscription[] = [];
     public connectedWalletId;
+    public exportModalDisplay: boolean = false;
+    public exportFilename: string = 'BalancesExport.csv';
+    public exportFileHash: string = '';
 
-    constructor(private reportingService: ReportingService,
-                private route: ActivatedRoute,
-                private changeDetector: ChangeDetectorRef) {
+    /**
+     * Constructor
+     *
+     * @param {ReportingService} reportingService
+     * @param {ActivatedRoute} route
+     * @param {ChangeDetectorRef} changeDetector
+     * @param {NgRedux<any>} ngRedux
+     * @param {FileService} fileService
+     */
+    public constructor(private reportingService: ReportingService,
+                       private route: ActivatedRoute,
+                       private changeDetector: ChangeDetectorRef,
+                       private ngRedux: NgRedux<any>,
+                       private fileService: FileService,
+    ) {
     }
 
-    ngOnInit() {
+    /**
+     * Ng On Init
+     *
+     * @returns {Promise<void>}
+     */
+    public ngOnInit() {
         let previous = [];
         this.balances$ = this.reportingService
             .getBalances()
             .distinctUntilChanged((oldAssets, newAssets) => isEqual(oldAssets, newAssets))
-            .map(assets => {
-                let result = this.markUpdated([previous, assets]);
+            .map((assets) => {
+                const result = this.markUpdated([previous, assets]);
                 previous = assets;
                 return result;
             })
             .share()
         ;
 
-        this.balances$.subscribe(balances => {
+        this.balances$.subscribe((balances) => {
             this.balances = balances;
         });
 
-        this.subscriptions.push(this.getConnectedWallet.subscribe((connectedWalletId) => {
+        this.subscriptions.push(
+            this.getConnectedWallet.subscribe((connectedWalletId) => {
                 this.connectedWalletId = connectedWalletId;
                 this.closeTabs();
                 previous = [];
-            }
+            },
         ));
 
         this.initTabUpdates();
@@ -65,35 +89,45 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
             active: true,
             data: {
                 asset: -1,
-            }
+            },
         });
         this.subscriptions.push(
             this.tabControl.getTabs().subscribe((tabs) => {
                 this.tabs = tabs;
                 this.changeDetector.markForCheck();
-            })
+            }),
         );
 
         const hash = this.route.snapshot.paramMap.get('hash');
         if (hash) {
             switch (this.route.snapshot.paramMap.get('action')) {
-                case 'history':
-                    return this.findAsset(hash).then(asset => this.handleViewHistory(asset));
-                case 'breakdown':
-                    return this.findAsset(hash).then(asset => this.handleViewBreakdown(asset));
+            case 'history':
+                return this.findAsset(hash).then(asset => this.handleViewHistory(asset));
+            case 'breakdown':
+                return this.findAsset(hash).then(asset => this.handleViewBreakdown(asset));
             }
         }
     }
 
-    ngAfterViewInit() {
+    /**
+     * Ng After View Init
+     *
+     * @return {void}
+     */
+    public ngAfterViewInit() {
         this.myDataGrid.resize();
     }
 
-    initTabUpdates() {
-        this.balances$.subscribe(balances => {
+    /**
+     * Init Tab Updates
+     *
+     * @return {void}
+     */
+    public initTabUpdates() {
+        this.balances$.subscribe((balances) => {
             if (this.tabs && this.tabs.length) {
-                this.tabs.filter(tab => tab.data.assetObject).forEach(tab => {
-                    let foundAsset = (balances as Array<any>).find(asset => asset.hash === tab.data.hash);
+                this.tabs.filter(tab => tab.data.assetObject).forEach((tab) => {
+                    const foundAsset = (balances as any[]).find(asset => asset.hash === tab.data.hash);
 
                     if (foundAsset !== undefined) {
                         tab.data.assetObject = foundAsset;
@@ -104,6 +138,13 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
         });
     }
 
+    /**
+     * Find Asset
+     *
+     * @param {string} hash
+     *
+     * @returns {Promise<any>}
+     */
     private findAsset(hash: string) {
         return new Promise((resolve) => {
             return this.reportingService.getBalances().first().subscribe((assets) => {
@@ -132,7 +173,7 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
                 assetObject: asset,
                 hash: asset.hash,
                 template: 'breakdown',
-            }
+            },
         });
     }
 
@@ -156,7 +197,7 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
                 transactions: this.reportingService.getTransactionsForAsset(asset.asset),
                 hash: asset.hash,
                 template: 'history',
-            }
+            },
         });
     }
 
@@ -178,9 +219,9 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
             active: false,
             data: {
                 hash: transaction.hash,
-                transaction: transaction,
+                transaction,
                 template: 'transaction',
-            }
+            },
         });
     }
 
@@ -193,7 +234,7 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
      * @returns {(tab) => boolean}
      */
     private findTab(hash: string, template: string) {
-        return (tab) => tab.data.hash === hash && tab.data.template === template;
+        return tab => tab.data.hash === hash && tab.data.template === template;
     }
 
     /**
@@ -206,7 +247,13 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
      */
     private markUpdated([prev, next]) {
         return next.map((asset) => {
-            const updatedAsset = {...asset, isNew: false, totalChange: false, encumberChange: false, freeChange: false};
+            const updatedAsset = {
+                ...asset,
+                isNew: false,
+                totalChange: false,
+                encumberChange: false,
+                freeChange: false,
+            };
             if (!prev.length) {
                 return updatedAsset;
             }
@@ -243,11 +290,96 @@ export class SetlBalancesComponent implements AfterViewInit, OnInit, OnDestroy {
             while (tabIndex) {
                 this.tabControl.close(tabIndex);
                 this.changeDetector.markForCheck();
-                tabIndex--;
+                tabIndex -= 1;
             }
         }
     }
 
+    /**
+     * Export List
+     *
+     * Exports current dataGrid List to CSV format
+     *
+     * @return {void}
+     */
+    public exportList() {
+        const options = {};
+        const csvData = this.myDataGrid.items['_filtered'];
+        if (csvData.length === 0) {
+            return;
+        }
+        const encodedCsv = Buffer.from(json2csv.parse(csvData, options)).toString('base64');
+        const fileData = {
+            name: this.exportFilename,
+            data: encodedCsv,
+            status: '',
+            filePermission: 1,
+        };
+        const asyncTaskPipe = this.fileService.addFile({
+            files: _.filter([fileData], (file) => {
+                return file.status !== 'uploaded-file';
+            }),
+        });
+
+        this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
+            asyncTaskPipe,
+            (data) => {
+                if (data[1] && data[1].Data) {
+                    let errorMessage;
+                    _.each(data[1].Data, (file) => {
+                        if (file.error) {
+                            errorMessage = file.error;
+                        } else {
+                            this.exportFileHash = file[0].fileHash;
+                            this.showExportModal();
+                        }
+                    });
+                    if (errorMessage) {
+                    }
+                }
+            },
+            (data) => {
+                let errorMessage;
+                _.each(data[1].Data, (file) => {
+                    if (file.error) {
+                        errorMessage += file.error + '<br/>';
+                    }
+                });
+                if (errorMessage) {
+                    if (errorMessage) {
+                    }
+                }
+            }),
+        );
+    }
+
+    /**
+     * Show Export Modal
+     *
+     * @return {void}
+     */
+    public showExportModal() {
+        this.exportModalDisplay = true;
+        this.changeDetector.markForCheck();
+        this.changeDetector.detectChanges();
+    }
+
+    /**
+     * Hide Export Modal
+     *
+     * @return {void}
+     */
+    public hideExportModal() {
+        this.exportModalDisplay = false;
+        this.changeDetector.markForCheck();
+        this.changeDetector.detectChanges();
+    }
+
+    /**
+     * Ng On Destroy
+     *
+     * @return {void}
+     */
     ngOnDestroy() {
         for (const subscription of this.subscriptions) {
             subscription.unsubscribe();
