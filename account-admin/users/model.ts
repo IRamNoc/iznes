7dd220b2ed342e7009577aa678ac724f25c7fb66
Phil Zeelte <philzeelte@IM00152.local>
2018-07-03 16:35:57 +0100

    added new fields

--- account-admin/users/model.ts
+++ account-admin/users/model.ts
@@ -1,10 +1,13 @@
 export interface User {
+    userID: number;
     reference: string;
     firstName: string;
     lastName: string;
     emailAddress: string;
     phoneNumber: string;
+    userTypeID: number;
     userType: string;
+    userStatus: number;
 }
 
 export interface UsersState {
