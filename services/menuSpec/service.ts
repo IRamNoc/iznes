b6d2905899c00d7592583b22e433039f6a82af44
Phil Zeelte <philzeelte@IM00152.local>
2018-07-27 12:49:28 +0100

    modified retrieval of menuSpec to populate it with appConfig menuSpec if no menu found in redux

--- services/menuSpec/service.ts
+++ services/menuSpec/service.ts
@@ -1,18 +1,34 @@
-import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
-import {select} from "@angular-redux/store";
-import {filter} from "rxjs/operators"
+import { Injectable, Inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { select } from '@angular-redux/store';
+import { filter, switchMap } from 'rxjs/operators';
+
+import { AppConfig } from '../../appConfig/appConfig.model';
+import { APP_CONFIG } from '../../appConfig/appConfig';
 
 @Injectable()
 export class MenuSpecService {
 
     @select(['user', 'siteSettings', 'siteMenu']) getSiteMenu: Observable<any>;
 
-    constructor() {
-
-    }
+    constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) { }
 
     getMenuSpec() {
-        return this.getSiteMenu.pipe(filter(m => !!Object.keys(m).length));
+        /*
+         * get the menuSpec from redux and check if;
+         * - it has been retrieved previously (if not, we don't return it)
+         * - if it is empty, grab the default menuSpec from appConfig
+         */
+        return this.getSiteMenu.pipe(filter(m => m.fetched), switchMap((m) => {
+            if (this.isMenuSpecEmpty(m)) {
+                return of(Object.assign({}, this.appConfig.menuSpec, { hidden: this.appConfig.nonMenuLink }));
+            }
+
+            return of(m);
+        }));
+    }
+
+    private isMenuSpecEmpty(menuSpec): boolean {
+        return !menuSpec || Object.keys(menuSpec).length === 1;
     }
 }
