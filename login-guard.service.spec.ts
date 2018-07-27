cea7ed9864d728d122026cacc0dd5d869f5b4e94
Phil Zeelte <philzeelte@IM00152.local>
2018-07-27 15:41:59 +0100

    fixed unit test

--- login-guard.service.spec.ts
+++ login-guard.service.spec.ts
@@ -8,7 +8,7 @@ import {
     MyUserServiceMock,
     MemberSocketServiceMock,
     AlertsServiceMock,
-    ToasterServiceMock
+    ToasterServiceMock,
 } from '@setl/core-test-util';
 import { MyUserService } from '@setl/core-req-services';
 import { MemberSocketService } from '@setl/websocket-service';
@@ -19,7 +19,7 @@ import { MenuSpecService } from '@setl/utils/services/menuSpec/service';
 const environment = {
     logoID: '',
     logoUrl: '',
-    platform: ''
+    platform: '',
 };
 
 describe('LoginGuardService', () => {
@@ -29,7 +29,7 @@ describe('LoginGuardService', () => {
             imports: [
                 ToasterModule,
                 NgReduxTestingModule,
-                CoreTestUtilModule
+                CoreTestUtilModule,
             ],
             providers: [
                 { provide: AlertsService, useClass: AlertsServiceMock },
@@ -42,7 +42,7 @@ describe('LoginGuardService', () => {
                     useValue: environment,
                 },
                 MenuSpecService,
-            ]
+            ],
         });
 
         MockNgRedux.reset();
@@ -54,31 +54,17 @@ describe('LoginGuardService', () => {
     }));
 
     it('when user is login, canActivate should return true',
-        inject([LoginGuardService], (service: LoginGuardService) => {
+       inject([LoginGuardService], (service: LoginGuardService) => {
             const isLoginStub: Subject<boolean> = MockNgRedux.getSelectorStub<any, any>(['user', 'authentication', 'isLogin']);
             isLoginStub.next(true);
             expect(service.canActivate(<any>{}, <any>{})).toBeTruthy();
         }));
 
     it('when user is not login, canActivate should return false',
-        inject([LoginGuardService], (service: LoginGuardService) => {
+       inject([LoginGuardService], (service: LoginGuardService) => {
             const isLoginStub: Subject<boolean> = MockNgRedux.getSelectorStub<any, any>(['user', 'authentication', 'isLogin']);
             isLoginStub.next(false);
             expect(service.canActivate(<any>{}, <any>{})).toBeFalsy();
-        }));
-
-    it('when user logged in, isLogin should set to true',
-        inject([LoginGuardService], (service: LoginGuardService) => {
-            const isLoginStub: Subject<boolean> = MockNgRedux.getSelectorStub<any, any>(['user', 'authentication', 'isLogin']);
-            isLoginStub.next(true);
-            expect(service.isLogin).toBeTruthy();
-        }));
-
-    it('when user loged out, isLogin should set to false',
-        inject([LoginGuardService], (service: LoginGuardService) => {
-            const isLoginStub: Subject<boolean> = MockNgRedux.getSelectorStub<any, any>(['user', 'authentication', 'isLogin']);
-            isLoginStub.next(false);
-            expect(service.isLogin).toBeFalsy();
         }));
 
 });
