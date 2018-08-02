a7e1315e0e15d2f1e184d2488798a2bf0a039249
Laurent HAUCK <laurent.hauck@setl.io>
2018-08-02 16:28:43 +0200

    fix issue TG-2031: add nonMenuLink urls

--- src/environments/nonMenuLink.ts
+++ src/environments/nonMenuLink.ts
@@ -23,5 +23,9 @@ export const nonMenuLink = {
         '\/reports\/holders-list\/funds\/[^\\/]*',
         '\/reports\/holders-list\/shares\/[^\\/]*',
         '\/invite-investors',
+        '\/reports\/precentralisation\/funds',
+        '\/reports\/precentralisation\/shares',
+        '\/reports\/centralisation\/funds',
+        '\/reports\/centralisation\/shares',
     ],
 };
