a72f14f038c1a524ffca86e719d7e81eb070e555
Thomas Bianchi <thomas.bianchi@setl.io>
2018-08-08 17:21:12 +0200

    TG-XXX fix(kyc) : Beautify KYC
    Cleaned two first steps of KYC tunnel

--- ofi-kyc/my-requests/request/steps/introduction.component.ts
+++ ofi-kyc/my-requests/request/steps/introduction.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector : 'kyc-step-introduction',
-    templateUrl : './introduction.component.html'
+    templateUrl : './introduction.component.html',
+    styleUrls : ['./introduction.component.scss']
 })
 export class NewKycIntroductionComponent implements OnInit{
 
