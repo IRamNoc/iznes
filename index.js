785893a4ab63ee44927eacdc0859851d7f7c1435
Phil Zeelte <philzeelte@IM00152.local>
2018-08-15 15:24:03 +0100

    initial signup commit

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export {SetlLoginComponent} from './login.component';
 export {SetlLogoutComponent} from './logout.component';
 export {LoginGuardService} from './login-guard.service';
 export {name} from './__init__';
+export {SignupComponent} from './signup/component';
--- login.module.ts
+++ login.module.ts
@@ -1,24 +1,28 @@
 /* Core imports. */
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 /* Login view. */
-import {SetlLoginComponent} from './login.component';
-import {SetlLogoutComponent} from './logout.component';
+import { SetlLoginComponent } from './login.component';
+import { SetlLogoutComponent } from './logout.component';
+
+/* Signup */
+import { SignupComponent } from './signup/component';
 
 /* Notifications. */
-import {ToasterModule, ToasterService} from 'angular2-toaster';
+import { ToasterModule, ToasterService } from 'angular2-toaster';
 
-import {ClarityModule} from '@clr/angular';
-import {MultilingualModule} from '@setl/multilingual';
-import {SetlDirectivesModule, SetlPipesModule} from '@setl/utils';
+import { ClarityModule } from '@clr/angular';
+import { MultilingualModule } from '@setl/multilingual';
+import { SetlDirectivesModule, SetlPipesModule } from '@setl/utils';
 
 @NgModule({
     declarations: [
         SetlLoginComponent,
         SetlLogoutComponent,
+        SignupComponent,
     ],
     imports: [
         ToasterModule,
@@ -32,6 +36,7 @@ import {SetlDirectivesModule, SetlPipesModule} from '@setl/utils';
     ],
     exports: [
         SetlLoginComponent,
+        SignupComponent,
     ],
     providers: [
         ToasterService,
