ea70c6ea2752d4e2e2f5e82863dffe38897e68f8
Phil Zeelte <philzeelte@IM00152.local>
2018-08-16 16:26:20 +0100

    refactored login to be in core

--- ofi-kyc/signup/component.ts
+++ ofi-kyc/signup/component.ts
@@ -57,6 +57,7 @@ export class OfiSignUpComponent implements OnInit, OnDestroy {
         const buttonText = this.translate.translate('Create an account');
 
         this.configuration = {
+            doLoginAfterCallback: true,
             title,
             description,
             buttonText,
@@ -64,85 +65,34 @@ export class OfiSignUpComponent implements OnInit, OnDestroy {
         };
     }
 
-    private signupCallback(): void {
-        const signupData = this.signupData();
+    private signupCallback(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const signupData = this.signupData();
 
-        this.ofiKycService.createUser({
-            token: signupData.invitationToken,
-            email: signupData.username,
-            password: signupData.password,
-            lang: signupData.language,
-        }).then(() => {
-            this.confirmationService.create(
-                'Success',
-                '<p><b>Your account was created</b></p><p>A confirmation email was sent to you.</p>',
-                {
-                    confirmText: 'Continue to ' + this.appConfig.platform,
-                    declineText: '',
-                    btnClass: 'success',
-                },
-            ).subscribe(() => {
-                this.loginAfterSignup(signupData.username, signupData.password);
+            this.ofiKycService.createUser({
+                token: signupData.invitationToken,
+                email: signupData.username,
+                password: signupData.password,
+                lang: signupData.language,
+            }).then(() => {
+                this.confirmationService.create(
+                    'Success',
+                    '<p><b>Your account was created</b></p><p>A confirmation email was sent to you.</p>',
+                    {
+                        confirmText: 'Continue to ' + this.appConfig.platform,
+                        declineText: '',
+                        btnClass: 'success',
+                    },
+                ).subscribe(() => {
+                    resolve();
+                });
+            }).catch((e) => {
+                this.alertsService.create(
+                    'error',
+                    '<span class="text-warning">Sorry, something went wrong.<br>Please try again later!</span>',
+                );
             });
-        }).catch((e) => {
-            this.alertsService.create(
-                'error',
-                '<span class="text-warning">Sorry, something went wrong.<br>Please try again later!</span>',
-            );
         });
-    }
-
-    private loginAfterSignup(username: string, password: string): void {
-        const loginRequestAction = loginRequestAC();
-        this.redux.dispatch(loginRequestAction);
-
-        const asyncTaskPipe = this.myUserService.loginRequest({
-            username,
-            password,
-        });
-
-        this.redux.dispatch(SagaHelper.runAsync(
-            [SET_LOGIN_DETAIL, SET_AUTH_LOGIN_DETAIL, SET_PRODUCTION],
-            [RESET_LOGIN_DETAIL, RESET_AUTH_LOGIN_DETAIL],
-            asyncTaskPipe,
-            {},
-            () => {
-            },
-            (data) => {
-                this.handleLoginFailMessage(data);
-            },
-        ));
-    }
-
-    private handleLoginFailMessage(data) {
-        const responseStatus = _.get(data, '[1].Data[0].Status', 'other').toLowerCase();
-
-        switch (responseStatus) {
-        case 'fail':
-            this.showLoginErrorMessage(
-                'warning',
-                '<span mltag="txt_loginerror" class="text-warning">Invalid email address or password!</span>',
-            );
-            break;
-        case 'locked':
-            this.showLoginErrorMessage(
-                'info',
-                '<span mltag="txt_accountlocked" class="text-warning">Sorry, your account has been locked. ' +
-                'Please contact Setl support.</span>',
-            );
-            break;
-        default:
-            this.showLoginErrorMessage(
-                'error',
-                '<span mltag="txt_loginproblem" class="text-warning">Sorry, there was a problem logging in, ' +
-                'please try again.</span>',
-            );
-            break;
-        }
-    }
-
-    private showLoginErrorMessage(type, msg) {
-        this.alertsService.create(type, msg, { buttonMessage: 'Please try again to log in' });
     }
 
     setSignupData(signupData: () => ISignupData): void {
