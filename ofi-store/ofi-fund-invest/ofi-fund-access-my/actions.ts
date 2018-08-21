deca310ba37bab9d2c87882e139f6c843817912a
Stuart Fyfe <stuart.fyfe@setl.io>
2018-08-21 15:11:58 +0100

    New orders append to list + Update NAV +  manage-orders component tidy up
    Add: Append new orders if on the first page of the manage orders list.
    Add: Update fund share NAV on "updatenav" events.
    Update: Pull out search filter and datagrid param logic into classes.

--- ofi-store/ofi-fund-invest/ofi-fund-access-my/actions.ts
+++ ofi-store/ofi-fund-invest/ofi-fund-access-my/actions.ts
@@ -1,5 +1,5 @@
-import {name} from './__init__';
-import {kAction} from '@setl/utils/common';
+import { name } from './__init__';
+import { kAction } from '@setl/utils/common';
 
 /**
  *  Set fund access my
@@ -13,7 +13,6 @@ export const SET_FUND_ACCESS_MY = `${name}/SET_FUND_ACCESS_MY`;
 export const SET_REQUESTED_FUND_ACCESS_MY = `${name}/SET_REQUESTED_FUND_ACCESS_MY`;
 export const setRequestedFundAccessMy = kAction(SET_REQUESTED_FUND_ACCESS_MY);
 
-
 /**
  * Clear (set to false) request my fund access state
  * Flag that to indicate we will ned to request it when we need it.
@@ -26,3 +25,5 @@ export const validateKiid = shareID => ({
     type: VALIDATE_KIID,
     payload: shareID,
 });
+export const OFI_FUND_LATEST_NAV = `${name}/OFI_FUND_LATEST_NAV`;
+export const ofiFundLatestNav = data => ({ type: OFI_FUND_LATEST_NAV, payload: data });
