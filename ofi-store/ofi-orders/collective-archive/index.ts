126336bb263c6e5ec171b0751e9d2a7e81e00e0a
David Duong <david.duong@setl.io>
2018-08-31 16:14:43 +0200

    TG-XXX refact(ofi-store/ofi-orders): get rid of "collective-archive" store because not used anymore

--- ofi-store/ofi-orders/collective-archive/index.ts
+++
@@ -1,3 +0,0 @@
-export {OfiCollectiveArchiveReducer} from './reducer';
-export {OfiCollectiveArchiveState} from './model';
-export {setRequestedCollectiveArchive, SET_COLLECTIVE_ARCHIVE, clearRequestedCollectiveArchive} from './actions';
