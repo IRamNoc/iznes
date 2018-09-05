84e21a1074aeb49c9f4bad058d80ee04911702e7
Stephen Strudwick <stephen.strudwick@setl.io>
2018-09-05 12:46:54 +0100

    TG-2306 fix(kycBeneficiaries): new beneficiaries form ammended with ID once saved so they are not treated as new.

--- ofi-kyc/my-requests/request/steps/identification.service.ts
+++ ofi-kyc/my-requests/request/steps/identification.service.ts
@@ -1,12 +1,24 @@
-import {Injectable} from '@angular/core';
-import {FormArray} from '@angular/forms';
-import {MemberSocketService} from '@setl/websocket-service';
+import { Injectable } from '@angular/core';
+import { FormArray } from '@angular/forms';
+import { MemberSocketService } from '@setl/websocket-service';
 
-import {mapValues, isArray, isObject, reduce, pickBy, get as getValue, merge, omit, flatten, pick, isNil} from 'lodash';
+import {
+    mapValues,
+    isArray,
+    isObject,
+    reduce,
+    pickBy,
+    get as getValue,
+    merge,
+    omit,
+    flatten,
+    pick,
+    isNil
+} from 'lodash';
 
-import {NewRequestService} from '../new-request.service';
-import {RequestsService} from '../../requests.service';
-import {DocumentsService} from './documents.service';
+import { NewRequestService } from '../new-request.service';
+import { RequestsService } from '../../requests.service';
+import { DocumentsService } from './documents.service';
 
 @Injectable()
 export class IdentificationService {
@@ -68,7 +80,7 @@ export class IdentificationService {
         return Promise.all(promises);
     }
 
-    handleBeneficiaries(formGroupBeneficiaries, kycID, connectedWallet){
+    handleBeneficiaries(formGroupBeneficiaries, kycID, connectedWallet) {
         let promises = [];
 
         formGroupBeneficiaries.controls.forEach(formGroupBeneficiary => {
@@ -132,7 +144,11 @@ export class IdentificationService {
             RequestName: 'updatekyccompanybeneficiaries',
             ...extracted
         };
-        return this.requestsService.sendRequest(messageBody);
+        return this.requestsService.sendRequest(messageBody).then((data) => {
+            if (!!data[1].Data[0].companyBeneficiariesID) {
+                formGroupBeneficiary.controls['companyBeneficiariesID'].setValue(data[1].Data[0].companyBeneficiariesID);
+            }
+        });
     }
 
     sendRequestBanking(formGroupBankingValue) {
