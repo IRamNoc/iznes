dfb6f0d14499f585797110d48a72e76d63130b8c
Adam Crowe <adam.crowe@setl.io>
2018-09-11 14:02:08 +0100

    TG-403 fix(request-asset): Fixed walletToRelationshipToSelectItem

--- helper/wallet/index.ts
+++ helper/wallet/index.ts
@@ -1,9 +1,11 @@
-import {fromJS} from 'immutable';
+import { fromJS } from 'immutable';
 import * as _ from 'lodash';
 
 /**
- * Convert wallet Address to an array the select2 can use to render a list a wallet address.
+ * Converts wallet Address to an array the select2 can use to render a list a wallet address.
+ *
  * @param walletAddressList
+ *
  * @return {any}
  */
 export function walletAddressListToSelectItem(walletAddressList: any, labelIdentifier: string = null): any {
@@ -12,69 +14,88 @@ export function walletAddressListToSelectItem(walletAddressList: any, labelIdent
         (result, thisWalletAddress, key) => {
             let label = thisWalletAddress.get(labelIdentifier);
 
-            if(labelIdentifier == 'label' && thisWalletAddress.get('addr')) {
+            if (labelIdentifier === 'label' && thisWalletAddress.get('addr')) {
                 // if using label add the wallet address to the end
-                label = `${label} (${key.substr(0,8)}...)`;
+                label = `${label} (${key.substr(0, 8)}...)`;
             }
 
             result.push({
                 id: key,
-                text: (labelIdentifier && label) ? label : key
+                text: (labelIdentifier && label) ? label : key,
             });
 
             return result;
-        }, []
+        },
+        [],
     );
 
     return walletAddressSelectItem;
 }
 
 /**
- * Convert wallet instruments to an array the select2 can use to render a list a wallet instrument.
+ * Converts wallet instruments to an array the select2 can use to render a list a wallet instrument.
+ *
  * @param walletInstrumetnList
+ *
  * @return {any}
  */
-export function walletInstrumentListToSelectItem(walletInstrumentList: object): Array<any> {
+export function walletInstrumentListToSelectItem(walletInstrumentList: object): any[] {
     const walletInstrumentListImu = fromJS(walletInstrumentList);
     const walletInstrumentSelectItem = walletInstrumentListImu.reduce(
         (result, thisWalletInstrument, key) => {
             result.push({
                 id: key,
-                text: key
+                text: key,
             });
 
             return result;
-        }, []
+        },
+        [],
     );
 
     return walletInstrumentSelectItem;
 }
 
 /**
- * Convert wallet to relationship to an array the select2 can use to render a list a wallet to relationship.
+ * Converts wallet to relationship to an array the select2 can use to render a list a wallet to relationship.
+ *
  * @param walletToRelationshipList
  * @param walletDirectoryList
+ *
  * @return {any}
  */
-export function walletToRelationshipToSelectItem(walletToRelationship: object, walletDirectoryList: object): Array<any> {
+export function walletToRelationshipToSelectItem(
+    walletToRelationship: object, walletDirectoryList: object): any[] {
     const walletToRelationshipListImu = fromJS(walletToRelationship);
     const walletToRelationshipSelectItem = walletToRelationshipListImu.reduce(
         (result, thisToRelationship, key) => {
+            let walletName = 'Unknown wallet name';
+
+            Object.keys(walletDirectoryList).forEach((item) => {
+                if (walletDirectoryList[item].id === String(key)) {
+                    walletName = walletDirectoryList[item].text;
+                }
+            });
+
             result.push({
                 id: thisToRelationship.get('keyDetail'),
-                text: _.get(walletDirectoryList, '[' + key + '].walletName', 'Unknown wallet name')
+                text: walletName,
             });
 
             return result;
-        }, []
+        },
+        [],
     );
 
     return walletToRelationshipSelectItem;
 }
 
 /**
- * Split full asset id, return issuer and instrument in an object.
- * @param fullAssetId
+ * Splits full asset ID and returns issuer and instrument in an object.
+ *
+ * @param {string} fullAssetId
+ *
+ * @return {object}
  */
 interface SplitFullAssetIdReturn {
     issuer: string;
@@ -85,6 +106,6 @@ export function splitFullAssetId(fullAssetId: string): SplitFullAssetIdReturn {
 
     return {
         issuer: parts[0],
-        instrument: parts[1]
+        instrument: parts[1],
     };
 }
