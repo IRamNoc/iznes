0f3a6574d1fb26ae5ed8554df9b3616930512773
Adam Crowe <adam.crowe@setl.io>
2018-09-25 17:30:31 +0100

    TG-000 Fix IE TouchEvent compatibility

--- helper/polyfills/ie11.ts
+++ helper/polyfills/ie11.ts
@@ -2,3 +2,8 @@ require('./dom.prepend');
 require('./dom.remove');
 require('./event.mouse');
 require('./svg.contains');
+
+/* For Internet Explorer compatibility. */
+if (!(window as any).hasOwnProperty('TouchEvent')) {
+    (window as any).TouchEvent = {};
+}
