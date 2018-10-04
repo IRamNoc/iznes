5cfecacb5f974f5b03db8e474c6037114ce9f07e
michaelbindley <michael.bindley@setl.io>
2018-10-04 17:22:54 +0100

    test_michael: Added New tests, new validation, new methods for future tests.

--- javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/AdministrationModule/OpenCSDTeamsSubModuleTest.java
+++ javaUITesting2/src/test/java/com/setl/openCSDClarityTests/UI/AdministrationModule/OpenCSDTeamsSubModuleTest.java
@@ -9,6 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 
 
@@ -55,7 +56,34 @@ public class OpenCSDTeamsSubModuleTest {
         logout();
         validateTeamsCreated(1, teamName[0], teamReference[0], teamDescription[0]);
     }
-
+    @Test
+    public void shouldNotAllowNullNameRefDesc() throws InterruptedException {
+        loginAndVerifySuccess("am", "alex01");
+        navigateToDropdown("menu-administration");
+        navigateToDropdown("menu-administration-teams");
+        selectAddNewTeam();
+        fillInTeamsNullCharacters();
+        logout();
+    }
+    @Test
+    public void shouldNotAllowDuplicateName() throws InterruptedException, SQLException {
+        String[] teamName = generateRandomTeamName();
+        String[] teamReference = generateRandomTeamReference();
+        String[] teamDescription = fillInDescription();
+        loginAndVerifySuccess("am", "alex01");
+        navigateToDropdown("menu-administration");
+        navigateToDropdown("menu-administration-teams");
+        selectAddNewTeam();
+        fillInTeamsDetails(teamName[0], teamReference[0], teamDescription[0]);
+        selectCreateNewTeamNoPermissions();
+        searchTeam(teamReference[0],teamName[0],teamDescription[0], "Pending");
+        selectAddNewTeam();
+        fillInTeamsDetails(teamName[0], teamReference[0], teamDescription[0]);
+        assertTeamName(teamName[0]);
+        assertCantCreateTeamNameAlreadyExists(teamName[0]);
+        logout();
+        validateTeamsCreated(1, teamReference[0], teamName[0], teamDescription[0]);
+    }
     @Test
     public void shouldUpdateTeam() throws InterruptedException, SQLException {
         String[] teamName = generateRandomTeamName();
@@ -77,9 +105,7 @@ public class OpenCSDTeamsSubModuleTest {
         searchTeam(teamReference[0], updateName[0], teamDescription[0], "Pending");
         logout();
         validateTeamsCreated(1, updateName[0], teamReference[0], teamDescription[0]);
-
     }
-
     @Test
     public void shouldCancelDeleteTeamIfNoIsSelected() throws InterruptedException, SQLException {
         String[] teamName = generateRandomTeamName();
@@ -99,7 +125,6 @@ public class OpenCSDTeamsSubModuleTest {
         logout();
         validateTeamsCreated(1, teamReference[0], teamName[0], teamDescription[0]);
     }
-
     @Test
     public void shouldDeleteTeamIfYesIsSelected() throws InterruptedException, SQLException {
         String[] teamName = generateRandomTeamName();
@@ -119,7 +144,6 @@ public class OpenCSDTeamsSubModuleTest {
         logout();
         validateTeamsDeleted(0, teamReference[0], teamName[0], teamDescription[0]);
     }
-
     @Test
     public void shouldCreateTeamWithPermissionsAdministration() throws InterruptedException, SQLException {
         String [] teamName = generateRandomTeamName();
@@ -136,6 +160,39 @@ public class OpenCSDTeamsSubModuleTest {
         logout();
         validateTeamsCreated(1, teamReference[0], teamName[0], teamDescription[0]);
         validateAdminTeamPermissions(teamName[0]);
+    }
+    @Test
+    public void createTeamAssignUser() throws InterruptedException, SQLException {
+        String [] emailaddress = generateEmail();
+        String [] phoneNumber = generatePhoneNumber();
+        String [] firstName = generateUser();
+        String [] lastName = generateUser();
+        String [] userRef = generateRandomTeamReference();
+        String [] teamName = generateRandomTeamName();
+        String [] teamReference = generateRandomTeamReference();
+        String [] teamDescription = fillInDescription();
+        loginAndVerifySuccess("am", "alex01");
+        navigateToDropdown("menu-administration");
+        navigateToDropdown("menu-administration-teams");
+        selectAddNewTeam();
+        fillInTeamsDetails(teamName[0], teamReference[0], teamDescription[0]);
+        selectAccountAdminPermissions();
+        selectCreateNewTeam();
+        searchTeam(teamReference[0], teamName[0], teamDescription[0], "Pending");
+        validateTeamsCreated(1, teamReference[0], teamName[0], teamDescription[0]);
+        validateAdminTeamPermissions(teamName[0]);
+        navigateToDropdown("menu-administration");
+        navigateToDropdown("menu-administration-users");
+        selectAddNewUser();
+        fillInUserDetails(emailaddress[0], firstName[0], lastName[0], userRef[0], phoneNumber[0]);
+        selectTeamFromUserCreation(teamName[0]);
+        selectCreateUser();
+        searchUser(userRef[0],firstName[0], lastName[0], emailaddress[0], phoneNumber[0]);
+        validateUserCreated(1, emailaddress[0], firstName[0], lastName[0], phoneNumber[0], userRef[0]);
+        navigateToDropdown("menu-administration");
+        navigateToDropdown("menu-administration-teams");
+        searchTeam(teamReference[0], teamName[0], teamDescription[0], "Active");
+        logout();
     }
     @Test
     public void shouldCreateTeamWithPermissionsOrderBook() throws InterruptedException, SQLException {
@@ -188,4 +245,5 @@ public class OpenCSDTeamsSubModuleTest {
         validateTeamsCreated(1, teamReference[0], teamName[0], teamDescription[0]);
         validateMyClientsTeamPermissions(teamName[0]);
     }
+
 }
