d1a61e622bcf87f0fd15e599b97bd4b8eeaaa0cb
Chris Haigh <chris.haigh@setl.io>
2018-10-18 10:27:54 +0100

    TG-176 feature(fixed-header-directive): create a fixed header directive to datagrids

--- directives/index.ts
+++ directives/index.ts
@@ -1,22 +1,23 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {NumberFormatterDirective} from './number-formatter/directive';
-import {ClrDgIconSortDirective} from './clr-dg-sort-icon/directive';
-import {ClrDgSortNoSearchDirective} from './clr-dg-sort-nosearch/directive';
-import {HighlightDirective} from './highlight/highlight.directive';
-import {ClickedDirective} from './clicked/clicked.directive';
-import {BackToTopDirective} from './back-to-top/back-to-top.directive';
-import {FormChecker} from './enhanced-form/enhanced-form.directive';
+import { NumberFormatterDirective } from './number-formatter/directive';
+import { ClrDgIconSortDirective } from './clr-dg-sort-icon/directive';
+import { ClrDgSortNoSearchDirective } from './clr-dg-sort-nosearch/directive';
+import { HighlightDirective } from './highlight/highlight.directive';
+import { ClickedDirective } from './clicked/clicked.directive';
+import { BackToTopDirective } from './back-to-top/back-to-top.directive';
+import { FormChecker } from './enhanced-form/enhanced-form.directive';
 
-import {TooltipDirective} from './tooltip/tooltip.directive';
-import {FormStepsDirective} from './form-steps/formsteps';
-import {FormPercentDirective} from './form-percent/formpercent';
-import {ActiveHeaderButtonDirective} from './activeHeaderButton/active-header-button.directive';
-import {ClrDgRowClickableDirective} from './clr-dg-row-clickable/directive';
-import {MoneyValuePipe} from '../pipes';
-import {RowsPerPageDirective} from './rows-per-page/rowsperpage.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
+import { FormStepsDirective } from './form-steps/formsteps';
+import { FormPercentDirective } from './form-percent/formpercent';
+import { ActiveHeaderButtonDirective } from './activeHeaderButton/active-header-button.directive';
+import { ClrDgRowClickableDirective } from './clr-dg-row-clickable/directive';
+import { MoneyValuePipe } from '../pipes';
+import { RowsPerPageDirective } from './rows-per-page/rowsperpage.directive';
+import { FixedHeaderDirective } from './fixed-header/fixed-header.directive';
 
 @NgModule({
     declarations: [
@@ -33,6 +34,7 @@ import {RowsPerPageDirective} from './rows-per-page/rowsperpage.directive';
         FormPercentDirective,
         ActiveHeaderButtonDirective,
         RowsPerPageDirective,
+        FixedHeaderDirective,
     ],
     exports: [
         NumberFormatterDirective,
@@ -48,6 +50,7 @@ import {RowsPerPageDirective} from './rows-per-page/rowsperpage.directive';
         FormPercentDirective,
         ActiveHeaderButtonDirective,
         RowsPerPageDirective,
+        FixedHeaderDirective,
     ],
     imports: [
         CommonModule,
