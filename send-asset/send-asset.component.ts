6347fd9e99cbf2ead4c98a4413a75ec56af21a1f
Adam Crowe <adam.crowe@setl.io>
2018-10-24 10:57:08 +0100

    TG-438 fix(send-asset-other-address) Added regex validator to recipent address

--- send-asset/send-asset.component.ts
+++ send-asset/send-asset.component.ts
@@ -166,10 +166,12 @@ export class SendAssetComponent implements OnInit, OnDestroy {
      * @return {void}
      */
     setFormGroup(): void {
+        const addressPattern = '^[A]{1}[A-Za-z0-9_-]{32}[AQgw]{1}$';
+
         this.sendAssetForm = new FormGroup({
             asset: new FormControl('', Validators.required),
             assetAddress: new FormControl('', Validators.required),
-            recipient: new FormControl('', Validators.required),
+            recipient: new FormControl('', [Validators.required, Validators.pattern(addressPattern)]),
             amount: new FormControl('', [Validators.required, Validators.pattern('^((?!(0))[0-9]+)$')]),
         });
     }
