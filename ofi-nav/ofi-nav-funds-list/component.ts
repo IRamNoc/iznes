49473237aa54bae3f1fd7362c6f8224cbfe22b30
Phil Zeelte <phil.zeelte@setl.io>
2018-10-30 16:00:23 +0000

    nav list now reloads after cancelling

--- ofi-nav/ofi-nav-funds-list/component.ts
+++ ofi-nav/ofi-nav-funds-list/component.ts
@@ -201,6 +201,8 @@ export class OfiNavFundsList implements OnInit, OnDestroy {
             if(res[1].Data[0].Status === 'Fail') return this.onCancelNavError();
 
             this.alertService.create('success', this.cancelNavSuccessMessage);
+
+            this.clearRequestedList();
         }
     }
 
