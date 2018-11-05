557e042f50b8d90ed892a31b4c9ec62a53c791a4
ming <mingrui.huang@setl.io>
2018-11-05 15:59:52 +0000

    TG-3062 feat: update wallet access
    - actions for add/remove wallet

--- wallet/my-wallets/reducer.ts
+++ wallet/my-wallets/reducer.ts
@@ -1,15 +1,17 @@
-import { AsyncTaskResponseAction } from '@setl/utils/sagaHelper/actions';
 import * as MyWalletActions from './actions';
 import { MyWalletsState, WalletDetail } from './model';
 import * as _ from 'lodash';
 import { List, fromJS, Map } from 'immutable';
+import { immutableHelper } from '@setl/utils';
+import { ADD_TO_OWN_WALLETS, DELETE_FROM_OWN_WALLETS, NewWallet } from './actions';
+import { Action } from 'redux';
 
 const initialState: MyWalletsState = {
     walletList: {},
 };
 
 export const MyWalletsReducer = function (state: MyWalletsState = initialState,
-                                          action: AsyncTaskResponseAction) {
+                                          action: Action) {
     switch (action.type) {
     case MyWalletActions.SET_OWN_WALLETS:
         const walletsData = _.get(action, 'payload[1].Data', []);
@@ -21,6 +23,13 @@ export const MyWalletsReducer = function (state: MyWalletsState = initialState,
         });
 
         return newState;
+
+    case DELETE_FROM_OWN_WALLETS:
+        return handleDeleteFromOwnWallets(state, action);
+
+    case ADD_TO_OWN_WALLETS:
+        return handleAddToOwnWallets(state, action);
+
     default:
         return state;
     }
@@ -51,4 +60,57 @@ function formatWalletsDataResponse(rawWalletsData: any[]): WalletDetail[] {
         {}));
 
     return walletDetailList.toJS();
+}
+
+/**
+ * Add a wallet to own wallets
+ * @param {MyWalletsState} state
+ * @param {{walletIds?: NewWallet[]}} action
+ * @return {MyWalletsState}
+ */
+function handleAddToOwnWallets(state: MyWalletsState, action: {type: string; walletIds?: NewWallet[]}): MyWalletsState {
+    const newWalletsPayload = _.get(action, 'walletIds', []);
+    const currentWallets = immutableHelper.copy(state.walletList);
+    const newWallets = newWalletsPayload.reduce((accu, wallet: NewWallet) => {
+       accu[wallet.walletID] = {
+           correspondenceAddress: _.get(wallet, 'CorrespondenceAddress', null),
+           GLEI: _.get(wallet, 'GLEI', null),
+           accountId: _.get(wallet, 'accountID', 0),
+           bankWalletId: _.get(wallet, 'bankWalletID', 0),
+           commuPub: _.get(wallet, 'commuPub', ''),
+           permission: _.get(wallet, 'permission', 0),
+           permissionDetail: _.get(wallet, 'permissionDetail', ''),
+           userId: _.get(wallet, 'userID', 0),
+           walletId: _.get(wallet, 'walletID', 0),
+           walletLocked: _.get(wallet, 'walletLocked', 0),
+           walletName: _.get(wallet, 'walletName', ''),
+           walletType: _.get(wallet, 'walletType', 0),
+           walletTypeName: _.get(wallet, 'walletTypeName', ''),
+       };
+
+       return accu;
+    }, currentWallets);
+
+    return Object.assign({}, state, {
+        walletList: newWallets,
+    });
+}
+
+/**
+ * Delete wallet from own wallets.
+ * @param {MyWalletsState} state
+ * @param {{type: string; walletIds?: number[]}} action
+ * @return {MyWalletsState}
+ */
+function handleDeleteFromOwnWallets(state: MyWalletsState, action: {type: string; walletIds?: number[]}): MyWalletsState {
+    const walletsToDelete = _.get(action, 'walletIds', []);
+    const newWallets = immutableHelper.copy(state.walletList);
+    walletsToDelete.map((walletId: number) => {
+       delete newWallets[walletId];
+    });
+
+    return Object.assign({}, state, {
+        walletList: newWallets,
+    });
+
 }
