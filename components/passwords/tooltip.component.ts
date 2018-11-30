c3b4cbc36c294f3049f6ec147541485522566ae7
Chris Haigh <chris.haigh@setl.io>
2018-11-30 15:58:32 +0000

    TG-3418 fix(tooltip-translations): update tooltip on language change

--- components/passwords/tooltip.component.ts
+++ components/passwords/tooltip.component.ts
@@ -1,22 +1,49 @@
-import {Component} from '@angular/core';
-import {MultilingualService} from '@setl/multilingual';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MultilingualService } from '@setl/multilingual';
+import { select } from '@angular-redux/store';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-    selector : 'app-password-tooltip',
-    template : '<i class="fa fa-info-circle" [tooltip]="{text: passwordStrengthText, size: \'medium\'}"></i>'
+    selector: 'app-password-tooltip',
+    template: `<i *ngIf="showTooltip" class="fa fa-info-circle"
+                  [tooltip]="{text: passwordStrengthText, size: \'medium\'}"></i>`,
 })
-export class PasswordTooltipComponent{
-    passwordStrengthText;
+export class PasswordTooltipComponent implements OnInit {
+    public passwordStrengthText: string = '';
+    public showTooltip: boolean = true;
+    private subscription: Subscription;
+    @select(['user', 'siteSettings', 'language']) languageOb;
 
-    constructor(private _translate : MultilingualService) {
-        this.passwordStrengthText = this._translate.translate('The password must be at least 8 characters long and satisfy 3 of the following rules: ') + `
-        <ul class="mt-1">
-            <li>${this._translate.translate('One uppercase letter')}</li>
-            <li>${this._translate.translate('One lowercase letter')}</li>
-            <li>${this._translate.translate('One number')}</li>
-            <li>${this._translate.translate('One symbol')}</li>
-            <li>${this._translate.translate('One international character')}</li>
-        </ul>
-        `;
+    constructor(
+        private translate: MultilingualService,
+        private changeDetectorRef: ChangeDetectorRef,
+    ) {
+    }
+
+    ngOnInit() {
+        this.subscription = this.languageOb.subscribe(() => {
+            this.showTooltip = false;
+            this.changeDetectorRef.detectChanges();
+            this.setPasswordStrengthText();
+        });
+    }
+
+    setPasswordStrengthText() {
+        this.passwordStrengthText = `${this.translate.translate(
+            'The password must be at least 8 characters long and satisfy 3 of the following rules:')}
+            <ul class="mt-1">
+                <li>${this.translate.translate('One uppercase letter')}</li>
+                <li>${this.translate.translate('One lowercase letter')}</li>
+                <li>${this.translate.translate('One number')}</li>
+                <li>${this.translate.translate('One symbol')}</li>
+                <li>${this.translate.translate('One international character')}</li>
+            </ul>`;
+        this.showTooltip = true;
+        this.changeDetectorRef.detectChanges();
+    }
+
+    ngOnDestroy() {
+        this.changeDetectorRef.detach();
+        this.subscription.unsubscribe();
     }
 }
