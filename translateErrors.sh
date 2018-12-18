b14474c5c05b18bce0632ac3605d511f22993cb4
Adam Crowe <adam.crowe@setl.io>
2018-12-18 09:28:27 +0000

    Add script to translate errors and status messages from stored procedures

---
+++ translateErrors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Translate errors and status messages from database stored procedures
+
+# Define database constants
+user="root"
+pwd="alex01"
+database="setlnet"
+query="SELECT errorMessage FROM tblErrors"
+
+# Define API constants
+iznesSiteId=1
+apiUrl="http://10.0.2.72"
+apiPort="8000"
+
+# Set API endpoint
+apiEndpoint="$apiUrl:$apiPort/api/sites/$iznesSiteId/whitelist-translations"
+
+# Send errors to the translations database
+mysql -u${user} -p${pwd} ${database} -N -e "${query}" | while IFS= read -r item
+do
+   
+    # Create mltag for each item
+    mltag=`echo "console.log('txt_' + '$item'.replace(/[^a-z0-9A-Z]/g, '').toLowerCase());" | node`
+    
+    # Truncate mltag if length is longer than 34 characters
+    length=${#mltag}
+
+    if [ $length -gt 34 ]; then
+        hash=`echo -n "$mltag" | shasum -a 256`
+        mltag=`echo "console.log('$mltag'.substring(0, 34) + '$hash'.substring(10, 20));" | node`
+    fi    
+
+    # Create payload object
+    payload=`echo "console.log(JSON.stringify({ mltag: '$mltag', value: '$item', location: 'stored_procedure' }));" | node`
+    
+    # Send to API
+    curl -d "$payload" -H "Content-Type: application/json" -X POST ${apiEndpoint}
+
+    # echo "$payload"
+   
+done 
