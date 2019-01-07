ad0dcfcd46c610648893198f9f2796c5c56a7583
Chris Haigh <chris.haigh@setl.io>
2019-01-07 15:32:57 +0000

    TG-490 feature(encumber-report): add encumberance data redux actions

--- wallet/encumbrances/index.ts
+++ wallet/encumbrances/index.ts
@@ -3,8 +3,6 @@ export { encumbrancesReducer } from './reducer';
 export { EncumbrancesState } from './model';
 export {
     SET_ENCUMBRANCES,
-    // SET_REQUESTED_ENCUMBRANCES,
     setRequestedEncumbrances,
-    // CLEAR_REQUESTED_ENCUMBRANCES,
     clearRequestedEncumbrances,
 } from './actions';
