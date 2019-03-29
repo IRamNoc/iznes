5829858f221c0b7076a6ea8f4e65c2fe98be1bec
Chris Haigh <chris.haigh@setl.io>
2019-03-29 15:52:38 +0000

    TG-XXX feat(ux-nav): add new appConfig properties to model

--- appConfig/appConfig.model.ts
+++ appConfig/appConfig.model.ts
@@ -5,21 +5,21 @@ export interface MenuItem {
     element_id: string;
     router_link?: string;
     dynamic_link?: string;
-    children?: Array<MenuItem>;
+    children?: MenuItem[];
 }
 
 export interface MenuSpec {
     top: {
         profile: {
-            [userType: string]: Array<MenuItem>;
-        }
+            [userType: string]: MenuItem[];
+        },
     };
     side: {
-        [userType: string]: Array<MenuItem>;
+        [userType: string]: MenuItem[];
     };
-    disabled?: Array<string>;
+    disabled?: string[];
     hidden?: {
-        [userType: string]: Array<String>;
+        [userType: string]: String[];
     };
 }
 
@@ -38,15 +38,27 @@ export interface AppConfig {
     menuSpec: MenuSpec;
     nonMenuLink?: RegStr[];
     reportingNodeUrl: string;
-    messagesMenu: Array<MessagesMenuItem>;
+    messagesMenu: MessagesMenuItem[];
     logoUrl: string;
+    logoLightUrl: string;
     logoID: string;
     platform: string;
     platformLegal?: string;
-    topbarLogoUrl: string;
-    topbarLogoUrlDark: string;
+    platformLabel?: string;
     applyRestrictUrl?: boolean;
     internalMessageSender?: string;
+    backgroundImage: string;
+    loginOverlay?: boolean;
+    loginContent?: {
+        title?: string,
+        description?: string,
+        btn?: {
+            text: string,
+            urlEN: string,
+            urlFR: string,
+        },
+    };
+    topNavBarColour?: string;
 }
 
 export interface MessagesMenuItem {
