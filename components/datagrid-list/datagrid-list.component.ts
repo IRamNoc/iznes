fbdfa2131d86cdf6b423ec3b5f4497696f1ebcdb
Chris Haigh <chris.haigh@setl.io>
2019-04-11 15:25:19 +0100

    TG-XXX feat(datagrid): refactor and fixup export csv and pdf

--- components/datagrid-list/datagrid-list.component.ts
+++ components/datagrid-list/datagrid-list.component.ts
@@ -1,10 +1,27 @@
-import { Component, Injectable, Input, Output, EventEmitter, ViewChild, ViewChildren, AfterViewInit, ElementRef,
-    QueryList, OnInit, ChangeDetectorRef } from '@angular/core';
-import { ClrDatagrid, ClrDatagridSortOrder, ClrDatagridStateInterface } from '@clr/angular';
+import {
+    Component,
+    Injectable,
+    Input,
+    Output,
+    EventEmitter,
+    ViewChild,
+    ViewChildren,
+    AfterViewInit,
+    ElementRef,
+    QueryList,
+    OnInit,
+    ChangeDetectorRef,
+} from '@angular/core';
+import { ClrDatagrid, ClrDatagridSortOrder, ClrDatagridStateInterface, ClrLoadingState } from '@clr/angular';
 import { NgRedux } from '@angular-redux/store';
 import { DatagridListFieldModel } from './models/datagrid-list-field.model';
 import { DatagridListActionModel } from './models/datagrid-list-action.model';
-import { DatagridFieldsModel, DatagridListData, DatagridSearchForm } from './models/datagrid-list-inputs.model';
+import {
+    DatagridFieldsInterface,
+    DatagridListData,
+    DatagridSearchForm,
+    ExportOptionsInterface,
+} from './models/datagrid-list-inputs.interface';
 import { DataGridStringFilter } from './filters/string.filter';
 import { PermissionsService } from '@setl/utils';
 import { FormItemType } from '@setl/utils/components/dynamic-forms';
@@ -12,13 +29,15 @@ import { FileService, PdfService } from '@setl/core-req-services';
 import * as SagaHelper from '@setl/utils/sagaHelper/index';
 import { DynamicFormComponent } from '@setl/utils/components/dynamic-forms/component';
 import { AlertsService } from '@setl/jaspero-ng2-alerts/src/alerts.service';
-import { filter, each, cloneDeep, pick, get } from 'lodash';
+import { cloneDeep, get } from 'lodash';
 import { Buffer } from 'buffer';
 import * as json2csv from 'json2csv';
+import { MultilingualService } from '@setl/multilingual';
 
 @Component({
     selector: 'datagrid-list',
     templateUrl: './datagrid-list.component.html',
+    styleUrls: ['./datagrid-list.styles.scss'],
 })
 
 @Injectable()
@@ -28,7 +47,7 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
     @ViewChild('dataGrid') public dataGrid: ClrDatagrid;
     @ViewChildren('dataGridFilter', { read: ElementRef }) public dataGridFilter: QueryList<ElementRef>;
 
-    @Input() fieldsModel: DatagridFieldsModel;
+    @Input() fieldsModel: DatagridFieldsInterface;
     @Input() listData: DatagridListData[] = [];
     @Input() listActions: DatagridListActionModel[];
     @Input() filters: any = {};
@@ -38,7 +57,7 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
     @Input() totalItems: number = 0;
     @Input() lazyLoaded: boolean = false;
     @Input() showHideColumns: boolean = false;
-    @Input() set exportOptions(options) { this.setExport(options); }
+    @Input() set exportOptions(options: ExportOptionsInterface) { this.export = { ...options }; }
 
     @Output() action: EventEmitter<any> = new EventEmitter<any>();
     @Output() refresh: EventEmitter<any> = new EventEmitter<any>();
@@ -47,23 +66,14 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
 
     public listFields: DatagridListFieldModel[] = [];
     public defaultSortOrder = ClrDatagridSortOrder.DESC;
-    public alertsService: AlertsService;
     public exportFileHash: string = '';
     public exportModalDisplay: boolean = false;
-    public export: any = {
+    public export: ExportOptionsInterface = {
         csv: false,
-        csvFileName: 'export.csv',
         pdf: false,
-        pdfFileName: 'export.pdf',
-        pdfOptions: {
-            title: '',
-            subtitle: '',
-            text: '',
-            rightAlign: [],
-            walletName: '',
-            date: '',
-        },
     };
+    public csvBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
+    public pdfBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
 
     public constructor(
         public ngRedux: NgRedux<any>,
@@ -71,6 +81,8 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
         public changeDetectorRef: ChangeDetectorRef,
         public permissionsService: PermissionsService,
         public pdfService: PdfService,
+        private alertsService: AlertsService,
+        private multilingual: MultilingualService,
     ) {}
 
     public ngOnInit() {
@@ -88,10 +100,10 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
             }
         }
 
-        this.alertsService = new AlertsService();
-
+        // Prepare search form
         if (typeof this.searchForm !== 'undefined') {
             Object.keys(this.searchForm).map((field) => {
+                // setup date range fields
                 if (this.searchForm[field].type === FormItemType.dateRange) {
                     this.searchForm[`${field}_from`] = cloneDeep(this.searchForm[field]);
                     this.searchForm[`${field}_from`].type = FormItemType.date;
@@ -182,158 +194,125 @@ export class DatagridListComponent implements OnInit, AfterViewInit {
     }
 
     /**
-     * Export List
-     *
      * Exports current dataGrid List to CSV format
+     *
      * @return {void}
      */
-    public exportList(type) {
-        const options = {};
+    public exportCSV() {
+        this.csvBtnState = ClrLoadingState.LOADING;
+
         const exportData = this.dataGrid.items['_filtered'];
+
         if (exportData.length === 0) {
+            this.alertsService.generate('error', this.multilingual.translate('There is no data to export'));
             return;
         }
-        if (type === 'csv') {
-            const encodedCsv = Buffer.from(json2csv.parse(exportData, options)).toString('base64');
-            const fileData = {
-                name: this.export['csvFileName'],
-                data: encodedCsv,
-                status: '',
-                filePermission: 1,
-            };
 
-            const asyncTaskPipe = this.fileService.addFile({
-                files: filter([fileData], (file) => {
-                    return file.status !== 'uploaded-file';
-                }),
-            });
+        const fileData = {
+            name: this.export['csvFileName'],
+            data: Buffer.from(json2csv.parse(exportData, {})).toString('base64'),
+            status: '',
+            filePermission: 1,
+        };
 
-            this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
-                asyncTaskPipe,
-                (data) => {
-                    if (data[1] && data[1].Data) {
-                        let errorMessage;
-                        each(data[1].Data, (file) => {
-                            if (file.error) {
-                                errorMessage = file.error;
-                            } else {
-                                this.exportFileHash = file[0].fileHash;
-                                this.showExportModal();
-                            }
-                        });
-                    }
-                },
-                (data) => {
-                    let errorMessage;
-                    each(data[1].Data, (file) => {
-                        if (file.error) {
-                            errorMessage += `${file.error}<br/>`;
-                        }
-                    });
-                    if (errorMessage) {
-                        if (errorMessage) {
-                        }
-                    }
-                }),
-            );
-        } else {
-            // generate pdf
-            const titles = {};
-            const titleKeys = [];
-            this.listFields.forEach((title) => {
-                titles[title.name] = title.label;
-                titleKeys.push(title.name);
-            });
+        const asyncTaskPipe = this.fileService.addFile({
+            files: [fileData],
+        });
 
-            const reportData = exportData.map((data) => {
-                return pick(data, titleKeys);
-            });
-            reportData.unshift(titles);
-            console.log('++ reportData', reportData);
+        this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
+            asyncTaskPipe,
+            (data) => {
+                const file = get(data, '[1].Data[0][0]', false);
 
-            const metadata = {
-                title: this.export.pdfOptions.title,
-                subtitle: this.export.pdfOptions.subtitle,
-                text: this.export.pdfOptions.text,
-                reportData,
-                rightAlign: this.export.pdfOptions.rightAlign,
-                walletName: this.export.pdfOptions.walletName,
-                date: this.export.pdfOptions.date,
-            };
+                if (file && file.fileHash) {
+                    this.exportFileHash = file.fileHash;
+                    this.csvBtnState = ClrLoadingState.SUCCESS;
+                    this.exportModalDisplay = true;
+                    this.changeDetectorRef.detectChanges();
+                } else {
+                    this.csvBtnState = ClrLoadingState.DEFAULT;
+                    this.alertsService.generate(
+                        'error',
+                        this.multilingual.translate('Something went wrong. Please try again later.'),
+                    );
+                }
+            },
+            (data) => {
+                const error = get(data, '[1].Data[0][0].error', '');
+                if (error) console.error('error');
+                this.csvBtnState = ClrLoadingState.DEFAULT;
+                this.alertsService.generate(
+                    'error',
+                    this.multilingual.translate('Something went wrong. Please try again later.'),
+                );
+            }),
+        );
+    }
 
-            const asyncTaskPipe = this.pdfService.createPdfMetadata({ type: null, metadata });
+    /**
+     * Exports current dataGrid List to PDF
+     *
+     * @return {void}
+     */
+    exportPDF() {
+        this.pdfBtnState = ClrLoadingState.LOADING;
+        const exportData = this.dataGrid.items['_filtered'];
 
-            this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
-                asyncTaskPipe,
-                (successResponse) => {
-                    const pdfID = get(successResponse, '[1].Data[0].pdfID', 0);
+        const titles = {};
+        this.listFields.forEach(title => titles[title.name] = title.label);
 
-                    if (!pdfID) {
-                        return this.alertsService.generate(
-                            'error', 'Something has gone wrong. Please try again later');
-                    }
+        const reportData = exportData.map((data) => {
+            return Object.keys(data).reduce((acc, key) => (titles[key] ? acc[titles[key]] = data[key] : '', acc), {});
+        });
 
-                    const pdfOptions = {
-                        orientation: 'portrait',
-                        border: { top: '15mm', right: '15mm', bottom: '0', left: '15mm' },
-                        footer: {
-                            height: '20mm',
-                            contents: `
-                            <div class="footer">
-                                <p class="left">${metadata.title} | {{page}} of {{pages}}</p>
-                                <p class="right">${metadata.date}</p>
-                            </div>`,
-                        },
-                    };
+        const metadata = {
+            title: this.export.pdfOptions.title,
+            subtitle: this.export.pdfOptions.subtitle,
+            text: this.export.pdfOptions.text,
+            data: reportData,
+            rightAlign: (this.export.pdfOptions.rightAlign || []).map(prop => titles[prop]),
+            walletName: this.export.pdfOptions.walletName,
+            date: this.export.pdfOptions.date,
+        };
 
-                    this.pdfService.getPdf(pdfID, this.export.pdfOptions.file, pdfOptions).then((response) => {
-                        this.exportFileHash = response;
-                        this.showExportModal();
-                        this.alertsService.create('clear');
-                    }).catch((e) => {
-                        console.error(e);
-                        this.alertsService.generate(
-                            'error', 'Something has gone wrong. Please try again later');
-                    });
-                },
-                (e) => {
+        const asyncTaskPipe = this.pdfService.createPdfMetadata({ type: null, metadata });
+
+        this.ngRedux.dispatch(SagaHelper.runAsyncCallback(
+            asyncTaskPipe,
+            (successResponse) => {
+                const pdfID = get(successResponse, '[1].Data[0].pdfID', 0);
+
+                if (!pdfID) {
+                    this.pdfBtnState = ClrLoadingState.DEFAULT;
+                    return this.alertsService.generate(
+                        'error', 'Something has gone wrong. Please try again later');
+                }
+
+                const pdfOptions = {
+                    orientation: this.export.pdfOptions.orientation,
+                    border: this.export.pdfOptions.border,
+                    footer: this.export.pdfOptions.footer,
+                };
+
+                this.pdfService.getPdf(pdfID, this.export.pdfOptions.file, pdfOptions).then((response) => {
+                    this.exportFileHash = response;
+                    this.pdfBtnState = ClrLoadingState.SUCCESS;
+                    this.exportModalDisplay = true;
+                    this.changeDetectorRef.detectChanges();
+                }).catch((e) => {
                     console.error(e);
+                    this.pdfBtnState = ClrLoadingState.DEFAULT;
                     this.alertsService.generate(
                         'error', 'Something has gone wrong. Please try again later');
-                }),
-            );
-        }
-    }
-
-    /**
-     * Show Export Modal
-     *
-     * @return {void}
-     */
-    public showExportModal() {
-        this.exportModalDisplay = true;
-        this.changeDetectorRef.markForCheck();
-        this.changeDetectorRef.detectChanges();
-    }
-
-    /**
-     * Hide Export Modal
-     *
-     * @return {void}
-     */
-    public hideExportModal() {
-        this.exportModalDisplay = false;
-        this.changeDetectorRef.markForCheck();
-        this.changeDetectorRef.detectChanges();
-    }
-
-    /**
-     * Sets export properties
-     *
-     * @param options
-     */
-    public setExport(options) {
-        this.export = Object.assign(this.export, options);
+                });
+            },
+            (e) => {
+                console.error(e);
+                this.pdfBtnState = ClrLoadingState.DEFAULT;
+                this.alertsService.generate(
+                    'error', 'Something has gone wrong. Please try again later');
+            }),
+        );
     }
 
     /**
---
+++ components/datagrid-list/models/datagrid-list-inputs.interface.ts
@@ -0,0 +1,54 @@
+import { FormItem } from '@setl/utils/index';
+
+export interface DatagridFieldsInterface {
+    [propertyName: string]: {
+        label: string,
+        type: 'text'|'number'|'label'|'icon',
+        options?: {
+            pipe?: string,
+            dateFormat?: string,
+            icon?: string,
+            labelMap?: {
+                [value: string]: {
+                    type: string,
+                    text: string,
+                },
+            };
+        },
+    };
+}
+
+export interface DatagridListData {
+    [propertyName: string]: string;
+}
+
+export interface DatagridSearchForm {
+    [propertyName: string]: FormItem;
+}
+
+export interface ExportOptionsInterface {
+    csv: boolean; // controls showing export btn
+    csvFileName?: string; // include .csv
+    pdf: boolean; // controls showing export btn
+    pdfFileName?: string; // include .pdf
+    pdfOptions?: {
+        file: string; // template file name (excluding .html) - lives in /classes/pdf/templates in MemberNode
+        title?: string;
+        subtitle?: string;
+        text?: string;
+        rightAlign?: string[]; // array of listData property names to right align on PDF datagrid
+        walletName?: string;
+        date?: string;
+        orientation?: 'portrait'|'landscape';
+        border?: {
+            top?: string; // default is 0, units: mm, cm, in, px (e.g. '20mm')
+            right?: string;
+            bottom?: string;
+            left?: string;
+        },
+        footer?: {
+            height?: string; // default is 0, units: mm, cm, in, px (e.g. '20mm')
+            contents?: string; // html to add to the footer - use {{page}} of {{pages}} to print out page numbering
+        };
+    };
+}
