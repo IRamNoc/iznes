bd1d0540dfa84e39f803027f29db7de671ec7773
Coppa <jucoppa@coppas-macbook-pro.home>
2020-06-04 14:04:33 +0200

    manage-transfers

---
+++ src/projects/ofi-main/ofi-transfer-in-out/manage-transfers/manage-transfers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-transfers',
+  templateUrl: './manage-transfers.component.html',
+  styleUrls: ['./manage-transfers.component.scss']
+})
+export class ManageTransfersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
