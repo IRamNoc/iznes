681528eb727ae08bb8d41fa98fcdddfe1ef05a3f
Charles Paris <charles.paris@iznes.io>
2020-06-05 13:12:09 +0000

    Added required modules into manage-transfer component

--- src/projects/ofi-main/ofi-transfer-in-out/manage-transfers/manage-transfers.component.ts
+++ src/projects/ofi-main/ofi-transfer-in-out/manage-transfers/manage-transfers.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgRedux, select } from '@angular-redux/store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LogService } from '@setl/utils';
+import { MultilingualService } from '@setl/multilingual';
 
 @Component({
     selector: 'app-manage-transfers',
@@ -7,7 +13,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageTransfersComponent implements OnInit {
 
-    constructor() { }
+    constructor(private fb: FormBuilder,
+                private cdr: ChangeDetectorRef,
+                private router: Router,
+                private redux: NgRedux<any>,
+                private logService: LogService,
+                public translate: MultilingualService,
+                private route: ActivatedRoute) { }
 
     ngOnInit() {
     }
