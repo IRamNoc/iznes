cf2444818a75ed8447c96d063312558081346034
Charles Paris <charles.paris@iznes.io>
2020-06-05 13:10:39 +0000

    Added some required components into transfer I/O modules

--- src/projects/ofi-main/ofi-transfer-in-out/ofi-transfer-in-out.module.ts
+++ src/projects/ofi-main/ofi-transfer-in-out/ofi-transfer-in-out.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+/* Multilingual module. */
+import { MultilingualModule } from '@setl/multilingual';
+
+/* Utils */
+import { SelectModule, DpDatePickerModule, SetlDirectivesModule, SetlPipesModule } from '@setl/utils';
+
+/* Clarity module. */
+import { ClarityModule } from '@clr/angular';
+
 import { CreateTransferComponent } from './create-transfer/create-transfer.component';
 import { ManageTransfersComponent } from './manage-transfers/manage-transfers.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        CommonModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        FormsModule,
+        ClarityModule,
+        SelectModule,
+        DpDatePickerModule,
+        SetlDirectivesModule,
+        SetlPipesModule,
+        MultilingualModule,
+        RouterModule,
+        DpDatePickerModule,
+        SetlDirectivesModule,
     ],
     declarations: [
         CreateTransferComponent,
