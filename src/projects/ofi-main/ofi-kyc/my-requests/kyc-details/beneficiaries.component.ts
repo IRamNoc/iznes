2728d6e677929d1d028804b6f2c1ee6e45ab260d
Coppa <jucoppa@coppas-macbook-pro.home>
2020-08-06 18:14:09 +0200

    TG-532 set the good percentage vue

--- src/projects/ofi-main/ofi-kyc/my-requests/kyc-details/beneficiaries.component.ts
+++ src/projects/ofi-main/ofi-kyc/my-requests/kyc-details/beneficiaries.component.ts
@@ -167,4 +167,27 @@ export class KycDetailsStakeholdersComponent implements OnInit {
 
         return finalName;
     }
+
+    percentageType(percentage) {
+
+        const onlyIntValue = percentage.replace(/\D/g,'');
+
+        if (onlyIntValue >= 1 && onlyIntValue < 10)
+            return this.translateService.translate("1 to less than 10 %");
+        else if (onlyIntValue >= 10 && onlyIntValue < 25)
+            return this.translateService.translate("10 to less than 25 %");
+        else if (onlyIntValue >= 25 && onlyIntValue < 33)
+            return this.translateService.translate("25 to less than 33 %");
+        else if (onlyIntValue >= 33 && onlyIntValue < 50)
+            return this.translateService.translate("33 to less than 50 %");
+        else if (onlyIntValue >= 50 && onlyIntValue < 66)
+            return this.translateService.translate("50 to less than 66 %");
+        else if (onlyIntValue >= 66 && onlyIntValue < 75)
+            return this.translateService.translate("66 to less than 75 %");
+        else if (onlyIntValue >= 75 && onlyIntValue < 90)
+            return this.translateService.translate("75 to less than 90 %");
+        else if (onlyIntValue >= 90 && onlyIntValue < 100)
+            return this.translateService.translate("90 to less than 100 %");
+        return percentage
+    }
 }
--- src/projects/ofi-main/ofi-kyc/my-requests/kyc-details/beneficiary-modal.component.ts
+++ src/projects/ofi-main/ofi-kyc/my-requests/kyc-details/beneficiary-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { KycDetailsService } from './details.service';
 import { mapValues, find } from 'lodash';
+import { MultilingualService } from '@setl/multilingual';
 
 @Component({
     selector: 'kyc-details-stakeholder-modal',
@@ -38,7 +39,9 @@ export class KycDetailsStakeholdersModalComponent implements OnInit {
 
     isOpen = true;
 
-    constructor() {
+    constructor(
+        private translateService: MultilingualService,
+    ) {
     }
 
     ngOnInit() {
@@ -46,6 +49,29 @@ export class KycDetailsStakeholdersModalComponent implements OnInit {
 
     closeModal() {
         this.close.emit();
+    }
+
+    percentageType(percentage) {
+
+        const onlyIntValue = percentage.replace(/\D/g,'');
+
+        if (onlyIntValue >= 1 && onlyIntValue < 10)
+            return this.translateService.translate("1 to less than 10 %");
+        else if (onlyIntValue >= 10 && onlyIntValue < 25)
+            return this.translateService.translate("10 to less than 25 %");
+        else if (onlyIntValue >= 25 && onlyIntValue < 33)
+            return this.translateService.translate("25 to less than 33 %");
+        else if (onlyIntValue >= 33 && onlyIntValue < 50)
+            return this.translateService.translate("33 to less than 50 %");
+        else if (onlyIntValue >= 50 && onlyIntValue < 66)
+            return this.translateService.translate("50 to less than 66 %");
+        else if (onlyIntValue >= 66 && onlyIntValue < 75)
+            return this.translateService.translate("66 to less than 75 %");
+        else if (onlyIntValue >= 75 && onlyIntValue < 90)
+            return this.translateService.translate("75 to less than 90 %");
+        else if (onlyIntValue >= 90 && onlyIntValue < 100)
+            return this.translateService.translate("90 to less than 100 %");
+        return percentage
     }
 
 }
