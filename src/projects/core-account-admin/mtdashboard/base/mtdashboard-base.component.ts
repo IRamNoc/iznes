fb312e08f76932f70c76add49e9676c94dea8bb5
Iznes Tech <tech@iznes.io>
2021-02-22 15:36:04 +0100

    TG-650 Populate MT Dashboard base compoenent

--- src/projects/core-account-admin/mtdashboard/base/mtdashboard-base.component.ts
+++ src/projects/core-account-admin/mtdashboard/base/mtdashboard-base.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MultilingualService } from '@setl/multilingual';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mtdashboard-base',
@@ -7,12 +8,30 @@ import { MultilingualService } from '@setl/multilingual';
   styleUrls: ['./mtdashboard-base.component.scss']
 })
 export class MtdashboardBaseComponent implements OnInit {
+  
+  mt502Url = '/account-admin/mtdashboard/mt502';
+  mt10xamUrl = '/account-admin/mtdashboard/mt10x-am';
+  mt10xinvUrl = '/account-admin/mtdashboard/mt10x-inv';
 
-  constructor(protected translate: MultilingualService) {
+  isMT502: boolean = false; 
+  isMT10XAM: boolean = false; 
+  isMT10XINV: boolean = false;
+
+  constructor(
+    protected translate: MultilingualService,
+    private router: Router,
+    ) {
     
    }
 
   ngOnInit() {
+    this.isMT502 = (this.router.url.indexOf('/account-admin/mtdashboard/mt502') !== -1) ? true : false;
+    this.isMT10XAM = (this.router.url.indexOf('/account-admin/mtdashboard/mt10x-am') !== -1) ? true : false;
+    this.isMT10XINV = (this.router.url.indexOf('/account-admin/mtdashboard/mt10x-inv') !== -1) ? true : false;
+
+    if (!this.isMT10XAM && !this.isMT10XINV && !this.isMT502) {
+      return this.router.navigateByUrl('/account-admin/mtdashboard/mt502');
+    }
   }
 
 }
