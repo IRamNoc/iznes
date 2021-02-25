c903befac1ba56746c11fd46a6e293ad3a9a1896
Iznes Tech <tech@iznes.io>
2021-02-25 11:01:30 +0100

    TG-650 Designed MT502 component

--- src/projects/core-account-admin/mtdashboard/mt10x-am/mt10x-am.component.ts
+++ src/projects/core-account-admin/mtdashboard/mt10x-am/mt10x-am.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MultilingualService } from '@setl/multilingual';
 @Component({
   selector: 'app-mt10x-am',
   templateUrl: './mt10x-am.component.html',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Mt10xAmComponent implements OnInit {
 
-  constructor() { }
-
+  filtersForm: FormGroup;
+  language = 'en';
+  dateFrom = '';
+  dateTo = '';
+  fundSpecificDates = [];
+  fundsList: Array<any> = [];
+  constructor(private fb: FormBuilder, public translate: MultilingualService) {
+    this.createFiltersForm();
+    this.fundSpecificDates = [
+      { id: 0, text: translate.translate('Specific NAV Date') },
+      { id: 1, text: translate.translate('Specific Settlement Date') },
+      { id: 2, text: translate.translate('Specific NAV Period') },
+      { id: 3, text: translate.translate('Specific Settlement Period') },
+  ];
+  }
+ 
   ngOnInit() {
   }
+ 
+  fromConfigDate = {
+    firstDayOfWeek: 'mo',
+    format: 'YYYY-MM-DD',
+    closeOnSelect: true,
+    disableKeypress: true,
+    locale: this.language,
+    isDayDisabledCallback: (thisDate) => {
+      // make sure the dateFrom that greater than dateTo can not be selected
+      if (!!thisDate && this.filtersForm.controls['dateTo'].value !== '') {
+        return (thisDate.diff(this.filtersForm.controls['dateTo'].value) > 0);
+      }
+      return false;
+    },
+  };
+  toConfigDate = {
+    firstDayOfWeek: 'mo',
+    format: 'YYYY-MM-DD',
+    closeOnSelect: true,
+    disableKeypress: true,
+    locale: this.language,
+    isDayDisabledCallback: (thisDate) => {
+      // make sure the dateTo that less than dateFrom can not be selected
+      if (!!thisDate && this.filtersForm.controls['dateFrom'].value !== '') {
+        return (thisDate.diff(this.filtersForm.controls['dateFrom'].value) < 0);
+      }
+      return false;
+    },
+  };
+  createFiltersForm() {
+    const today = new Date();
+    const yesterday = new Date(today.setDate(today.getDate() - 1)).toISOString().slice(0, 10);
+    const nextWeek = new Date(today.setDate(today.getDate() + 7)).toISOString().slice(0, 10);
+    this.filtersForm = this.fb.group({
+      selectList: [
+        '',
+      ],
+      specificDate: [
+        '',
+      ],
+      dateFrom: [
+        '',
+      ],
+      dateTo: [
+        '',
+      ],codeISIN:[
+        '',
+      ],
+    });
+    this.dateFrom = yesterday;
+    this.dateTo = nextWeek;
+    this.filtersForm.get('specificDate').patchValue(
+      [{
+        id: 2,
+        text: this.translate.translate('Specific NAV Period'),
+      }],
+      { emitEvent: false },
+    );
+    this.filtersForm.get('dateFrom').patchValue(yesterday, { emitEvent: false }); 
+    this.filtersForm.get('specificDate').updateValueAndValidity();
+    this.filtersForm.get('dateFrom').updateValueAndValidity();
+    this.filtersForm.get('dateTo').updateValueAndValidity();
+  }
+
 
 }
--- src/projects/core-account-admin/mtdashboard/mt502/mt502.component.ts
+++ src/projects/core-account-admin/mtdashboard/mt502/mt502.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MultilingualService } from '@setl/multilingual';
 @Component({
   selector: 'app-mt502',
   templateUrl: './mt502.component.html',
@@ -7,9 +8,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Mt502Component implements OnInit {
 
-  constructor() { }
+  panelDef = {};
+  panelColumns = {};
 
-  ngOnInit() {
+  constructor(public translate: MultilingualService) {
   }
 
+  ngOnInit() {
+    this.initPanelColumns();
+    this.initPanelDefinition();
+  }
+
+  initPanelColumns(): void {
+		this.panelColumns = {
+      date: {
+        label: this.translate.translate('Date'),
+        dataSource: 'date',
+        sortable: true,
+      },
+      fundShareName: {
+				label: this.translate.translate('Fund Share'),
+				dataSource: 'fundShareName',
+				sortable: true,
+			},
+			isin: {
+				label: this.translate.translate('ISIN Code'),
+				dataSource: 'isin',
+				sortable: true,
+			},
+      orderType: {
+        label: this.translate.translate('Order Type'),
+				dataSource: 'orderType',
+				sortable: true,
+      },
+      quantity: {
+        label: this.translate.translate('Quantity'),
+				dataSource: 'quantity',
+				sortable: true,
+      },
+      messageType: {
+        label: this.translate.translate('Message Type'),
+				dataSource: 'messageType',
+				sortable: true,
+      },
+      messageReference: {
+        label: this.translate.translate('Message Reference'),
+				dataSource: 'messageReference',
+				sortable: true,
+      },
+      cutoffDate: {
+        label: this.translate.translate('Cutoff'),
+				dataSource: 'cutoffDate',
+				sortable: true,
+      },
+      generationIznes: {
+        label: this.translate.translate('Generation IZNES'),
+				dataSource: 'generationIznes',
+				sortable: true,
+      },
+      centralizer: {
+        label: this.translate.translate('Centralizer'),
+				dataSource: 'centralizer',
+				sortable: true,
+      },
+      send_to_centralizer: {
+        label: this.translate.translate('Send to centralizer'),
+				dataSource: 'send_to_centralizer',
+				sortable: true,
+      },
+      acknowledged: {
+        label: this.translate.translate('Acknowledged'),
+				dataSource: 'acknowledged',
+				sortable: true,
+      },
+      providertreatment: {
+        label: this.translate.translate('Providertreatment'),
+				dataSource: 'providertreatment',
+				sortable: true,
+      },
+      comments: {
+        label: this.translate.translate('Comments'),
+				dataSource: 'comments',
+				sortable: true,
+      }
+      
+		};
+	}
+
+  initPanelDefinition() {
+    this.panelDef = {
+      columns: [
+        this.panelColumns['date'],
+        this.panelColumns['fundShareName'],
+        this.panelColumns['isin'],
+        this.panelColumns['orderType'],
+        this.panelColumns['quantity'],
+        this.panelColumns['messageType'],
+        this.panelColumns['messageReference'],
+        this.panelColumns['cutoffDate'],
+        this.panelColumns['generationIznes'],
+        this.panelColumns['centralizer'],
+        this.panelColumns['send_to_centralizer'],
+        this.panelColumns['acknowledged'],
+        this.panelColumns['providertreatment'],
+        this.panelColumns['comments'],
+      ],
+    
+      open: true,
+      data: [],
+      count: 0,
+    };
+  }
 }
