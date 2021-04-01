6da7201f9f466330b31480b98c164e18ff0cd050
charlesparis <charles.paris@iznes.io>
2021-04-01 15:07:33 +0200

    Added BPSS inside custodian payment item on subportfolio

--- src/projects/ofi-main/ofi-sub-portfolio/sub-portfolio/component.ts
+++ src/projects/ofi-main/ofi-sub-portfolio/sub-portfolio/component.ts
@@ -39,8 +39,9 @@ export class OfiSubPortfolioComponent implements OnDestroy {
     custodianPaymentItems = [
         { id: 0, text: 'None / Aucun' },
         { id: 1, text: 'SGSS' },
+        { id: 3, text: 'BPSS' },
         { id: 5, text: 'CACEIS Bank France' },
-        {id: 11, text: 'OFI AM' }
+        { id: 11, text: 'OFI AM' }
     ];
     custodianPositionItems = [
         { id: 0, text: 'None / Aucun' },
