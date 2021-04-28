94ea97fd2667c47a22f5597b855fc94633407e86
charlesparis <charles.paris@iznes.io>
2021-04-28 19:08:03 +0200

    TG-665 Implemented give me access

--- src/projects/core-account-admin/godmode/component.ts
+++ src/projects/core-account-admin/godmode/component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GodModeService } from './service';
+import { ToasterService, Toast } from 'angular2-toaster';
 
 @Component({
   selector: 'app-account-godmode',
@@ -11,14 +13,58 @@ export class AccountGodmodeComponent implements OnInit {
     username: ['', Validators.required],
   });
   disableBtn: boolean = false;
+  giveMeAccessTriggered: boolean = false;
+  secondsLeft: number = 0;
+  secondsMax: number = 10;
+  toastTimer;
+  timerToast: Toast;
+  toasterConfig: any = {
+      type: 'warning',
+      title: '',
+      timeout: 0,
+      tapToDismiss: false,
+  };
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private GodModeService: GodModeService,
+    private toaster: ToasterService,
+    ) { }
 
   ngOnInit() {
+    this.godModeFormGroup.valueChanges.subscribe(() => {
+      this.disableBtn = this.godModeFormGroup.valid;
+    });
   }
   
   handleSubmitButtonClick()
   {
-    console.log("ok");
+    const request = {
+      username: this.godModeFormGroup.controls['username'].value,
+    };
+
+    const timer = setInterval(() => {
+      this.secondsLeft = this.secondsLeft > 0 ? this.secondsLeft - 1 : 0;
+    }, 1000);
+
+    this.disableBtn = false;
+    this.giveMeAccessTriggered = true;
+    this.secondsLeft = this.secondsMax;
+
+    this.GodModeService.requestGiveMeAccessAdmin(request).then((result) => {
+      const data = result[1].Data;
+      if (data.error) {
+        this.godModeFormGroup.controls['username'].setValue(['']);
+        return this.toaster.pop('error', data.Message);
+      }
+    }).catch((error) => {
+      const data = error[1].Data;
+      this.godModeFormGroup.controls['username'].setValue(['']);
+      return this.toaster.pop('error', data.Message);
+    }).finally(() => {
+      this.disableBtn = true;
+      this.giveMeAccessTriggered = false;
+      clearInterval(timer);
+    });
   }
 }
